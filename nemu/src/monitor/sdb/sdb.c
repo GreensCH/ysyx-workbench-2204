0ce8915a6c7a264912177ff4558c0c62ee2daca0
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 22:54:34 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:54:34 up  1:24,  1 user,  load average: 0.29, 0.46, 0.45

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -140,10 +140,14 @@ static int cmd_x(char *args) {
   }
   //DO ADDR CONVERT
   for(int p=0;p<offset;p++){
-    val = (*guest_to_host(base+4*(p  )))<<0/
-        + (*guest_to_host(base+4*(p+1)))<<8/
-        + (*guest_to_host(base+4*(p+2)))<<16/
-        + (*guest_to_host(base+4*(p+3)))<<24;
+    val = 12<<0/
+        + 34<<8/
+        + 56<<16/
+        + 78<<24;
+    // val = (*guest_to_host(base+4*(p  )))<<0/
+    //     + (*guest_to_host(base+4*(p+1)))<<8/
+    //     + (*guest_to_host(base+4*(p+2)))<<16/
+    //     + (*guest_to_host(base+4*(p+3)))<<24;
 
     printf("addr(0x%lx),value(0x%lx)\n",(base+4*p),val);
   }
