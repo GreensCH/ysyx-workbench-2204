5493538bde0ceb198d2357a80618fb26ad233730
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 15:08:51 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:08:51 up  2:21,  1 user,  load average: 0.38, 0.30, 0.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -9,7 +9,7 @@ enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-
+  TK_NUM,
 };
 
 static struct rule {
@@ -24,6 +24,10 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+  {"[0-9]+", TK_NUM},   // number
+  {"\\-", '-'},         // sub
+  {"\\*", '*'},         // mult
+  {"\\/", '/'},         // division
 };
 
 #define NR_REGEX ARRLEN(rules)
