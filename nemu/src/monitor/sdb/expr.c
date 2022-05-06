8038facb248c8e12d4b85d05440133d2541e133c
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 22:46:21 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:46:21 up  9:50,  1 user,  load average: 0.26, 0.31, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -198,7 +198,7 @@ word_t eval(int p,int q,bool *success){
       //printf("%c%s",tokens[i].type,tokens[i].str);
     }
     if(count){
-      Log("*** Info: count>0***");
+      Log("*** Info: count>0 ***");
       p = q;
       op = q+1;
     }
@@ -257,11 +257,12 @@ word_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
-  word_t result = eval(0,nr_token-1,success);
-  printf("result:%ld\n",result);
+  // word_t result = eval(0,nr_token-1,success);
+  // printf("result:%ld\n",result);
+
   // printf("p:%d \t q:%d\n",tokens[0].type,tokens[nr_token-1].type);
   // printf("%d\n",check_parentheses(0,nr_token-1));
-  // Assert(check_parentheses(0,nr_token-1),"*** ERROR Check parentheses invalid");
+  Assert(check_parentheses(0,nr_token-1),"*** ERROR Check parentheses invalid");
 
   // printf("1\t%s:%ld\n",tokens[0].str,immdiate);
 
