24c1ad15b1f7f712969fa695089f232fcb27e981
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 22:09:34 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:09:34 up  9:13,  1 user,  load average: 0.77, 0.73, 0.50

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -223,8 +223,8 @@ word_t eval(int p,int q,bool *success){
 
     val1 = eval(p, op - 1, success);
     val2 = eval(op + 1, q, success);
-    printf("主运算符:%c,val1:%ld,val2:%ld\n",op_type,val1,val2);
     op_type = tokens[op].type;
+    printf("主运算符:%c,val1:%ld,val2:%ld\n",op_type,val1,val2);
     switch (op_type) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
