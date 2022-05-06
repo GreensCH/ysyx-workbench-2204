6cee4e3d65a6f2a43201624c94693812ca72f379
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 16:52:03 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:52:03 up  4:05,  1 user,  load average: 0.50, 0.30, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -183,6 +183,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
   // word_t res = eval(0,nr_token-1,success);
+  printf("p:%d \t q:%d\n",tokens[0].type,tokens[nr_token-1].type);
   Assert(check_parentheses(0,nr_token-1),"*** ERROR Check parentheses invalid");
   return 0;
 }
