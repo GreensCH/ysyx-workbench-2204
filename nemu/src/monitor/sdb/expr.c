28c5d0105a1cf3084dc1a09eefc676501257f2a6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 19:15:31 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:15:31 up  6:28,  1 user,  load average: 0.31, 0.31, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -91,7 +91,7 @@ static bool make_token(char *e) {
           case(TK_NUM):
             // Clone string
             for(int p=0; p<substr_len; p++)
-              tokens[nr_token].str[p]=substr_start[i];
+              tokens[nr_token].str[p]=substr_start[p];
             tokens[nr_token].str[substr_len]='\0';
             // Transfer type
             tokens[nr_token].type=rules[i].token_type;
