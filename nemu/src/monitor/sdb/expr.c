2b067f61e1de03682fdd2300225c1193ee7ea5a0
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-06 21:06:15 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:06:15 up  8:19,  1 user,  load average: 1.04, 0.59, 0.36

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -185,7 +185,7 @@ word_t eval(int p,int q,bool *success){
     }
     printf("p:%d,q:%d op_type:%c\t op:%d\n",p,q,op_type,op);
     if(op_type==')'){
-      for(int i = p; i < q ; i++){
+      for(int i = p; i < op+1 ; i++){
         printf("%c%s",tokens[i].type,tokens[i].str);
       }
       printf("\n");
