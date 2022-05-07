312b2ad29c9def1884926de89b1a6eca86777317
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-07 20:33:01 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:33:01 up 34 min,  1 user,  load average: 0.24, 0.17, 0.23

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -7,7 +7,8 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  vaddr_t pc;
+  word_t val_old;
 } WP;
 
 static WP wp_pool[NR_WP] = {};
@@ -25,4 +26,19 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+
+/* 
+* new_wp(),free_wp()监视点池的接口，被其它函数调用
+* 
+*/
+
+/*
+* new_wp()从free_链表中返回一个空闲的监视点结构
+*/
+WP* new_wp();
+
+/*
+* free_wp()将wp归还到free_链表中
+*/
+void free_wp(WP *wp);
 
