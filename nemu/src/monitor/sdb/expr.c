74c0a509e308a2a147882a5f817d1fe7517497ab
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-07 15:27:03 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:27:03 up  5:09,  1 user,  load average: 0.56, 0.46, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -233,19 +233,23 @@ word_t eval(int p,int q,bool *success){
     for(int i = p; i <= q; i++){
       //单符号逻辑
       if(tokens[i].type == '-'){//负号判断逻辑
-        if(i == 0 ||( i > 0 && tokens[i-1].type != TK_NUM)){//去除前一位是数字位情况
-            if(!is_ope_pri(1,tokens[i-1].type)){//去除前一位是括号位情况
-              printf("该符是负号:%c,op%d,p:%d,q:%d\n",tokens[i].type,i,p,q);//即前一位只要是符号则该位符号为负号
-              tokens[i].type = TK_MINUS;// tokens
-            }
+        if(i == 0)
+          tokens[i].type = TK_MINUS;// tokens
+        else if(i == 0 ||( i > 0 && tokens[i-1].type != TK_NUM)){//去除前一位是数字位情况
+          if(!is_ope_pri(1,tokens[i-1].type)){//去除前一位是括号位情况
+            printf("该符是负号:%c,op%d,p:%d,q:%d\n",tokens[i].type,i,p,q);//即前一位只要是符号则该位符号为负号
+            tokens[i].type = TK_MINUS;// tokens
+          }
         }
       }
       else if(tokens[i].type == '*'){
-        if(i == 0 ||( i > 0 && tokens[i-1].type != TK_NUM)){//去除前一位是数字位情况
-            if(!is_ope_pri(1,tokens[i-1].type)){//去除前一位是括号位情况
-              printf("该符是指针解引用符号:%c,op%d,p:%d,q:%d\n",tokens[i].type,i,p,q);//即前一位只要是符号则该位符号为指针解引用符
-              tokens[i].type = TK_DERE;// tokens
-            }
+        if(i == 0)
+          tokens[i].type = TK_MINUS;// tokens
+        else if(i == 0 ||( i > 0 && tokens[i-1].type != TK_NUM)){//去除前一位是数字位情况
+          if(!is_ope_pri(1,tokens[i-1].type)){//去除前一位是括号位情况
+            printf("该符是指针解引用符号:%c,op%d,p:%d,q:%d\n",tokens[i].type,i,p,q);//即前一位只要是符号则该位符号为指针解引用符
+            tokens[i].type = TK_DERE;// tokens
+          }
         }
       }
       else{//其他意外排除(即两个符号连在一起)
