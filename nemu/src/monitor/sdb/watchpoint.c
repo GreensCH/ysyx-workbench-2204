9784d75fe5b15b041245d98d3068aefeb6815aff
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-07 22:38:02 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:38:02 up  1:04,  1 user,  load average: 0.41, 0.26, 0.28

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -69,13 +69,13 @@ void free_wp(WP *wp){
 }
 
 WP* find_active_wp(int NO){
-  WP *p = head;
+  // WP *p = head;
   // for(; p -> next != NULL; p = p->next){
   //   if(p -> NO == NO){
   //     return p;
   //   }
   // }
-  return p;
+  return head;
 }
 
 WP* find_idle_wp(int NO){
