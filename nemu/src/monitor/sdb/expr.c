cc1a82bd6f49cc2a10d9c33ccb0fec18f4d34181
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-07 15:32:39 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:32:39 up  5:14,  1 user,  load average: 0.37, 0.39, 0.43

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -255,7 +255,8 @@ word_t eval(int p,int q,bool *success){
       else{//其他意外排除(即两个符号连在一起)
         if(i == 0 ||( i > 0 && tokens[i-1].type != TK_NUM)){//去除前一位是数字位情况
           if(!is_ope_pri(1,tokens[i-1].type)){//去除前一位是括号位情况
-            Log("*** ERROR Cannot get main operation position!!! ***");
+            Log("*** ERROR Operator connection :%s%s***", tokens[i].str, tokens[i-1].str);
+            return false;
           }
         }
       } 
