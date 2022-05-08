a9bcc39185c59a2db9cb3f0f7534b034122f1a71
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-08 17:54:38 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:54:38 up  6:37,  2 users,  load average: 0.85, 0.75, 0.67

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -124,8 +124,8 @@ static int cmd_watch(char *args){
 static int cmd_b(char *args){
   bool success = false;
   //get pc addr
-  // word_t addr = expr(args, &success);
-  word_t addr = atoi(args);
+  word_t addr = expr(args, &success);
+  // word_t addr = atoi(args);
   if(addr == 0){
     Log("*** ERROR Fail to add break point ***");
     return -1;
