088fe42084a2fc3b6773ced3d3c3f6f55dd98f10
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-08 14:16:45 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:16:45 up  2:59,  2 users,  load average: 0.72, 0.53, 0.45

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -242,10 +242,11 @@ word_t eval(int p, int q, bool *success){
       //单符号逻辑
       if(tokens[i].type == '-' || tokens[i].type == '*'){// More Exprs Expected
         //单运算符在第0位的情况
-        //去除前一位是数字位//和括号位情况
+        //去除前一位是数字位//和右括号位情况
         if((i == 0) || (i > 0 && 
         cal_pri_lut(TK_NUM) != cal_pri_lut(tokens[i-1].type) && 
-        cal_pri_lut('(')  != cal_pri_lut(tokens[i-1].type))){
+        ')' != tokens[i-1].type)){
+        /*cal_pri_lut('(')  != cal_pri_lut(tokens[i-1].type))){*/
             tokens[i].type = (tokens[i].type == '-') ? TK_MINUS :
                               (tokens[i].type == '*') ? TK_DERE : tokens[i].type;
         }
