fb66bc29955766589c1af1d4b650a36ed43136c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-11 22:39:53 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:39:53 up  6:32,  1 user,  load average: 0.18, 0.18, 0.20

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -21,7 +21,7 @@ enum {
 
 static word_t test;
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
-static word_t immU(uint32_t i) { test =  SEXT(BITS(-1, 31, 12), 20) << 12; return test;}
+static word_t immU(uint32_t i) { test =  SEXT(BITS(-1, 31, 12), 20) ; return test;}
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
 //TODO：
 // static word_t immR(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
