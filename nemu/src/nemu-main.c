c71b554179c16c269723a0251a40d3706d120332
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-11 22:46:07 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:46:07 up  6:39,  1 user,  load average: 0.15, 0.23, 0.20

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-  test1 = SEXT(BITS(1, 30, 0),1);
+  test1 = SEXT(BITS(0, 30, 0),1);
   printf("test1:0x%8lld\n",BITS(1, 30, 0));
   printf("test1:0x%8ld\n",test1);
   /* Start engine. */
