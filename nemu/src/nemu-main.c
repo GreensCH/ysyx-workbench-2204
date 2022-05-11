5287a6506625e4ac80c473c701fb8a08c985f42b
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-11 22:44:31 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:44:31 up  6:37,  1 user,  load average: 0.52, 0.30, 0.23

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -5,6 +5,8 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
+static word_t test1;
+
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
@@ -12,7 +14,8 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-  return SEXT(BITS(-1, 31, 12),20);
+  test1 = SEXT(BITS(-1, 31, 12),20);
+  printf("test1:0x%8ld\n",test1);
   /* Start engine. */
   engine_start();
 
