8524e36f1b0a0e45a39b47da5b38980b8362611f
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-12 14:55:51 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:55:51 up  5:03,  2 users,  load average: 0.27, 0.26, 0.21

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -12,35 +12,60 @@ enum {
   TYPE_N, // none
 };
 
+// xxxR指把reg的值存入xxx, xxxI指根据immediate存入xxx
 #define src1R(n) do { *src1 = R(n); } while (0)
 #define src2R(n) do { *src2 = R(n); } while (0)
 #define destR(n) do { *dest = n; } while (0)
 #define src1I(i) do { *src1 = i; } while (0)
 #define src2I(i) do { *src2 = i; } while (0)
-#define destI(i) do { *dest = i; } while (0)
+#define destI(i) do { *dest = i; } while (0) //立即数i放入dstR
 
-static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
+static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }//i是指令码而不是立即数
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
-//TODO：
-// static word_t immR(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
-// static word_t immJ(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
-// static word_t immB(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
+//TODO：SEXT.n = imm的最高位 BITS截取的是指令码
+static word_t immB(uint32_t i) { return SEXT(BITS(i, 31, 31), 1) << 12 | BITS(i, 6, 6) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1 ; }
+static word_t immJ(uint32_t i) { return SEXT(BITS(i, 31, 31), 1) << 20 | BITS(i, 19, 12) << 12 | BITS(i, 30, 21) << 1 ; }
+
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
   int rd  = BITS(i, 11, 7);
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
-  destR(rd);
+  // destR(rd);
   switch (type) {
-    case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
-    case TYPE_U: src1I(immU(i)); break;
-    case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
-    //TODO：
-    case TYPE_R: destI(immS(i)); src1R(rs1); src2R(rs2); break;
-    case TYPE_J: destI(immS(i)); src1R(rs1); src2R(rs2); break;
-    case TYPE_B: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+    //TODO
+    case TYPE_R: 
+      destR(rd); 
+      src1R(rs1); 
+      src2R(rs2); 
+    break;
+    case TYPE_I:
+      destR(rd);//redundancy
+      src1R(rs1);    
+      src2I(immI(i)); 
+    break;
+    case TYPE_S: 
+      destI(immS(i)); 
+      src1R(rs1); 
+      src2R(rs2); 
+    break;
+    //TODO
+    case TYPE_B: 
+      destI(immB(i)); //TODO
+      src1R(rs1); 
+      src2R(rs2); 
+    break;
+    case TYPE_U:
+      destR(rd);//redundancy
+      src1I(immU(i)); 
+    break;
+    //TODO
+    case TYPE_J: 
+      destR(rd); 
+      src1I(immJ(i)); //TODO
+    break;
   }
 }
 
