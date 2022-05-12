278362ac2ee1426652d113555da73a57d952caa6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-12 19:56:42 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:56:42 up  9:13,  2 users,  load average: 0.13, 0.34, 0.36

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -5,20 +5,6 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
-static word_t test1;
-
-void bprint(int p){
-        int i=0;
-        int bit1 = 1;
-        for (i=sizeof (p) * 8-1;i>=0; i--){
-                unsigned int x = (((bit1<<i)&p) !=0);
-                printf("%d", x);
-        }
-
-        printf("\n");
-}
-
-
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
@@ -27,15 +13,7 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
-  word_t b = BITS(0b1011111001011,62,0);
-  word_t i = SEXT(BITS(b, 31, 31), 1) << 20 | BITS(b, 19, 12) << 12 | BITS(b, 20, 20) << 11 | BITS(b, 30, 21) << 1 ;
-  printf("bits:0b");bprint(i);
-  test1 = SEXT(i,12);
-  printf("sext12:0x");bprint(test1);
-  test1 = SEXT(i,13);
-  printf("sext13:0x");bprint(test1);
   /* Start engine. */
-
   engine_start();
 
   return is_exit_status_bad();
