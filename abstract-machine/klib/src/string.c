1f5633bdbdc0fc00960ba37d1253ce18670e7f44
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-13 23:12:38 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:12:38 up  7:27,  1 user,  load average: 0.45, 0.41, 0.42

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -49,33 +49,40 @@ char *strcat(char *dst, const char *src) {
  * 如果返回值等于 0，则表示 str1 等于 str2。
 */
 
-int strcmp(const char *s1, const char *s2)
-{
-  int i = 0;
-  for (i = 0; s1[i] && s2[i]; i++)
+int strcmp(const char *s1, const char *s2) {
+  while(*s1 != '\0' && *s2 !='\0' )
   {
-    if (s1[i] < s2[i])
-      return -1;
-    else if (s1[i] > s2[i])
+    if(*s1 > *s2)
       return 1;
+    else if(*s1 < *s2)
+      return -1;
+    s1++;
+    s2++;
   }
-  if (!s1[i] && s2[i])
-    return -1;
-  if (s1[i] && !s2[i])
+  if(*s1 > *s2)
     return 1;
-  return 0;
+  else if(*s1 < *s2)
+    return -1;
+  else 
+    return 0;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n)
-{
-  for (int i = 0; i < n; i++)
+int strncmp(const char *s1, const char *s2, size_t n) {
+  while(*s1 != '\0' && *s2 !='\0' && n --)
   {
-    if (s1[i] > s2[i])
+    if(*s1 > *s2)
       return 1;
-    else if (s1[i] < s2[i])
+    else if(*s1 < *s2)
       return -1;
+    s1++;
+    s2++;
   }
-  return 0;
+  if(*s1 > *s2)
+    return 1;
+  else if(*s1 < *s2)
+    return -1;
+  else 
+    return 0;
 }
 
 void *memset(void *s, int c, size_t n)
