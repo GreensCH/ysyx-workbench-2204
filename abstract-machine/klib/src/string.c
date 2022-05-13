3f3996da9e5f05a695b54411ff47e567aaf88257
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-13 23:04:16 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:04:16 up  7:18,  1 user,  load average: 0.69, 0.52, 0.42

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -13,7 +13,8 @@ size_t strlen(const char *s) {
 
 char *strcpy(char *dst, const char *src) {
   char *p = dst;
-  while(*src != '\0'){
+  while(*src != '\0')
+  {
     *p++ = *src++;
   }
   return dst;
@@ -21,7 +22,8 @@ char *strcpy(char *dst, const char *src) {
 
 char *strncpy(char *dst, const char *src, size_t n) {
   char *p = dst;
-  while(n--){
+  while(n--)
+  {
     if(*src != '\0')
       *p++ = *src++;
     else
@@ -34,37 +36,107 @@ char *strcat(char *dst, const char *src) {
   char *p = dst;
   if(*src == '\0')
     return dst;
-  while(*p != '\0'){
+  while(*p != '\0')
     p++;
-  }
-  while(*src != '\0'){
+  while(*src != '\0')
     *p++ = *src++;
+  return dst;
+}
+
+/*
+ * 如果返回值小于 0，则表示 str1 小于 str2。
+ * 如果返回值大于 0，则表示 str1 大于 str2。
+ * 如果返回值等于 0，则表示 str1 等于 str2。
+*/
+
+int strcmp(const char *s1, const char *s2)
+{
+  int i = 0;
+  for (i = 0; s1[i] && s2[i]; i++)
+  {
+    if (s1[i] < s2[i])
+      return -1;
+    else if (s1[i] > s2[i])
+      return 1;
+  }
+  if (!s1[i] && s2[i])
+    return -1;
+  if (s1[i] && !s2[i])
+    return 1;
+  return 0;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    if (s1[i] > s2[i])
+      return 1;
+    else if (s1[i] < s2[i])
+      return -1;
+  }
+  return 0;
+}
+
+void *memset(void *s, int c, size_t n)
+{
+  if (s == NULL || n < 0)
+    return NULL;
+
+  void *tmp = s;
+  char *str = s;
+  for (int i = 0; i < n; i++)
+    str[i] = c;
+  return tmp;
+}
+
+void *memmove(void *dst, const void *src, size_t n)
+{
+  if (dst == NULL || n < 0 || src == NULL)
+    return NULL;
+
+  char *str_dst = dst;
+  const char *str_src = src;
+  if (str_dst > str_src && str_src + n > str_dst)
+  {
+    for (int i = n - 1; i >= 0; i--)
+      str_dst[i] = str_src[i];
+  }
+  else
+  {
+    for (int i = 0; i < n; i++)
+      str_dst[i] = str_src[i];
   }
   return dst;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+void *memcpy(void *out, const void *in, size_t n)
+{
+  if (out == NULL || n < 0 || in == NULL)
+    return NULL;
+
+  char *str_out = out;
+  const char *str_in = in;
+  for (int i = 0; i < n; i++)
+    str_out[i] = str_in[i];
+  return out;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
-}
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+  if (s1 == NULL || n < 0 || s2 == NULL)
+    return -1;
+  const char *str_s1 = s1;
+  const char *str_s2 = s2;
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
-}
-
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
-}
-
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
-
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++)
+  {
+    if (str_s1[i] > str_s2[i])
+      return 1;
+    else if (str_s1[i] < str_s2[i])
+      return -1;
+  }
+  return 0;
 }
 
 #endif
