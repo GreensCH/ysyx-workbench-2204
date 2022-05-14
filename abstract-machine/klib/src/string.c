f3396f3ab188f6133d4d5222110bc1cab7d30ae1
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-14 16:33:15 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:15 up  2:05,  1 user,  load average: 0.69, 0.44, 0.39

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,3 +1,8 @@
+/*
+ *  string.c
+ *
+ *  Copyright (C) 1991, 1992  Linus Torvalds
+ */
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdint.h>
@@ -5,42 +10,41 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t len = 0;
-  while(s[len])
-    len++;
-  return len;
+	const char *sc;
+
+	for (sc = s; *sc != '\0'; ++sc)
+		/* nothing */;
+	return sc - s;
 }
 
 char *strcpy(char *dst, const char *src) {
-  char *p = dst;
-  while(*src != '\0')
-  {
-    *p++ = *src++;
-  }
-  return dst;
+	char *tmp = dst;
+
+	while ((*dst++ = *src++) != '\0')
+		/* nothing */;
+	return tmp;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  char *p = dst;
-  while(n--)
-  {
-    if(*src != '\0')
-      *p++ = *src++;
-    else
-      *p++ = '\0';
-  }
-  return dst;
+	char *tmp = dst;
+
+	while (n) {
+		if ((*tmp = *src) != 0)
+			src++;
+		tmp++;
+		n--;
+	}
+	return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  char *p = dst;
-  if(*src == '\0')
-    return dst;
-  while(*p != '\0')
-    p++;
-  while(*src != '\0')
-    *p++ = *src++;
-  return dst;
+	char *tmp = dst;
+
+	while (*dst)
+		dst++;
+	while ((*dst++ = *src++) != '\0')
+		;
+	return tmp;
 }
 
 /*
@@ -50,96 +54,83 @@ char *strcat(char *dst, const char *src) {
 */
 
 int strcmp(const char *s1, const char *s2) {
-  while(*s1 != '\0' && *s2 !='\0' )
-  {
-    if(*s1 > *s2)
-      return 1;
-    else if(*s1 < *s2)
-      return -1;
-    s1++;
-    s2++;
-  }
-  if(*s1 > *s2)
-    return 1;
-  else if(*s1 < *s2)
-    return -1;
-  else 
-    return 0;
+	unsigned char c1, c2;
+
+	while (1) {
+		c1 = *s1++;
+		c2 = *s2++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+	}
+	return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  while(--n && *s1 == *s2 && *s1 != '\0' && *s2 !='\0') 
-  {
-    s1++;
-    s2++;
-  }
-  if(!n || (*s1 == *s2))
-    return 0;
-  if(*s1 > *s2)
-    return 1;
-  else 
-    return -1;
+	unsigned char c1, c2;
+
+	while (n) {
+		c1 = *s1++;
+		c2 = *s2++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+		n--;
+	}
+	return 0;
 }
 
 void *memset(void *s, int c, size_t n)
 {
-  if (s == NULL || n < 0)
-    return NULL;
+	char *xs = s;
 
-  void *tmp = s;
-  char *str = s;
-  for (int i = 0; i < n; i++)
-    str[i] = c;
-  return tmp;
+	while (n--)
+		*xs++ = c;
+	return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n)
 {
-  if (dst == NULL || n < 0 || src == NULL)
-    return NULL;
+	char *tmp;
+	const char *s;
 
-  char *str_dst = dst;
-  const char *str_src = src;
-  if (str_dst > str_src && str_src + n > str_dst)
-  {
-    for (int i = n - 1; i >= 0; i--)
-      str_dst[i] = str_src[i];
-  }
-  else
-  {
-    for (int i = 0; i < n; i++)
-      str_dst[i] = str_src[i];
-  }
+	if (dst <= src) {
+		tmp = dst;
+		s = src;
+		while (n--)
+			*tmp++ = *s++;
+	} else {
+		tmp = dst;
+		tmp += n;
+		s = src;
+		s += n;
+		while (n--)
+			*--tmp = *--s;
+	}
   return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n)
 {
-  if (out == NULL || n < 0 || in == NULL)
-    return NULL;
+	char *tmp = out;
+	const char *s = in;
 
-  char *str_out = out;
-  const char *str_in = in;
-  for (int i = 0; i < n; i++)
-    str_out[i] = str_in[i];
-  return out;
+	while (n--)
+		*tmp++ = *s++;
+	return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n)
 {
-  if (s1 == NULL || n < 0 || s2 == NULL)
-    return -1;
-  const char *str_s1 = s1;
-  const char *str_s2 = s2;
+	const unsigned char *su1, *su2;
+	int res = 0;
 
-  for (int i = 0; i < n; i++)
-  {
-    if (str_s1[i] > str_s2[i])
-      return 1;
-    else if (str_s1[i] < str_s2[i])
-      return -1;
-  }
-  return 0;
+	for (su1 = s1, su2 = s2; 0 < n; ++su1, ++su2, n--)
+		if ((res = *su1 - *su2) != 0)
+			break;
+	return res;
 }
 
 #endif
