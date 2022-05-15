350193ed551887720ae485cba522ff3ccbf58e85
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-15 17:05:46 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:05:46 up  3:18,  1 user,  load average: 0.27, 0.41, 0.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -91,7 +91,7 @@ int vsprintf(char *out, const char *fmt, va_list ap)
     /* 转移sfmt指针 */
     sfmt += 2;
   }
-  return sout - out;
+  return strlen(out);
 }
 
 int sprintf(char *out, const char *fmt, ...)
