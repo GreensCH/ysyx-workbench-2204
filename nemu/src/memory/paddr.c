cf62f224122155398a16f945a8d408dc9e2b6213
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 11:15:29 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:15:29 up  2:20,  1 user,  load average: 0.16, 0.30, 0.33

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -43,6 +43,9 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+  #ifdef CONFIG_MTRACE_COND
+    if(MTRACE_COND) { printf("read\n"); };
+  #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -50,6 +53,9 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE_COND
+    if(MTRACE_COND) { printf("write\n"); };
+  #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
