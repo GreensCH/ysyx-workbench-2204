ef7548c2dfc18e8f3d87b5deac6653140858558b
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 11:33:26 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:33:26 up  2:38,  1 user,  load average: 0.36, 0.43, 0.43

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -58,8 +58,8 @@ word_t paddr_read(paddr_t addr, int len) {
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE_COND
     if(MTRACE_COND) { 
-      if (likely(in_pmem(addr))) printf("PMEM-WE:%016lx@%016lx\n", data, (word_t)addr); 
-      IFDEF(CONFIG_DEVICE, printf("MMIO-WE:%016lx@%016lx\n", data, (word_t)addr)); 
+      if (likely(in_pmem(addr))) printf("PMEM-WE:0x%016lx @0x%016lx\n", data, (word_t)addr); 
+      IFDEF(CONFIG_DEVICE, printf("MMIO-WE:0x%016lx @0x%016lx\n", data, (word_t)addr)); 
     };
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
