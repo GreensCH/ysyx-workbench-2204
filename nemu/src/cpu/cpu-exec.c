dc10104002ae50cd53e7409111ad2378d76cef62
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 10:12:00 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:12:00 up  1:17,  1 user,  load average: 0.28, 0.35, 0.36

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -21,9 +21,9 @@ void itrace_log();
 
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-// #ifdef CONFIG_ITRACE_COND
-//   if (ITRACE_COND) {  add_itrace(_this->logbuf);  }//log_write("%s\n", _this->logbuf); 
-// #endif
+#ifdef CONFIG_ITRACE_COND
+  if (ITRACE_COND) {  add_itrace(_this->logbuf);  }
+#endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }//printf小于10条的命令
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT
