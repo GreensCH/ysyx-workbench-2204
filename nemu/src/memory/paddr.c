f6bf30db8b3fba71f7896df746c308eaed1c74c4
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 11:26:39 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:26:39 up  2:31,  1 user,  load average: 0.72, 0.58, 0.47

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -44,7 +44,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   #ifdef CONFIG_MTRACE_COND
-    if(MTRACE_COND) { printf("read:addr%d,value%ld\n", addr, pmem_read(addr, len)); };
+    if(MTRACE_COND) { printf("read:addr0x%016lx,value%ld\n", (word_t)addr, pmem_read(addr, len)); };
   #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -54,7 +54,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE_COND
-    if(MTRACE_COND) { printf("write:addr%d,value%ld\n", addr, data); };
+    if(MTRACE_COND) { printf("write:addr0x%016lx,value%ld\n", (word_t)addr, data); };
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
