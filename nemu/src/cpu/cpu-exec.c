def67721d22429bc07fcd0b2c6e21428548f5ced
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 10:58:53 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:58:53 up  2:04,  1 user,  load average: 0.24, 0.21, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -21,7 +21,7 @@ int   iringbuf_index = 0;
 
 void add_itrace(char *s){
   strcpy(iringbuf[iringbuf_index], s);
-  if(iringbuf_index < 16)
+  if(iringbuf_index < 15)
     iringbuf_index += 1;
   else
     iringbuf_index = 0;
