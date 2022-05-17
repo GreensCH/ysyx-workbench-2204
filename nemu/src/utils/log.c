a7bcb2cc794aa556c43a699ba967d77b4edd72ee
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 10:20:16 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:16 up  1:25,  1 user,  load average: 0.17, 0.48, 0.44

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -26,11 +26,11 @@ char  iringbuf[16][128];
 int   iringbuf_index = 0;
 
 void add_itrace(char *s){
-  // strcpy(iringbuf[iringbuf_index], s);
-  // if(iringbuf_index < 16)
-  //   iringbuf_index += 1;
-  // else
-  //   iringbuf_index = 0;
+  strcpy(iringbuf[iringbuf_index], s);
+  if(iringbuf_index < 16)
+    iringbuf_index += 1;
+  else
+    iringbuf_index = 0;
 }
 
 int get_itrace(char *s){
@@ -55,9 +55,9 @@ int get_itrace(char *s){
   return 16 - (_i - 2 - _irindex);
 }
 
+static char out[132];
 void itrace_log(){
   char s[128];
-  char out[132];
   int i = get_itrace(s);
   while(i){
     if(i == 1){
