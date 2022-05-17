d91fe698608898c94bb4bc4430ac0d8b582500ef
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 20:55:54 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:54 up  1:43,  6 users,  load average: 0.13, 0.28, 0.27

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -1,4 +1,7 @@
 #include <common.h>
+#ifdef FTRACE
+  #include <elf.h>
+#endif
 
 extern uint64_t g_nr_guest_inst;
 FILE *log_fp = NULL;
@@ -96,3 +99,78 @@ void mtrace_we_log(word_t data, word_t addr){
   IFDEF(CONFIG_DEVICE, Log("MMIO-WE:PC(0x%016lx) 0x%016lx @0x%016lx", isa_reg_str2val("PC", flag), data, addr)); 
 }
 #endif
+
+
+/*
+* ftrace
+*/
+#ifdef CONFIG_FTRACE
+#include <elf.h>
+
+typedef struct
+{
+    uint64_t fun_addr;
+    uint64_t fun_size;
+    char *fun_name;
+} elf_info;
+elf_info elf_func[1000];
+int elf_cnt = 0;
+
+
+void read_elf(char *elf_name)
+{
+    if (elf_name == NULL)
+    {
+        Log("no input elf file\n");
+        return;
+    }
+    FILE *stream;
+    stream = fopen(elf_name, "rb");
+    Assert(stream, "Can not open '%s'", elf_name);
+
+    unsigned char *buffer;
+    buffer = (unsigned char *)malloc(100500 * sizeof(unsigned char));
+    int ret = fread(buffer, sizeof(unsigned char), 100500, stream);
+    Assert(ret != 0, "Can not open '%s'", elf_name);
+
+    Elf64_Ehdr *ehdr = (Elf64_Ehdr *)buffer;
+    Elf64_Shdr *shdr = (Elf64_Shdr *)(buffer + ehdr->e_shoff);
+    Elf64_Shdr *shdr_strtab = NULL;
+    Elf64_Shdr *shdr_symtab = NULL;
+
+    for (int i = 0; shdr_strtab == NULL || shdr_symtab == NULL; i++)
+    {
+        if (shdr[i].sh_type == SHT_SYMTAB)
+        {
+            shdr_symtab = &shdr[i];
+        }
+        else if (shdr[i].sh_type == SHT_STRTAB)
+        {
+            shdr_strtab = &shdr[i];
+            break;
+        }
+    }
+
+    Elf64_Sym *table_sym = (Elf64_Sym *)(buffer + shdr_symtab->sh_offset);
+
+    for (int i = 0; i <= shdr_symtab->sh_size / shdr_symtab->sh_entsize; i++)
+    {
+        if (ELF64_ST_TYPE(table_sym[i].st_info) == STT_FUNC)
+        {
+            elf_func[elf_cnt].fun_addr = table_sym[i].st_value;
+            elf_func[elf_cnt].fun_size = table_sym[i].st_size;
+            elf_func[elf_cnt].fun_name = (char *)(buffer + shdr_strtab->sh_offset + table_sym[i].st_name);
+            elf_cnt++;
+        }
+    }
+
+    for (int i = 0; i < elf_cnt; i++)
+       Log("%lx %lx %s\n", elf_func[i].fun_addr, elf_func[i].fun_size, elf_func[i].fun_name);
+    return;
+}
+#endif
+
+
+///
+
+
