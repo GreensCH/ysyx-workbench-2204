9e9834977381414181ad7f27232280bd19556ed6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-18 13:31:50 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:31:50 up  1:56,  1 user,  load average: 0.56, 0.40, 0.31

--- nemu/include/isa.h
+++ nemu/include/isa.h
@@ -17,6 +17,7 @@ void init_isa();
 extern CPU_state cpu;
 void isa_reg_display();
 word_t isa_reg_str2val(const char *name, bool *success);
+const char *get_reg_name(int i);
 
 // exec
 struct Decode;
