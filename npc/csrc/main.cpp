3d8e6d3daa589949b2880d7f1db207bd8d486bf1
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-23 21:09:17 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:09:17 up 57 min,  1 user,  load average: 0.43, 0.45, 0.36

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,47 +1,36 @@
-// #include "include.h"
+#include "include.h"
+#include "verilated.h"
+
+int main(int argc, char** argv, char** env) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    VTop* top = new VTop{contextp};
+    printf("hello world\n");
+    while (!contextp->gotFinish()) { top->eval(); }
+    delete top;
+    delete contextp;
+    return 0;
+}
 
 
-// VerilatedContext* contextp = NULL;
-// VerilatedVcdC* tfp = NULL;
-// Vtop* top = NULL;
+static long load_img() {
+  if (img_file == NULL) {
+    Log("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
 
-// vluint64_t main_time = 0;       // Current simulation time
-// // This is a 64-bit integer to reduce wrap over issues and
-// // allow modulus.  This is in units of the timeprecision
-// // used in Verilog (or from --timescale-override)
-// double sc_time_stamp() {        // Called by $time in Verilog
-//     return main_time;           // converts to double, to match
-//                                 // what SystemC does
-// }
+  FILE *fp = fopen(img_file, "rb");
+  Assert(fp, "Can not open '%s'", img_file);
 
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
 
-// void init_nvboard(){
-//     // Below is arguments of nvboard_bind_pin()
-//     // pins , is_realtime_signal , is_output , width , phy_pins
-//     //nvboard_bind_pin( &top->f, BIND_RATE_SCR/*BIND_RATE_RT*/, BIND_DIR_OUT, 1, LD0);//bind pin to phy board
-    
-//     // If auto_bind is used , below line should be uncomment
-//     nvboard_bind_all_pins(top);
+  Log("The image is %s, size = %ld", img_file, size);
 
-//     nvboard_init();//init nvboard
-// }
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  assert(ret == 1);
 
-
-
-// int main(int argc, char** argv, char** env) {
-
-//     sim_init();
-//     init_nvboard();
-
-//     while (1/*!contextp->gotFinish()&&main_time<1000*/) { 
-//         sim_main();
-//         break;
-//         nvboard_update();
-//     }
-    
-//     nvboard_quit(); //quit nvboard
-//     sim_exit();
-
-//     return 0;
-//   }
-
+  fclose(fp);
+  return size;
+}
