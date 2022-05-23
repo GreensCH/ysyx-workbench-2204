d20d47749c76f640db48c9c9ef2b2b3212c417ac
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-23 21:12:24 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:12:24 up  1:00,  1 user,  load average: 0.23, 0.44, 0.38

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv, char** env) {
     VerilatedContext* contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     VTop* top = new VTop{contextp};
-    printf("hello world\n");
+    printf("%s\n", argv[0]);
     while (!contextp->gotFinish()) { top->eval(); }
     delete top;
     delete contextp;
@@ -13,24 +13,24 @@ int main(int argc, char** argv, char** env) {
 }
 
 
-static long load_img() {
-  if (img_file == NULL) {
-    Log("No image is given. Use the default build-in image.");
-    return 4096; // built-in image size
-  }
+// static long load_img() {
+//   if (img_file == NULL) {
+//     Log("No image is given. Use the default build-in image.");
+//     return 4096; // built-in image size
+//   }
 
-  FILE *fp = fopen(img_file, "rb");
-  Assert(fp, "Can not open '%s'", img_file);
+//   FILE *fp = fopen(img_file, "rb");
+//   Assert(fp, "Can not open '%s'", img_file);
 
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
+//   fseek(fp, 0, SEEK_END);
+//   long size = ftell(fp);
 
-  Log("The image is %s, size = %ld", img_file, size);
+//   Log("The image is %s, size = %ld", img_file, size);
 
-  fseek(fp, 0, SEEK_SET);
-  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
-  assert(ret == 1);
+//   fseek(fp, 0, SEEK_SET);
+//   int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+//   assert(ret == 1);
 
-  fclose(fp);
-  return size;
-}
+//   fclose(fp);
+//   return size;
+// }
