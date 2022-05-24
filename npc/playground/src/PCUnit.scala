de193b2dba0274c4828e5cd5cc69ecc9a364ddb9
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-24 14:43:10 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:43:10 up 33 min,  1 user,  load average: 1.51, 1.44, 1.27

--- npc/playground/src/PCUnit.scala
+++ npc/playground/src/PCUnit.scala
@@ -1,31 +1,29 @@
-// import chisel3._
-// import chisel3.util._
-// import chisel3.experimental._
+import chisel3._
+import chisel3.util._
+import chisel3.experimental._
 
-// object PcOpcode extends ChiselEnum {
-//   val rst = Value(0.U)
-//   val sta = Value(1.U)
-//   val dyn = Value(2.U)
-// }
+object PcOpcode extends ChiselEnum {
+  val init = Value(0.U)
+  val next = Value(1.U)
+  val jump = Value(2.U)
+}
 
+class PCUnit extends Module {
+  val io = IO(new Bundle {
+    val npc_op  = Input (PcOpcode())
+    val offset  = Input (UInt(64.W))
+    val pc      = Output(UInt(64.W))
+  })
 
-// class PCUnit extends Module {
-//   val io = IO(new Bundle {
-//     val npc_i   = Input (UInt(64.W))
-//     val npcop_i = Input (PcOpcode())
-//     val pc_o    = Output(UInt(64.W))
-//   })  
-//     io.o_test <> io.i_test;
-//     val mux_out = Mux1H(Seq(
-//     io.npcop_i(0) -> 2.U,
-//     io.npcop_i(1) -> 4.U,
-//     io.npcop_i(2) -> 8.U,
-//     io.npcop_i(4) -> 11.U
-// ))
-//     val pc = RegInit("h80000000".U(64.W))
-//     pc := mux_out
-// //    pc := io.i_test//0x1.U(64.W)
-// //    io.i_test := pc(0)
-// //    pc := io.npc_i
-
-// }
+    val npc_mux_out = MuxCase(
+      "h80000000".U(64.W),
+      Seq(
+        (io.npc_op === PcOpcode.init) -> "h80000000".U(64.W),
+        (io.npc_op === PcOpcode.next) -> 4.U(64.W),
+        (io.npc_op === PcOpcode.jump) -> io.offset,
+      ))
+    
+    val pc = RegInit("h80000000".U(64.W))
+    pc := npc_mux_out
+    io.pc <> pc
+}
--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -1,12 +1,6 @@
 import chisel3._
-import chisel3.util._
-import chisel3.experimental._
-
-
-object AluMux1Sel extends ChiselEnum {
-  val selectRS1, selectPC = Value
-}
-
+//import chisel3.util._
+//import chisel3.experimental._
 
 
 /**
@@ -17,17 +11,11 @@ object AluMux1Sel extends ChiselEnum {
 
 class Top extends Module {
   val io = IO(new Bundle {
-      val test_i = Input(Bool())
       val inst_i = Input(UInt(64.W))
-      val test_o = Output(Bool())
   })
-  // val pcunit = Module(new PCUnit)
-  io.test_i := DontCare
-  io.test_o := DontCare
-  io.inst_i := DontCare
 
-  // We can see the mapping by printing each Value
-AluMux1Sel.all.foreach(println)
-// AluMux1Sel(0=selectRS1)
-// AluMux1Sel(1=selectPC)
+  val pcunit = Module(new PCUnit)
+  pcunit.io.offset := io.inst_i
+  pcunit.io.npc_op := PcOpcode.init
+  pcunit.io.pc := DontCare
 }
