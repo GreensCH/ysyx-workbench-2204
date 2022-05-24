599437db252a54c426c48f3216bf29397a22bd21
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-24 15:21:10 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:21:10 up  1:11,  1 user,  load average: 0.59, 1.03, 1.07

--- npc/playground/src/PCUnit.scala
+++ npc/playground/src/PCUnit.scala
@@ -3,7 +3,6 @@ import chisel3.util._
 import chisel3.experimental._
 
 object PcOpcode extends ChiselEnum {
-  val init = Value(0.U)
   val next = Value(1.U)
   val jump = Value(2.U)
 }
@@ -16,12 +15,12 @@ class PCUnit extends Module {
   })
 
     val npc_mux_out = MuxCase("h80000000".U(64.W),
-      Seq((io.npc_op === PcOpcode.init) -> "h80000000".U(64.W),
-          (io.npc_op === PcOpcode.next) -> 4.U(64.W),
+      Seq((io.npc_op === PcOpcode.next) -> 4.U(64.W),
           (io.npc_op === PcOpcode.jump) -> io.offset))
 
     val pc = RegInit("h80000000".U(64.W))
-    pc := npc_mux_out
+
+    pc := pc + npc_mux_out
     io.pc <> pc
 
 }
