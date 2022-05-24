75540bcb23a61f7004a5996cfe5906c1af94c630
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-24 21:37:31 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:37:31 up  1:46,  1 user,  load average: 2.31, 1.40, 0.88

--- npc/playground/src/MEMUnit.scala
+++ npc/playground/src/MEMUnit.scala
@@ -10,13 +10,9 @@ class MemInf extends Bundle {
   val wmask   =   Input (UInt(8.W))
 }
 
-class DPICMem extends BlackBox with HasBlackBoxResource {
-  val io = IO(new Bundle {
-    val in1 = Input(UInt(64.W))
-    val in2 = Input(UInt(64.W))
-    val out = Output(UInt(64.W))
-  })
-  addResource("/dpic_memory.v")
+class DPICMem extends Module{//BlackBox with HasBlackBoxResource {
+  val io = IO(new MemInf)
+  //addResource("/dpic_memory.v")
 }
 
 
