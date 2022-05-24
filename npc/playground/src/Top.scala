7fc5c137e98e6e9566c8cb3c1f7f3eb06c4550a8
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-24 15:24:15 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:24:15 up  1:14,  1 user,  load average: 0.57, 0.87, 0.99

--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -12,14 +12,8 @@ import chisel3._
 class Top extends Module {
   val io = IO(new Bundle {
     val inst_i = Input(UInt(64.W))
-    val test_o = Output(UInt(64.W))
+
   })
-
-  val test_reg = RegInit(PcOpcode.next)
-  test_reg := PcOpcode(test_reg.asUInt() + 1.U)
   val pc_unit = Module(new PCUnit)
-  pc_unit.io.offset := io.inst_i
-  pc_unit.io.npc_op := test_reg//PcOpcode.init
-
-  io.test_o <> pc_unit.io.pc
+  io.inst_i := DontCare
 }
