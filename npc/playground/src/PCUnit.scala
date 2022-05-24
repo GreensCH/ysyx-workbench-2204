8975dad6dab798742560c4196e09972646c51ed3
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-24 10:47:58 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:47:58 up 7 min,  1 user,  load average: 0.31, 0.47, 0.30

--- npc/playground/src/PCUnit.scala
+++ npc/playground/src/PCUnit.scala
@@ -5,14 +5,19 @@ import chisel3.experimental._
 
 class PCUnit extends Module {
   val io = IO(new Bundle {
-    val i_test = Input(Bool())
-    val o_test = Output(Bool())
-    // val npc_i   = Input(UInt(64.W))
-    // val npcop_i = Input(UInt( 4.W))
-    // val pc_o    = Input(UInt(64.W))
+    val npc_i   = Input(UInt(64.W))
+    val npcop_i = Input(UInt( 4.W))
+    val pc_o    = Input(UInt(64.W))
   })  
     io.o_test <> io.i_test;
-
+    val mux_out = Mux1H(Seq(
+    io.npcop_i(0) -> 2.U,
+    io.npcop_i(1) -> 4.U,
+    io.npcop_i(2) -> 8.U,
+    io.npcop_i(4) -> 11.U
+))
+    val pc = RegInit("h80000000".U(64.W))
+    pc := mux_out
 //    pc := io.i_test//0x1.U(64.W)
 //    io.i_test := pc(0)
 //    pc := io.npc_i
