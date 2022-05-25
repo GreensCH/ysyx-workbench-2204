3d68669daea0d8b23d38772caa1878d2888a1222
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-25 15:51:27 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:51:27 up  2:53,  1 user,  load average: 0.56, 0.68, 0.61

--- npc/playground/src/Controller.scala
+++ npc/playground/src/Controller.scala
@@ -1,12 +1,25 @@
 import chisel3._
+import chisel3.util._
+import chisel3.experimental._
+
+object Opcode extends ChiselEnum {
+  val R, I, S, B, U, J = Value
+}
 
 class Controller extends Module{
   val io = IO(new Bundle {
-    val inst_i    =   Input (UInt(32.W))
-    val instfmt_o =   Output(UInt(6 .W))
+    val inst_i        =   Input (UInt(32.W))
+    val inst_fmt_o    =   Output(UInt(6 .W))
+    val inst_opcode_o =   Output(UInt(6 .W))
     val exop_o    =   Output(UInt(64.W))
     val m2r_ctrl_o =   Output(Bool())
     val regfile_wen_o = Output(Bool())
   })
+  val optype = io.inst_i(6, 0)
+  val inst_fmt_o = MuxCase(default = R,
+    Seq((
+     optype ===  
+    ))
+  )
 
 }
