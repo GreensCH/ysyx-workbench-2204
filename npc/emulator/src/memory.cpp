a06d1eadb6357e570c29e4b2e968342101cc3b5b
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-25 11:52:48 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:52:48 up 14 min,  1 user,  load average: 0.68, 0.79, 0.59

--- npc/emulator/src/memory.cpp
+++ npc/emulator/src/memory.cpp
@@ -7,8 +7,8 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 extern "C" word_t pmem_read(paddr_t addr, int len) {
-  printf("guest addr:%lx, host addr %lx \n",0x80000000, guest_to_host(addr));
-  word_t ret = host_read(guest_to_host(0x80000000), len);
+  printf("guest addr:%lx, host addr %lx \n",addr, guest_to_host(addr));
+  word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
 
