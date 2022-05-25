4e4f778ba618f6c3c98d8f0c7a6e1648f8c88a7a
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-25 10:49:26 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:49:26 up 35 min,  1 user,  load average: 1.30, 0.93, 0.80

--- npc/emulator/src/main.cpp
+++ npc/emulator/src/main.cpp
@@ -37,19 +37,19 @@ int main(int argc, char** argv, char** env) {
 //     Log("No image is given. Use the default build-in image.");
 //     return 4096; // built-in image size
 //   }
-
+//
 //   FILE *fp = fopen(img_file, "rb");
 //   Assert(fp, "Can not open '%s'", img_file);
-
+//
 //   fseek(fp, 0, SEEK_END);
 //   long size = ftell(fp);
-
+//
 //   Log("The image is %s, size = %ld", img_file, size);
-
+//
 //   fseek(fp, 0, SEEK_SET);
 //   int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
 //   assert(ret == 1);
-
+//
 //   fclose(fp);
 //   return size;
 // }
