5ea4ec856ec3946c1a5e0a1bcc58e47734ff22ca
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 14:24:52 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:24:52 up  3:03,  1 user,  load average: 0.70, 0.86, 0.78

--- npc/playground/src/PC.scala
+++ npc/playground/src/PC.scala
@@ -3,7 +3,6 @@ import chisel3.util._
 
 class PC extends Module {
   val io = IO(new Bundle {
-    val enable = Input(Bool())
     val is_jump = Input (Bool())
     val offset  = Input(UInt(64.W))
     val pc      = Output(UInt(64.W))
@@ -13,13 +12,14 @@ class PC extends Module {
   /* interface */
   val is_jump = io.is_jump
   val offset = io.offset
-  val enable = io.enable
-
+  /* instance */
   val npc = Wire(UInt(64.W))
   val npc_mux_out = Mux(is_jump, offset, 4.U(64.W))
-  val pc_reg = RegNext(next = npc, init = "h80000000".U(64.W))
+  val pc_reg = RegInit(init = "h80000000".U(64.W))
   printf(p"NPC-PCU@pc_reg:${Hexadecimal(pc_reg)}, ")
   npc := pc_reg + 4.U(64.W)
+  pc_reg := npc
+  /* monitor interface */
   io.pc := pc_reg
   io.npc:= npc
   printf(p"npc:${Hexadecimal(npc)}, is_jump:${io.is_jump}, mux_out:${Hexadecimal(npc_mux_out)}, off:${Hexadecimal(io.offset)}\n")
