5cd9ecbdfcb098253a452f62c7ca147388c82145
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 11:26:58 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:26:58 up 5 min,  1 user,  load average: 2.40, 1.47, 0.69

--- npc/playground/src/Ebreak.scala
+++ npc/playground/src/Ebreak.scala
@@ -7,16 +7,18 @@
    val io = IO(new Bundle {
      var valid = Input(UInt(1.W))
    } )
-     setInline("Ebreak.v",
-     s"""
-     | import \"DPI-C\" function void ebreak();
-     | module Ebreak (valid);
-     | input valid;
-     | always@(*)
-     | begin
-     |   if(valid)
-     |     ;//ebreak();
-     | end
-     | endmodule
-     """.stripMargin)
+
+    val valid = io.valid
+//     setInline("Ebreak.v",
+//     s"""
+//     | import \"DPI-C\" function void ebreak();
+//     | module Ebreak (valid);
+//     | input valid;
+//     | always@(*)
+//     | begin
+//     |   if(valid)
+//     |     ebreak();
+//     | end
+//     | endmodule
+//     """.stripMargin)
  }
