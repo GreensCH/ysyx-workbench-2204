0fd6b71e13f614659092eed631d32322e8727184
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 12:20:29 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:20:29 up 59 min,  1 user,  load average: 0.97, 1.20, 1.14

--- npc/playground/resources/dpic_memory.v
+++ npc/playground/resources/dpic_memory.v
@@ -1,6 +1,7 @@
 import "DPI-C" function longint pmem_read(input longint addr, input int len);
-import "DPI-C" context function void pmem_write(input longint addr, input longint wdata, input byte wmask);
-
+// import "DPI-C" function void pmem_write(input longint addr, input longint wdata, input byte wmask);
+import "DPI-C" function void pmem_write(input longint addr, input int len, input longint data);
+// void  pmem_write(paddr_t addr, int len, word_t data)
 
 module dpic_memory (
     input               rd_en,
@@ -15,10 +16,16 @@ module dpic_memory (
   // assign rd_data = rd_en ? 'h0 : pmem_read(rd_addr, 8);
   assign rd_data = pmem_read(rd_addr, 8);
   always @(*) begin
-//    $display("rd_en:%d\n",rd_en);
-    if(we_en) pmem_write(we_addr, we_data, we_mask);
+    $display("we_addr:0x%lx we_data:0x%lx we_mask:0x%lx\n",we_addr, we_data, we_mask);
+    if(we_en)begin
+      case(we_mask)
+        8'b0000_0001: pmem_write(we_addr, 1, we_data);
+        8'b0000_0011: pmem_write(we_addr, 2, we_data);
+        8'b0000_1111: pmem_write(we_addr, 4, we_data);
+        8'b1111_1111: pmem_write(we_addr, 8, we_data);
+      endcase
+    end
   end
-
 endmodule
 
 
