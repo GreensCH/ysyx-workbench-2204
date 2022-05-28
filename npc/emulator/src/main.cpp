c3cf323d6c43a2ed573531ca72c1c5994538f7b0
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 18:55:53 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:55:53 up  7:34,  1 user,  load average: 0.69, 0.71, 0.67

--- npc/emulator/src/main.cpp
+++ npc/emulator/src/main.cpp
@@ -1,6 +1,47 @@
 #include "include.h"
 
 
+
+
+
+
+// static int parse_args(int argc, char *argv[]) {
+//   const struct option table[] = {
+//     {"batch"    , no_argument      , NULL, 'b'},
+//     {"log"      , required_argument, NULL, 'l'},
+//     {"diff"     , required_argument, NULL, 'd'},
+//     {"port"     , required_argument, NULL, 'p'},
+//     {"help"     , no_argument      , NULL, 'h'},
+//     {"elf"      , required_argument, NULL, 'e'},
+//     {0          , 0                , NULL,  0 },
+//   };
+//   int o;
+//   while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
+//     switch (o) {
+//       case 'b': sdb_set_batch_mode(); break;
+//       case 'p': sscanf(optarg, "%d", &difftest_port); break;
+//       case 'l': log_file = optarg; break;
+//       case 'd': diff_so_file = optarg; break;
+//       case 'e': elf_file = optarg; break;
+//       case 1: img_file = optarg; return 0;
+//       default:
+//         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+//         printf("\t-b,--batch              run with batch mode\n");
+//         printf("\t-l,--log=FILE           output log to FILE\n");
+//         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+//         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+//         printf("\t-e,--elf=FILE           input elf\n");
+//         printf("\n");
+//         exit(0);
+//     }
+//   }
+//   return 0;
+// }
+
+
+
+
+
 static const uint32_t img [] = {
   0x800002b7,  // lui t0,0x80000 0
   0x0002a023,  // sw  zero,0(t0) 
@@ -17,6 +58,9 @@ void init_isa() {
 }
 
 int main(int argc, char *argv[], char** env) {
+    int  i; 
+    for(i = 0; i < argc; i++) 
+      std::cout<<argv[i]<<std::endl;
     sim_init(argc,argv);
     init_isa();
     reset(1);
@@ -32,25 +76,4 @@ int main(int argc, char *argv[], char** env) {
     return 0;
 }
 
-static long load_img() {
-  if (img_file == NULL) {
-    Log("No image is given. Use the default build-in image.");
-    return 4096; // built-in image size
-  }
-
-  FILE *fp = fopen(img_file, "rb");
-  Assert(fp, "Can not open '%s'", img_file);
-
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
-
-  Log("The image is %s, size = %ld", img_file, size);
-
-  fseek(fp, 0, SEEK_SET);
-  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
-  assert(ret == 1);
-
-  fclose(fp);
-  return size;
-}
 
