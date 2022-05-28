af7098498dc004636b63229f505ef90de7260b41
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 16:36:34 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:36:34 up  5:15,  1 user,  load average: 0.85, 1.08, 1.04

--- npc/emulator/src/memory.cpp
+++ npc/emulator/src/memory.cpp
@@ -18,7 +18,7 @@ extern "C" word_t pmem_read(paddr_t addr, int len) {
 }
 
 extern "C" void  pmem_write(paddr_t addr, int len, word_t data) {
-//  printf("VLT@WRITE addr0x%016lx, len:%d ,data0x%016lx\t",addr, len, data);
+ printf("VLT@WRITE addr0x%016lx, len:%d ,data0x%016lx\t",addr, len, data);
   if(addr < 0x80000000){
     // printf("write fail\n");
     return;
--- npc/playground/src/Util.scala
+++ npc/playground/src/Util.scala
@@ -4,12 +4,7 @@ import chisel3.util._
 object Util {
 
 }
-//def SETX(a:UInt, b:Int):UInt = Cat(Fill(64-b, a(b-1)) ,a(b-1,0))(63,0);
 
 object  Sext{
-  def apply (data:UInt, pos:Int) : UInt = {
-//     printf(p"NPC@data:${Hexadecimal(data)}, pos:$pos\n")
-    Cat(Fill(128 - pos, data(pos - 1)), data(pos - 1, 0))//(data.getWidth - 1, 0)
-//    "hffff_ffff".U(64.W)
-  }
+  def apply (data:UInt, pos:Int) : UInt = Cat(Fill(128 - pos, data(pos - 1)), data(pos - 1, 0))//(data.getWidth - 1, 0)
 }
