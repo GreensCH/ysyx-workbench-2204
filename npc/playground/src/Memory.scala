686ae0ae9b68e6e3c037adb8d1087e72f689e6bd
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 16:06:36 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:06:36 up  4:45,  1 user,  load average: 0.56, 0.67, 0.78

--- npc/playground/src/Memory.scala
+++ npc/playground/src/Memory.scala
@@ -33,17 +33,13 @@ class MemoryInf extends Module{
   val io = IO(new MemoryIO)
   val m = Module(new dpic_memory)
 //  printf("NPC@Memory\n")
-  when(io.rd_en){
     m.io.rd_en := io.rd_en
     m.io.rd_addr := io.rd_addr
     io.rd_data := io.rd_data
     printf(p"NPC@rd_addr=0x${Hexadecimal(io.rd_addr)}, rd_data=0x${Hexadecimal(io.rd_data)}, rd_en=${Binary(io.rd_en)}\n")
-  }
-  when(io.we_en){
     m.io.we_en := io.we_en
     m.io.we_addr := io.we_addr
     m.io.we_data := io.we_data
     m.io.we_mask := io.we_mask
     printf(p"NPC@we_addr=0x${Hexadecimal(io.we_addr)}, we_data=0x${Hexadecimal(io.we_data)}, we_mask=${Binary(io.we_mask)}\n")
-  }
 }
