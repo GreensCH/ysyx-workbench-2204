a7df75a4f174903bb40956141812660b3463487f
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 12:40:52 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:40:52 up  1:19,  1 user,  load average: 0.74, 1.15, 1.03

--- npc/playground/src/Util.scala
+++ npc/playground/src/Util.scala
@@ -2,5 +2,5 @@ import chisel3._
 import chisel3.util._
 
 object Util {
-  def sext(data:UInt, pos:Int) : UInt = 0.U(64.W)//Cat(Fill(64 - pos, data(pos - 1)), data(pos - 1, 0))(63, 0)
+  def sext(data:UInt, pos:Int) : UInt = Cat(Fill(64 - pos, data(pos - 1)), data(pos - 1, 0))(63, 0)
 }
