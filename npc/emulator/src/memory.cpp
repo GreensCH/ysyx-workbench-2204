37933d91fd631f55530973fb5a7c5c95b37c1eb2
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 16:20:38 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:20:38 up  4:59,  1 user,  load average: 0.57, 0.82, 0.91

--- npc/emulator/src/memory.cpp
+++ npc/emulator/src/memory.cpp
@@ -7,20 +7,22 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 extern "C" word_t pmem_read(paddr_t addr, int len) {
-  printf("VLT@READ addr:0x%016lx, len:%d\n",addr, len);
+  printf("VLT@READ addr:0x%016lx, len:%d\t",addr, len);
   if(addr < 0x80000000){
-    printf("VLT@read fail\n");
+    printf("read fail\n");
     return 0;
   }
+  printf("\n");
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
 
 extern "C" void  pmem_write(paddr_t addr, int len, word_t data) {
- printf("VLT@WRITE addr0x%016lx, len:%d ,data0x%016lx\n",addr, len, data);
+ printf("VLT@WRITE addr0x%016lx, len:%d ,data0x%016lx\t",addr, len, data);
   if(addr < 0x80000000){
-    printf("VLT@write fail\n");
+    printf("write fail\n");
     return;
   }
+  printf("\n");
   host_write(guest_to_host(addr), len, data);
 }
