dce20fd6a63dcfe5a740269b6fa6620be20e885d
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 13:26:14 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:26:14 up  2:04,  1 user,  load average: 0.97, 1.22, 1.29

--- npc/playground/src/Sext.scala
+++ npc/playground/src/Sext.scala
@@ -11,7 +11,7 @@ object  Sext{
     // return 0.U(64.W)
     val result = Cat(Fill(128, data(pos - 1)), data(pos - 1, 0))
     printf(s"NPC@${data.getWidth} \n")
-    "h1000".U(64.W)
+    "hffff_ffff".U(64.W)
 //    result(result.getWidth - 1, 0)
   }
 }
