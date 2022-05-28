aca871fd0c36b30e040e55512c42f51ab1f3e402
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 15:40:21 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:40:21 up  4:19,  1 user,  load average: 1.78, 1.28, 1.02

--- npc/playground/src/Memory.scala
+++ npc/playground/src/Memory.scala
@@ -22,19 +22,10 @@ class MemoryIO extends Bundle{
   val we_addr  =   Input (UInt(64.W))
   val we_data  =   Input (UInt(64.W))
   val we_mask  =   Input (UInt(8.W))
-  val id = Input(UInt(8.W))
 }
 
-class dpic_memory extends Module with HasBlackBoxResource {
-  val io = IO(new Bundle() {
-      val rd_en    =   Input (Bool())
-      val rd_addr  =   Input (UInt(64.W))
-      val rd_data  =   Output(UInt(64.W))
-      val we_en    =   Input (Bool())
-      val we_addr  =   Input (UInt(64.W))
-      val we_data  =   Input (UInt(64.W))
-      val we_mask  =   Input (UInt(8.W))
-  })
+class dpic_memory extends BlackBox with HasBlackBoxResource {
+  val io = IO(new MemoryIO)
   addResource("/dpic_memory.v")
 }
 
@@ -42,16 +33,8 @@ class MemoryInf extends Module{
   val io = IO(new MemoryIO)
   val m = Module(new dpic_memory)
 //  printf("NPC@Memory\n")
-//  m.io <> io
-  printf(p"NPC@id${io.id}, rd_addr=0x${Hexadecimal(io.rd_addr)}, rd_data=0x${Hexadecimal(io.rd_data)}, rd_en=${Binary(io.rd_en)}\n")
-
-  m.io.rd_en   := io.rd_en
-  m.io.rd_addr := io.rd_addr
-  io.rd_data := m.io.rd_data
-  m.io.we_en   := io.we_en
-  m.io.we_addr := io.we_addr
-  m.io.we_data := io.we_data
-  m.io.we_mask := io.we_mask
+  m.io <> io
+  printf(p"NPC@rd_addr=0x${Hexadecimal(io.rd_addr)}, rd_data=0x${Hexadecimal(io.rd_data)}, rd_en=${Binary(io.rd_en)}\n")
   when(io.we_en){
     printf(p"NPC@we_addr=0x${Hexadecimal(io.we_addr)}, we_data=0x${Hexadecimal(io.we_data)}, we_mask=${Binary(io.we_mask)}\n")
   }
