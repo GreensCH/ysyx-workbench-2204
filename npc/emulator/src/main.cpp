b246d190aa953a667c8304fed56a9e3b6e06891a
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 12:10:32 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:10:32 up 49 min,  1 user,  load average: 0.58, 0.90, 1.02

--- npc/emulator/src/main.cpp
+++ npc/emulator/src/main.cpp
@@ -38,13 +38,13 @@ int main(int argc, char** argv, char** env) {
     sim_init(argc,argv);
     init_isa();
     step_and_dump_wave();
-    printf( "\033[1;34;40m""start simulating\n""\033[0m" );
+    printf("start simulating\n");
     while (!contextp->gotFinish()){ 
       step_and_dump_wave();
       // printf("@vlt:finish=%d\n",contextp->gotFinish());
       // printf("pc: 0x%lx  inst: 0x%lx\n",(word_t)top->io_pc , (word_t)top->io_inst);
     }
-    printf( "\033[1;34;40m""quiting verilator\n""\033[0m" );
+    printf( "quiting verilator\n");
     sim_exit();
     return 0;
   }
