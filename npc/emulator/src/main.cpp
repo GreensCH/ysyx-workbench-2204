155bcd4df2a723b7c4e17b2ad7cd11667b8435ac
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 21:55:54 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:55:54 up  1:40,  1 user,  load average: 0.89, 0.93, 0.89

--- npc/emulator/src/main.cpp
+++ npc/emulator/src/main.cpp
@@ -6,42 +6,42 @@ static char *img_file = NULL;
 static char *elf_file = NULL;
 static int difftest_port = 1234;
 
-#include <getopt.h>
-static int parse_args(int argc, char *argv[]) {
-  std::cout<<"argc:"<<argc<<std::endl;
-  for(int i = 0; i < argc; i++) 
-    std::cout<<argv[i]<<std::endl;
-  const struct option table[] = {
-    {"batch"    , no_argument      , NULL, 'b'},
-    {"log"      , required_argument, NULL, 'l'},
-    {"diff"     , required_argument, NULL, 'd'},
-    {"port"     , required_argument, NULL, 'p'},
-    {"help"     , no_argument      , NULL, 'h'},
-    {"elf"      , required_argument, NULL, 'e'},
-    {0          , 0                , NULL,  0 },
-  };
-  int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
-    switch (o) {
-      // case 'b': sdb_set_batch_mode(); break;
-      // case 'p': sscanf(optarg, "%d", &difftest_port); break;
-      case 'l': log_file = optarg; break;
-      // case 'd': diff_so_file = optarg; break;
-      case 'e': elf_file = optarg; break;
-      case 1: img_file = optarg; return 0;
-      default:
-        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
-        printf("\t-b,--batch              run with batch mode\n");
-        printf("\t-l,--log=FILE           output log to FILE\n");
-        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
-        printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
-        printf("\t-e,--elf=FILE           input elf\n");
-        printf("\n");
-        exit(0);
-    }
-  }
-  return 0;
-}
+// #include <getopt.h>
+// static int parse_args(int argc, char *argv[]) {
+//   std::cout<<"argc:"<<argc<<std::endl;
+//   for(int i = 0; i < argc; i++) 
+//     std::cout<<argv[i]<<std::endl;
+//   const struct option table[] = {
+//     {"batch"    , no_argument      , NULL, 'b'},
+//     {"log"      , required_argument, NULL, 'l'},
+//     {"diff"     , required_argument, NULL, 'd'},
+//     {"port"     , required_argument, NULL, 'p'},
+//     {"help"     , no_argument      , NULL, 'h'},
+//     {"elf"      , required_argument, NULL, 'e'},
+//     {0          , 0                , NULL,  0 },
+//   };
+//   int o;
+//   while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
+//     switch (o) {
+//       // case 'b': sdb_set_batch_mode(); break;
+//       // case 'p': sscanf(optarg, "%d", &difftest_port); break;
+//       case 'l': log_file = optarg; break;
+//       // case 'd': diff_so_file = optarg; break;
+//       case 'e': elf_file = optarg; break;
+//       case 1: img_file = optarg; return 0;
+//       default:
+//         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+//         printf("\t-b,--batch              run with batch mode\n");
+//         printf("\t-l,--log=FILE           output log to FILE\n");
+//         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+//         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+//         printf("\t-e,--elf=FILE           input elf\n");
+//         printf("\n");
+//         exit(0);
+//     }
+//   }
+//   return 0;
+// }
 
 
 
@@ -61,9 +61,11 @@ void init_isa() {
   memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
 }
 
+void init_monitor(int, char *[]);
+
 int main(int argc, char *argv[], char** env) {
 
-    parse_args(argc, argv);
+    init_monitor(argc, argv);
     printf("log_file:%s\n",log_file);
     printf("elf_file:%s\n",elf_file);
     printf("img_file:%s\n",img_file);
