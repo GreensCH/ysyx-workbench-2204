f24fb0ced1684b02994de4d74c19816fc4cd6098
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 15:15:36 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:15:36 up  3:54,  1 user,  load average: 1.15, 1.03, 0.87

--- npc/emulator/src/main.cpp
+++ npc/emulator/src/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv, char** env) {
     step_and_dump_wave();
     top->reset = 0;
     printf("start npc\n");
-    while (/*!contextp->gotFinish()*/sc_time_stamp()<10){ 
+    while (!contextp->gotFinish() && sc_time_stamp()<10){ 
       step_and_dump_wave();
       // printf("@vlt:finish=%d\n",contextp->gotFinish());
       // printf("pc: 0x%lx  inst: 0x%lx\n",(word_t)top->io_pc , (word_t)top->io_inst);
