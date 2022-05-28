6ff2e8a7dfefc79db6cb2153c89d5d741271704e
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 13:29:38 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:29:38 up  2:08,  1 user,  load average: 1.24, 1.43, 1.37

--- npc/playground/src/Sext.scala
+++ npc/playground/src/Sext.scala
@@ -7,10 +7,9 @@ object Util {
 //def SETX(a:UInt, b:Int):UInt = Cat(Fill(64-b, a(b-1)) ,a(b-1,0))(63,0);
 object  Sext{
   def apply (data:UInt, pos:Int) : UInt = {
-    // printf(s"NPC@data:$data, pos:$pos\n")
+     printf(s"NPC@data:$data, pos:$pos\n")
     // return 0.U(64.W)
     val result = Cat(Fill(128, data(pos - 1)), data(pos - 1, 0))
-    printf(s"NPC@${data.getWidth} \n")
     "hf0ff_ffff".U(64.W)
 //    result(result.getWidth - 1, 0)
   }
