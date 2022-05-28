a689bbba695bfca60c0ce678471c333c7faab1c4
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 15:14:29 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:14:29 up  3:53,  1 user,  load average: 1.11, 0.95, 0.83

--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -11,7 +11,6 @@ class Top extends Module {
   val io = IO(new Bundle {
     val inst = Output(UInt(32.W))
     val pc = Output(UInt(64.W))
-    val npc = Output(UInt(64.W))
   })
   val regfile = Module(new RegFile)
 
@@ -43,7 +42,6 @@ class Top extends Module {
   /* monitor and top interface */
   io.inst := ifu.io.if2id.inst
   io.pc := ifu.io.if2id.pc
-  io.npc := ifu.io.npc
   val monitor = Module(new Monitor)
   monitor.io.pc := ifu.io.if2id.pc
   monitor.io.inst :=ifu.io.if2id.inst
