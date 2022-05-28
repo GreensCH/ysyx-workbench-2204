bf810efbcad7a94a8cfaaf5684768c85cba8c470
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 15:09:15 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:09:15 up  3:47,  1 user,  load average: 1.00, 0.76, 0.74

--- npc/playground/resources/dpic_memory.v
+++ npc/playground/resources/dpic_memory.v
@@ -13,8 +13,8 @@ module dpic_memory (
     input   [7  : 0]    we_mask
 );
 
-  // assign rd_data = rd_en ? 'h0 : pmem_read(rd_addr, 8);
-  assign rd_data = pmem_read(rd_addr, 8);
+  assign rd_data = rd_en ? 'h0 : pmem_read(rd_addr, 8);
+  //assign rd_data = pmem_read(rd_addr, 8);
   always @(*) begin
     // $display("addr:0x%lx data:0x%lx mask:0x%lx\n",we_addr, we_data, we_mask);
     if(we_en)begin
