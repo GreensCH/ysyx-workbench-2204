575fa84a135d5783fb1fe6a63fb76882c8491ad9
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 22:55:53 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:55:53 up 11:01,  1 user,  load average: 0.79, 0.93, 0.81

--- npc/playground/resources/dpic_difftest.v
+++ npc/playground/resources/dpic_difftest.v
@@ -1,9 +1,9 @@
 import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
 
 module dpic_difftest (
-    input   [63 : 0]    gpr,
+    input   [63 : 0]    rf,
     input               clk,
     input               rst
 );
-  initial set_gpr_ptr(gpr);  // rf为通用寄存器的二维数组变量
+  initial set_gpr_ptr(rf);  // rf为通用寄存器的二维数组变量
 endmodule
