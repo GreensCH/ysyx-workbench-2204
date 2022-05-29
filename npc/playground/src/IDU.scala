fc1b06cacfd825f93f345aef68ac56d9e69b2fc6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 19:20:40 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:20:40 up  7:26,  1 user,  load average: 0.37, 0.61, 0.88

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -110,7 +110,7 @@ class IDU extends Module {
       b_jump -> Sext(data = Cat(inst(31), inst(7), inst(30, 25), inst(11, 8), 0.U), pos = 13)
     )
   )
-  io.id2pc.jump_reg := Cat(io.id2ex.src1 + io.id2ex.src2, 0.U(1.W))(63, 0)
+  io.id2pc.jump_reg := "h80000000".U//Cat(io.id2ex.src1 + io.id2ex.src2, 0.U(1.W))(63, 0)
 }
 
 
