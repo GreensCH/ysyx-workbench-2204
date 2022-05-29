a72f4b4ad7ff2b18e83ff705a14b291e116b5e23
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 15:37:57 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:37:57 up  3:43,  1 user,  load average: 0.82, 0.60, 0.56

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -101,7 +101,6 @@ class IDU extends Module {
   val bgeu_jump = operator.bgeu & (reg_src1 >= reg_src2)
   val b_jump = beq_jump | bne_jump | blt_jump | bge_jump | bltu_jump | bgeu_jump
   io.id2pc.is_jump := b_jump | operator.jal | operator.jalr
-  printf(p"jal${operator.jal} jalr${operator.jalr}")
   io.id2pc.offset := MuxCase(0.U(64.W),
     Array(
       (operator.jal | operator.jalr) -> (reg_src1 + Sext(data = Cat(inst(31), inst(19, 12), inst(20), inst(30, 21), 0.U), pos = 21)),
