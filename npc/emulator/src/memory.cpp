61172f4f89660575c34f6d4f4e3565c66071b560
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 13:07:08 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:07:08 up  1:13,  1 user,  load average: 1.13, 1.10, 1.05

--- npc/emulator/src/memory.cpp
+++ npc/emulator/src/memory.cpp
@@ -15,21 +15,21 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 extern "C" word_t pmem_read(paddr_t addr, int len) {
-  printf("\33[1;34mVLT\tREAD addr:0x%016lx, len:%d \33[0m \n" ,addr, len);
+  // printf("\33[1;34mVLT\tREAD addr:0x%016lx, len:%d \33[0m \n" ,addr, len);
 
   if(addr < 0x8000000){
     // printf("read fail\n");
     return 0;
   }
-  if(addr > 0x90000000)
-    return 0;
+  // if(addr > 0x90000000)
+  //   return 0;
   // printf("\n");
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
 
 extern "C" void  pmem_write(paddr_t addr, int len, word_t data) {
-  printf("\33[1;34mVLT\tWRITE addr0x%016lx, len:%d ,data0x%016lx \33[0m \n" ,addr, len, data);
+  // printf("\33[1;34mVLT\tWRITE addr0x%016lx, len:%d ,data0x%016lx \33[0m \n" ,addr, len, data);
   if(addr < 0x80000000){
     // printf("write fail\n");
     return;
