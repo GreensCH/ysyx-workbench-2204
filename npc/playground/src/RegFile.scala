20b8c4fd6b224c4caa61fa5cf3f41ad52dc5fb99
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 19:07:09 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:07:09 up  7:13,  1 user,  load average: 1.17, 1.11, 1.04

--- npc/playground/src/RegFile.scala
+++ npc/playground/src/RegFile.scala
@@ -21,7 +21,7 @@ class RegFile extends Module{
     val idu = new RegFileID // Write Back Unit interface
   })
 
-  val gpr = RegInit(Vec(32, 0.U(64.W)))
+  val gpr = RegInit(VecInit(Seq.fill(32)(0.U(64.W))))
   io.idu.data1 := gpr(io.idu.addr1 & Fill(64, io.idu.en))
   io.idu.data2 := gpr(io.idu.addr2 & Fill(64, io.idu.en))
   gpr(io.wbu.addr & Fill(64, io.wbu.en)) := (io.wbu.data & Fill(64, io.wbu.en))
