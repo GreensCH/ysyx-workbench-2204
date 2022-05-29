7aece943b4662bac7c2528082b8bdc52496b9537
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 22:49:52 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:49:52 up 10:55,  1 user,  load average: 0.97, 0.71, 0.67

--- npc/playground/src/DiffTest.scala
+++ npc/playground/src/DiffTest.scala
@@ -6,7 +6,7 @@ class dpic_difftest extends BlackBox with HasBlackBoxResource {
   val io = IO(new Bundle {
     val clk = Input(Clock())
     val rst = Input(Reset())
-    val rf = Input(Vec(32, UInt(64.W)))
+    val gpr = Input(Vec(32, UInt(64.W)))
   })
   addResource("/dpic_difftest.v")
 }
@@ -14,11 +14,11 @@ class dpic_difftest extends BlackBox with HasBlackBoxResource {
 
 class DiffTest extends Module{
   val io = IO(new Bundle {
-    val rf = Input(Vec(32, UInt(64.W)))
+    val gpr = Input(Vec(32, UInt(64.W)))
   } )
 
   val dpic_difftest = Module(new dpic_difftest)
-  dpic_difftest.io.rf := io.rf
+  dpic_difftest.io.gpr := io.gpr
   dpic_difftest.io.clk := clock
   dpic_difftest.io.rst := reset
 }
