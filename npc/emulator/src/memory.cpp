48a0e58e2fb567aa3f0dbc3cd474fdc6b3436a5a
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 12:55:54 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:55:54 up  1:01,  1 user,  load average: 0.97, 1.07, 1.04

--- npc/emulator/src/memory.cpp
+++ npc/emulator/src/memory.cpp
@@ -16,6 +16,7 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 extern "C" word_t pmem_read(paddr_t addr, int len) {
   printf("\33[1;34m VLT\tREAD addr:0x%016lx, len:%d \33[0m \n" ,addr, len);
+
   if(addr < 0x8000000){
     // printf("read fail\n");
     return 0;
@@ -28,7 +29,7 @@ extern "C" word_t pmem_read(paddr_t addr, int len) {
 }
 
 extern "C" void  pmem_write(paddr_t addr, int len, word_t data) {
-//  _Log(ASNI_FMT("VLT\tWRITE addr0x%016lx, len:%d ,data0x%016lx\n", ASNI_FG_BLUE ASNI_NONE),addr, len, data );
+  printf("\33[1;34m VLT\tWRITE addr0x%016lx, len:%d ,data0x%016lx \33[0m \n" ,addr, len, data);
   if(addr < 0x80000000){
     // printf("write fail\n");
     return;
