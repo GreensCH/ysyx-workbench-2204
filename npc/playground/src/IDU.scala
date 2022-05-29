942212462197f3fbfc04738b3d48efcfc16ddd1e
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 16:09:09 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:09:09 up  4:15,  1 user,  load average: 0.86, 0.95, 0.92

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -103,7 +103,7 @@ class IDU extends Module {
   io.id2pc.is_jump := b_jump | operator.jal | operator.jalr
   io.id2pc.offset := MuxCase(0.U(64.W),
     Array(//static word_t immJ(uint32_t i) { return SEXT(BITS(i, 31, 31), 1) << 20 | BITS(i, 19, 12) << 12 | BITS(i, 20, 20) << 11 | BITS(i, 30, 21) << 1 ; }
-      operator.jal -> (reg_src1 + Sext(data = (Cat(inst(31), inst(19, 12), inst(20), inst(30, 21))<<1).asUInt(), pos = 21)),
+      operator.jal -> (reg_src1 + Sext(data = Cat(inst(31), inst(19, 12), inst(20), inst(30, 25), inst(24, 21), 0.U(1.W)), pos = 21)),
       operator.jalr -> Cat(io.id2ex.src1 + io.id2ex.src2, 0.U(1.W))(63, 0),
       b_jump -> Sext(data = Cat(inst(31), inst(7), inst(30, 25), inst(11, 8), 0.U), pos = 13)
     )
