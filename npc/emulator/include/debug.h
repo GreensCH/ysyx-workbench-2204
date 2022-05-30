a255178d7dbbd56f5b12f3033ad5c372a056c7ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 18:12:02 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:12:02 up  6:09,  1 user,  load average: 1.34, 0.97, 1.15

--- npc/emulator/include/debug.h
+++ npc/emulator/include/debug.h
@@ -1,8 +1,3 @@
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
-
-#include "include.h"
-
 #define Log(format, ...) \
     _Log(ASNI_FMT("[%s:%d %s] " format, ASNI_FG_BLUE) "\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
@@ -22,5 +17,3 @@
 #define panic(format, ...) Assert(0, format, ## __VA_ARGS__)
 
 #define TODO() panic("please implement me")
-
-#endif
