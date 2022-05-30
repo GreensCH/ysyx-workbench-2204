fc9e04bad2be0bb77b13e9043c3ae3ad5c838668
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 18:07:38 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:07:38 up  6:05,  1 user,  load average: 0.89, 1.08, 1.26

--- npc/emulator/include/npc/npc.h
+++ npc/emulator/include/npc/npc.h
@@ -23,6 +23,10 @@ typedef struct Decode {
   IFDEF(CONFIG_ITRACE, char logbuf[128]);
 } Decode;
 
+// monitor
+extern char isa_logo[];
+void init_isa();
+
 // reg
 extern CPU_state cpu;
 extern uint64_t *cpu_gpr;
@@ -45,5 +49,24 @@ void cpu_exec(uint64_t n);
 
 // #define NEMUTRAP(thispc, code) set_npc_state(NEMU_END, thispc, code)
 // #define INV(thispc) invalid_inst(thispc)
+
+
+// memory
+enum { MMU_DIRECT, MMU_TRANSLATE, MMU_FAIL };
+enum { MEM_TYPE_IFETCH, MEM_TYPE_READ, MEM_TYPE_WRITE };
+enum { MEM_RET_OK, MEM_RET_FAIL, MEM_RET_CROSS_PAGE };
+#ifndef isa_mmu_check
+int isa_mmu_check(vaddr_t vaddr, int len, int type);
+#endif
+paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type);
+
+// interrupt/exception
+vaddr_t isa_raise_intr(word_t NO, vaddr_t epc);
+#define INTR_EMPTY ((word_t)-1)
+word_t isa_query_intr();
+
+// difftest
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc);
+void isa_difftest_attach();
 
 #endif
