bc5d0b9f31d753855f6b0c8449bda2672ac69a54
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 18:10:41 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:10:41 up  6:08,  1 user,  load average: 0.34, 0.81, 1.13

--- npc/emulator/include/npc/decode.h
+++ npc/emulator/include/npc/decode.h
@@ -2,7 +2,25 @@
 #define __DECODE_H__
 
 #include "npc.h"
-#include "debug.h"
+#define Log(format, ...) \
+    _Log(ASNI_FMT("[%s:%d %s] " format, ASNI_FG_BLUE) "\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
+#define Assert(cond, format, ...) \
+  do { \
+    if (!(cond)) { \
+      MUXDEF(CONFIG_TARGET_AM, printf(ASNI_FMT(format, ASNI_FG_RED) "\n", ## __VA_ARGS__), \
+        (fflush(stdout), fprintf(stderr, ASNI_FMT(format, ASNI_FG_RED) "\n", ##  __VA_ARGS__))); \
+      IFNDEF(CONFIG_TARGET_AM, extern FILE* log_fp; fflush(log_fp)); \
+      extern void assert_fail_msg(); \
+      assert_fail_msg(); \
+      assert(cond); \
+    } \
+  } while (0)
+
+#define panic(format, ...) Assert(0, format, ## __VA_ARGS__)
+
+#define TODO() panic("please implement me")
 
 // --- pattern matching mechanism ---
 __attribute__((always_inline))
