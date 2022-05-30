f4325a6f0cbb6df3eb55f2b472ac77b91d4c78be
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 23:08:25 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:08:25 up  2:58,  1 user,  load average: 0.66, 0.51, 0.68

--- npc/emulator/src/npc/npc.cpp
+++ npc/emulator/src/npc/npc.cpp
@@ -75,8 +75,8 @@ static void execute(uint64_t n) {
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
-    // trace_and_difftest(&s, cpu.pc);
-    // if (npc_state.state != NPC_RUNNING) break;
+    trace_and_difftest(&s, cpu.pc);
+    if (npc_state.state != NPC_RUNNING) break;
     // IFDEF(CONFIG_DEVICE, device_update());
   }
 }
