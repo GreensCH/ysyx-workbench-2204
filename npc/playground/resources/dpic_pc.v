4e8525baaf77f61c6586c3d4b392eb112362aa1a
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 19:30:29 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:30:29 up 13 min,  1 user,  load average: 1.16, 0.97, 0.77

--- npc/playground/resources/dpic_pc.v
+++ npc/playground/resources/dpic_pc.v
@@ -1,4 +1,4 @@
-import "DPI-C" function void get_pc(output longint pc,  output longint npc);
+import "DPI-C" function void set_pc(output longint pc,  output longint npc);
 
 module dpic_pc (
     output   [63 : 0]    pc,
@@ -8,6 +8,6 @@ module dpic_pc (
 );
 
   always @(*) begin  
-    get_pc(pc, npc);  // rf为通用寄存器的二维数组变量
+    set_pc(pc, npc);  // rf为通用寄存器的二维数组变量
   end
 endmodule
