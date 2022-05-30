80684f6306f5e939cb3cb91e466bf08738a86f66
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 16:05:38 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:05:38 up  4:03,  1 user,  load average: 0.73, 0.82, 0.98

--- npc/emulator/include/npc/reg.h
+++ npc/emulator/include/npc/reg.h
@@ -1,14 +1,14 @@
-#ifndef __RISCV32_REG_H__
-#define __RISCV32_REG_H__
+#ifndef __RISCV64_REG_H__
+#define __RISCV64_REG_H__
 
-#include <common.h>
+#include "common.h"
 
 static inline int check_reg_idx(int idx) {
   IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 32));
   return idx;
 }
 
-#define gpr(idx) cpu.pc[check_reg_idx(idx)]
+#define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
 
 static inline const char* reg_name(int idx, int width) {
   extern const char* regs[];
