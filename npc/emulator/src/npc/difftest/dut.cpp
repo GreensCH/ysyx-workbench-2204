7e99e3aed74c7781b70637e32d0562d7350f297e
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 18:13:36 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:13:36 up  6:11,  1 user,  load average: 1.20, 1.05, 1.17

--- npc/emulator/src/npc/difftest/dut.cpp
+++ npc/emulator/src/npc/difftest/dut.cpp
@@ -1,6 +1,6 @@
-#include <dlfcn.h>
-
 #include "include.h"
+
+#include <dlfcn.h>
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
--- npc/emulator/src/utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "include.h"
-
-
-void assert_fail_msg() {
-  printf("FAIL!\n");
-  // isa_reg_display();
-  // statistic();
-}
