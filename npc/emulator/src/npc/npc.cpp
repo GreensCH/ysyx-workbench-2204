7cc16c7913b1d6a3275f8144a75ea7aa1d3d5762
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 23:17:41 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:17:41 up  3:07,  1 user,  load average: 1.16, 0.76, 0.72

--- npc/emulator/src/npc/npc.cpp
+++ npc/emulator/src/npc/npc.cpp
@@ -21,13 +21,13 @@ IFDEF(CONFIG_ITRACE, void itrace_log();)
 IFDEF(CONFIG_FTRACE, void ftrace_log(Decode *_this, vaddr_t dnpc);)
 IFDEF(CONFIG_WATCHPOINT, bool wp_exec();)
 
-static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   add_itrace(_this->logbuf);
-  // IFDEF(CONFIG_ITRACE, add_itrace(_this->logbuf);)
-  // IFDEF(CONFIG_FTRACE, ftrace_log(_this, dnpc);)
-  // if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }//printf小于10条的命令
-  // IFDEF(CONFIG_WATCHPOINT, if(wp_exec()) npc_state.state = NPC_STOP;)
-  // IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+  IFDEF(CONFIG_ITRACE, add_itrace(_this->logbuf);)
+  IFDEF(CONFIG_FTRACE, ftrace_log(_this, dnpc);)
+  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }//printf小于10条的命令
+  IFDEF(CONFIG_WATCHPOINT, if(wp_exec()) npc_state.state = NPC_STOP;)
+  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 }
 
 int isa_exec_once(Decode *s) {
