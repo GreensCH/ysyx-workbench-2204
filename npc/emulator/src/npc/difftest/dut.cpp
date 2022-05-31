e8167db1522637e5fda80e7bae959ca8fcffbc4a
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-31 23:05:03 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:05:03 up 10:52,  1 user,  load average: 1.08, 1.09, 1.06

--- npc/emulator/src/npc/difftest/dut.cpp
+++ npc/emulator/src/npc/difftest/dut.cpp
@@ -78,16 +78,15 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
     npc_state.state = NPC_ABORT;
     npc_state.halt_pc = pc;
-      printf(ASNI_FG_RED "VLT Register List" ASNI_NONE "\n");
-      isa_reg_display();
       printf(ASNI_FG_RED "NPC Register List" ASNI_NONE "\n");
       dump_gpr();
+      printf(ASNI_FG_RED "VLT Register List" ASNI_NONE "\n");
+      common_reg_display(ref);
   }
 }
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   CPU_state ref_r;
-
   if (skip_dut_nr_inst > 0) {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     if (ref_r.pc == npc) {
