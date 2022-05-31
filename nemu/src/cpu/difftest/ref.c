197ba97f75e7e0154870acb8a80221c74bf900d5
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-31 17:17:19 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:17:19 up  5:41,  1 user,  load average: 0.33, 0.48, 0.36

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -9,14 +9,18 @@
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   printf("difftest_memcpy\n");
-  uint8_t *p = buf;
-  if(direction == DIFFTEST_TO_REF){
-    for (size_t i = 0; i < n; i++) {
-      *guest_to_host(addr) = p[i];
-    }
+  if (direction == DIFFTEST_TO_DUT)
+  {
+    buf = (void *)guest_to_host(addr);
   }
-  else{
-    assert(0);
+  else
+  {
+    Log("%x,%lx", addr, n);
+    char *buf_char = (char *)buf;
+    for (int i = 0; i < n; i++)
+    {
+      paddr_write(addr + i, 1, buf_char[i]);
+    }
   }
 }
 // `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
