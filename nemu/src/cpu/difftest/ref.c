104ceb34221b062885c16154bb1fd24307adfbd1
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-31 15:31:56 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:31:56 up  3:55,  1 user,  load average: 0.79, 0.77, 0.70

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -23,12 +23,14 @@ void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
 void difftest_regcpy(void *dut, bool direction) {
   riscv64_CPU_state* p = dut;
   if(direction == DIFFTEST_TO_DUT){
+    printf("DIFFTEST_TO_DUT\n");
     for(int i = 0; i < 32; i++){
       p->gpr[i] = cpu.gpr[i];
     }
     p->pc = cpu.pc;
   }
   else{
+    printf("DIFFTEST_TO_REF\n");
     for(int i = 0; i < 32; i++){
       cpu.gpr[i] = p->gpr[i];
     }
@@ -39,12 +41,8 @@ void difftest_regcpy(void *dut, bool direction) {
 void difftest_exec(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
-    if(cpu.pc < 0x8000000)
-      return;
     s.pc = cpu.pc;
     s.snpc = cpu.pc;
-    // printf("From NEMU:\n");
-    // isa_reg_display();
     isa_exec_once(&s);
     cpu.pc = s.dnpc;
   }
