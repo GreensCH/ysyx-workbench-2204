7ed1ad5ff6be62e85b13b28a092a7d1fb2288557
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-31 21:39:08 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:39:08 up  9:26,  1 user,  load average: 1.68, 1.44, 1.25

--- npc/emulator/src/npc/npc.cpp
+++ npc/emulator/src/npc/npc.cpp
@@ -66,12 +66,12 @@ static void exec_once(Decode *s, vaddr_t pc) {
   p += space_len;
 
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(p, s->logbuf + sizeof(s->logbuf) - p, s->pc, (uint8_t *)&s->isa.inst.val, ilen);
-  printf("pc:%016lx\n",s->pc);
-  printf("size:%ld\n",s->logbuf + sizeof(s->logbuf) - p);
-  printf("nbyte:%d\n",ilen);
-  printf("p:%s\n",p);
-  printf("logbuf:%s\n",s->logbuf);
+  // disassemble(p, s->logbuf + sizeof(s->logbuf) - p, s->pc, (uint8_t *)&s->isa.inst.val, ilen);
+  // printf("pc:%016lx\n",s->pc);
+  // printf("size:%ld\n",s->logbuf + sizeof(s->logbuf) - p);
+  // printf("nbyte:%d\n",ilen);
+  // printf("p:%s\n",p);
+  // printf("logbuf:%s\n",s->logbuf);
 #endif
 }
 
