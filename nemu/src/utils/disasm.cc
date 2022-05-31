b831eb82c106dc22afe7f13ba92958cdf78ddca1
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-31 22:46:25 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:46:25 up 10:34,  1 user,  load average: 0.87, 1.06, 0.99

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -72,7 +72,7 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   uint64_t dummy_size = 0;
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
 
-  inst.dump();
+  // inst.dump();
   std::cout<<"dummy_size"<<dummy_size<<std::endl;
   std::cout<<"code:"<<code<<std::endl;
   std::cout<<"nbyte:"<<nbyte<<std::endl;
