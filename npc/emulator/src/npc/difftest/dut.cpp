59a7438253be544eb98a4227bd738bced12f0803
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-31 18:26:37 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:26:37 up  6:14,  1 user,  load average: 1.24, 0.70, 0.50

--- npc/emulator/src/npc/difftest/dut.cpp
+++ npc/emulator/src/npc/difftest/dut.cpp
@@ -114,7 +114,6 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {//pc=npc
-return true;
   for(int i = 0; i < 32; i++){
     if(ref_r->gpr[i] != cpu.gpr[i]){
       Log("*** Difftest fail: current pc ref(0x%016lx) dut(0x%016lx) ***", ref_r->pc, cpu.pc);
