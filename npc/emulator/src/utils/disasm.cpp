b59c62447a5093df39d1788b2f6234a041b6bcb3
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-31 22:39:24 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:39:24 up 10:27,  1 user,  load average: 2.18, 1.37, 1.00

--- npc/emulator/src/utils/disasm.cpp
+++ npc/emulator/src/utils/disasm.cpp
@@ -64,10 +64,10 @@ void init_disasm(const char *triple) {
 #if LLVM_VERSION_MAJOR >= 11
   gIP->setPrintBranchImmAsAddress(true);
 #endif
-  if(gDisassembler==nullptr){
+  if(!gDisassembler){
     printf("error");
+    assert(0);
   }
-  assert(0);
 }
 
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
