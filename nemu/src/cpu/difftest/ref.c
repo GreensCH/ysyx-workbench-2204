c98e02ac4501f9807c1b25f292be1a0f0bc7cd5f
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-31 11:01:37 +0800

    > compile NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:01:37 up 31 min,  1 user,  load average: 0.70, 0.64, 0.51

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -3,16 +3,33 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
-}
+// `direction`指定拷贝的方向, 
+// `DIFFTEST_TO_DUT`表示往DUT拷贝, 
+// `DIFFTEST_TO_REF`表示往REF拷贝
 
+void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+  uint8_t *p = buf;
+  if(direction == DIFFTEST_TO_REF){
+    for (size_t i = 0; i < n; i++) {
+      *guest_to_host(addr) = p[i];
+    }
+  }
+  else{
+    assert(0);
+  }
+}
+// `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
+// `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`;
 void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  word_t* p = dut;
+  if(direction == DIFFTEST_TO_DUT)
+    for(int i = 0; i < 32; i++) p[i] = cpu.gpr[i];
+  else
+    for(int i = 0; i < 32; i++) cpu.gpr[i] = p[i];
 }
 
 void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
 
 void difftest_raise_intr(word_t NO) {
