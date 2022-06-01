e43c6e53a8d533c494d82c8a4e271999dd594979
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-01 13:38:19 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:38:19 up  3:41,  1 user,  load average: 0.88, 1.05, 0.81

--- npc/emulator/src/npc/difftest/dut.cpp
+++ npc/emulator/src/npc/difftest/dut.cpp
@@ -118,13 +118,13 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {//pc=npc
   for(int i = 0; i < 32; i++){
     if(ref_r->gpr[i] != cpu.gpr[i]){
-      Log("*** Difftest fail: current pc ref(0x%016lx) dut(0x%016lx) ***", ref_r->pc, cpu.pc);
-      Log("*** Difftest fail: reg %s dismatch ref(0x%016lx) dut(0x%016lx) ***", get_reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
+      Log(ASNI_FG_RED "*** Difftest fail: current pc ref(0x%016lx) dut(0x%016lx) ***", ref_r->pc, cpu.pc);
+      Log(ASNI_FG_RED "*** Difftest fail: reg %s dismatch ref(0x%016lx) dut(0x%016lx) ***", get_reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
       return false;
     }
   }
   if(ref_r->pc != cpu.pc){
-    Log("*** Difftest fail: pc dismatch ref(0x%016lx) dut(0x%016lx) ***", ref_r->pc, cpu.pc);
+    Log(ASNI_FG_RED "*** Difftest fail: pc dismatch ref(0x%016lx) dut(0x%016lx) ***", ref_r->pc, cpu.pc);
     return false;
   }
   return true;
