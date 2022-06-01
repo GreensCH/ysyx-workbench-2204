77e58fa3538c722fb8b751f98eafca0fabf043b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-01 14:09:45 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:09:44 up  4:13,  1 user,  load average: 0.70, 0.86, 0.72

--- npc/emulator/src/simulator.cpp
+++ npc/emulator/src/simulator.cpp
@@ -30,9 +30,6 @@ void reset(int n){
   
   top->reset = 1;
   step_and_dump_wave();
-  step_and_dump_wave();
-  step_and_dump_wave();
-  step_and_dump_wave();
   top->reset = 0;
 /*
   step_and_dump_wave();
