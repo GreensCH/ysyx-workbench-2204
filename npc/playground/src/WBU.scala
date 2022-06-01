5bc95316d4e4589d8d22eace94f59427bd835d53
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-01 19:20:11 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:20:11 up  9:23,  1 user,  load average: 0.84, 0.78, 0.48

--- npc/playground/src/WBU.scala
+++ npc/playground/src/WBU.scala
@@ -11,12 +11,12 @@ class WBU extends Module {
 //  printf("WBU\t\n")
   /* interface */
   val wb_sel = io.id2wb.wb_sel
-  val memory_data = io.mem2wb.memory_data
-  val result_data = io.ex2wb.result_data
+  val mem_data = io.mem2wb.memory_data
+  val exe_data = io.ex2wb.result_data
   /* wb2regfile interface */
-  io.wb2regfile.data:= Mux(wb_sel, memory_data, result_data)
+  io.wb2regfile.data:= Mux(wb_sel, mem_data, exe_data)
   when(wb_sel){
-    printf(p"result_data ${Hexadecimal(result_data)} ")
+    printf(p"result_data ${Hexadecimal(exe_data)} ")
     printf(p"io.mem2wb.memory_data ${Hexadecimal(io.mem2wb.memory_data )}")
     printf(p"io.wb2regfile.data ${Hexadecimal(io.wb2regfile.data)} \n")
   }
