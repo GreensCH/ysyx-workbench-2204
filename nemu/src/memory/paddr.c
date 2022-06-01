805ef005cc0e78bb52747787e167e86e36f8f590
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-01 11:41:03 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:41:03 up  1:44,  1 user,  load average: 0.45, 0.50, 0.57

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -50,6 +50,8 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
   #ifdef CONFIG_MTRACE_COND
     if(MTRACE_COND) IFDEF(CONFIG_MTRACE, mtrace_rd_log(pmem_read(addr, len), addr););//{  mtrace_rd_log(pmem_read(addr, len), (word_t)addr);  };
+  #else
+    mtrace_rd_log(pmem_read(addr, len), addr);
   #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -60,6 +62,8 @@ word_t paddr_read(paddr_t addr, int len) {
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE_COND
     if(MTRACE_COND) IFDEF(CONFIG_MTRACE,  mtrace_we_log(data, addr););//if(MTRACE_COND) {  mtrace_we_log(data, addr);  };
+  #else
+    mtrace_we_log(data, addr);
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
