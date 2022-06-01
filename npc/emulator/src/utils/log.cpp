f18fd9a6b3b33f4b09733ef1c118414c47cbd947
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-01 17:31:36 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:31:36 up  7:35,  1 user,  load average: 0.44, 0.64, 0.67

--- npc/emulator/src/utils/log.cpp
+++ npc/emulator/src/utils/log.cpp
@@ -87,11 +87,13 @@ void itrace_log(){
 word_t isa_reg_str2val(const char *s, bool *success);
 void mtrace_rd_log(word_t data, word_t addr){
   bool flag = true;
+  if(cpu_npc == addr) return;
   if (likely(in_pmem(addr))) Log("PMEM-RD:PC(0x%016lx) 0x%016lx @0x%016lx", cpu_npc, data, addr); 
   IFDEF(CONFIG_DEVICE, Log("MMIO-RD:PC(0x%016lx) 0x%016lx @0x%016lx", cpu_npc, data, addr)); 
 }
 void mtrace_we_log(word_t data, word_t addr){
   bool flag = true;
+  if(cpu_npc == addr) return;
   if (likely(in_pmem(addr))) Log("PMEM-WE:PC(0x%016lx) 0x%016lx @0x%016lx", cpu_npc, data, addr); 
   IFDEF(CONFIG_DEVICE, Log("MMIO-WE:PC(0x%016lx) 0x%016lx @0x%016lx", cpu_npc, data, addr)); 
 }
