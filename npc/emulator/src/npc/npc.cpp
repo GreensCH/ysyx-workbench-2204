124ac9f99ecf73d969d3c72d8580b31e7dd97a15
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-02 15:57:48 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:57:48 up  5:41,  1 user,  load average: 0.58, 0.67, 0.53

--- npc/emulator/src/npc/npc.cpp
+++ npc/emulator/src/npc/npc.cpp
@@ -48,7 +48,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   s->snpc = cpu.pc + 4;
   s->isa.inst.val = paddr_read(cpu.pc, 4);
   top->io_inst = paddr_read(cpu.pc, 4);
-  printf("pc%016lx inst%016lx\n",cpu.pc,paddr_read(cpu.pc, 8));
+  printf("pc%016lx inst%016lx\n",cpu.pc,host_read(guest_to_host(cpu.pc), 4);
   step_and_dump_wave();
   for (int i = 0; i < 32; i++) {
     cpu.gpr[i] = cpu_gpr[i];
