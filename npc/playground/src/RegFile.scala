bf7b8f1d404ce1eb9857645438e5baf0a4534788
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-02 12:41:56 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:41:56 up  2:25,  1 user,  load average: 0.37, 0.24, 0.17

--- npc/playground/src/RegFile.scala
+++ npc/playground/src/RegFile.scala
@@ -29,8 +29,8 @@ class RegFile extends Module{
 
 //  when(io.wbu.en){
     printf("RegFile\t\n")
-    printf(p"io.wbu.addr ${Hexadecimal(io.wbu.addr)} ")
-    printf(p"io.wbu.addr ${gpr(io.wbu.addr)} ")
+    printf(p"io.wbu.addr ${Hexadecimal(io.wbu.addr& Fill(5, io.wbu.en))} ")
+    printf(p"io.wbu.addr ${gpr(io.wbu.addr& Fill(5, io.wbu.en))} ")
     printf(p"io.wbu.data ${Hexadecimal(io.wbu.data)} \n")
 //  } .otherwise{
 //    printf(p"io.wbu.addr ${Hexadecimal(io.wbu.addr)} \n")
