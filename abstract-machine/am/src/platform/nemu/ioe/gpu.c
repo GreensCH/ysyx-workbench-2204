b79413b6c12c923145fba69c0d30bf52dbc187cc
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-06 02:24:00 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:24:00 up  6:23,  1 user,  load average: 0.49, 0.47, 0.43

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,14 +3,14 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-int w = 0, h = 0;
+uint32_t w = 0, h = 0;
 void __am_gpu_init() {
 #define N 32
 #include <klib.h>
   int i;
   uint32_t vgactl = inl(VGACTL_ADDR);
   w = vgactl >> 16;  // TODO: get the correct width
-  h = vgactl & 0xF;  // TODO: get the correct height
+  h = vgactl & 0b11111111;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
