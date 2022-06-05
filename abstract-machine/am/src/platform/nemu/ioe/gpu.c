b3c33ab38e5b7190908f4edf1507f7dbcf58b011
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-06 02:42:38 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:42:38 up  6:42,  1 user,  load average: 0.61, 0.39, 0.33

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,14 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 #include <klib.h>
-#define N 32
+
+uint32_t w = 0, h = 0;  
+
 void __am_gpu_init() {
   int i;
   uint32_t vgactl = inl(VGACTL_ADDR);
-  uint32_t w = vgactl >> 16;  // TODO: get the correct width
-  uint32_t h = vgactl & 0xFFFF;  // TODO: get the correct height
+  w = vgactl >> 16;     // TODO: get the correct width
+  h = vgactl & 0xFFFF;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -18,19 +20,14 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t vgactl = inl(VGACTL_ADDR);
-  uint32_t w = vgactl >> 16;  
-  uint32_t h = vgactl & 0xFFFF;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = w, .height = h,
+    .width = vgactl >> 16, .height = vgactl & 0xFFFF,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-
-  uint32_t vgactl = inl(VGACTL_ADDR);
-  uint32_t w = vgactl >> 16;  
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   printf("%d\n", w);
   int cnt=0;
