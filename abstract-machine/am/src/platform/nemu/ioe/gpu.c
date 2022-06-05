eea2a51e623b7e92748e6a1cd2bc592ec8e37542
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-06 03:26:44 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:26:44 up  7:26,  1 user,  load average: 0.27, 0.22, 0.37

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,12 +5,12 @@
 
 #include <klib.h>
 
-uint32_t w = 0, h = 0;  
+uint32_t vgaw = 0, vgah = 0;  
 
 void __am_gpu_init() {
   uint32_t vgactl = inl(VGACTL_ADDR);
-  w = vgactl >> 16;
-  h = vgactl & 0xFFFF;
+  vgaw = vgactl >> 16;
+  vgah = vgactl & 0xFFFF;
   // int i;
   // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   // for (i = 0; i < w * h; i ++) fb[i] = i;
@@ -18,33 +18,30 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t vgactl = inl(VGACTL_ADDR);//屏幕大小寄存器
-  w = vgactl >> 16;
-  h = vgactl & 0xFFFF;
+  uint32_t vgactl = inl(VGACTL_ADDR);
+  vgaw = vgactl >> 16;
+  vgah = vgactl & 0xFFFF;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = w, .height = h,
+    .width = vgaw, .height = vgah,
     .vmemsz = 0
   };
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {//屏幕大小寄存器
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // int i;
-  // for (i = 0; i < ctl->h * ctl->w; i ++) 
-  //   fb[i] = ((uint32_t*)ctl->pixels)[i + 1];
-
-  int cnt=0;
   int i, j;
+  int cnt = 0;
   for(j = 0; j < ctl->h; j ++){
     for (i = 0; i < ctl->w; i ++){
       int p = ctl->y + j;
       int q = ctl->x + i;
-      fb[p * w + q] = ((uint32_t*)ctl->pixels)[++cnt];
+      fb[p * vgaw + q] = ((uint32_t*)ctl->pixels)[++cnt];
     }
   }
-
-
+  printf("%d %d\n",ctl->h ,ctl->w);
+  //ctl am gpu
+  //fb  vga mem 
   if (ctl->sync) {//同步寄存器
     outl(SYNC_ADDR, 1);
   }
