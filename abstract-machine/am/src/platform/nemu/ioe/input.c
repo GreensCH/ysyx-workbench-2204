02fa4151bb2897f8799de19fb2c933441d9f30fb
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-06 01:50:12 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:50:12 up  5:49,  1 user,  load average: 0.31, 0.48, 0.44

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,24 +2,25 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
+#define KEY_QUEUE_LEN 1024
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t key = inl(KBD_ADDR);
-  // int keydown = key & KEYDOWN_MASK;
-  // int keycode = key % KEY_QUEUE_LEN;
-  // if (keycode == AM_KEY_NONE){
-  //   kbd->keydown = false;
-  //   kbd->keycode = AM_KEY_NONE;
-  // }
-  // else{
-  //   if(keydown != 0)
-  //     kbd->keydown = true;
-  //   else
-  //     kbd->keydown = false;
-  //   kbd->keycode = keycode;
-  // }
+  int keydown = key & KEYDOWN_MASK;
+  int keycode = key % KEY_QUEUE_LEN;
+  if (keycode == AM_KEY_NONE){
+    kbd->keydown = false;
+    kbd->keycode = AM_KEY_NONE;
+  }
+  else{
+    if(keydown != 0)
+      kbd->keydown = true;
+    else
+      kbd->keydown = false;
+    kbd->keycode = keycode;
+  }
 
 
-  kbd->keydown = (key & KEYDOWN_MASK ? true : false);
-  kbd->keycode = key & ~KEYDOWN_MASK;
+  // kbd->keydown = (key & KEYDOWN_MASK ? true : false);
+  // kbd->keycode = key & ~KEYDOWN_MASK;
 }
