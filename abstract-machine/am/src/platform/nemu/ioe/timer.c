279c2cc8f020a9abcdb2e4fc73269d2be3c8dbe6
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-06 00:09:53 +0800

    > gdb NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:09:53 up  4:09,  1 user,  load average: 0.33, 0.29, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = (uint64_t)inl(RTC_ADDR);//(((uint64_t)inl(RTC_ADDR + 4)) << 32) | inl(RTC_ADDR);
+  // printf("%d\n",inl(RTC_ADDR))
+  uint64_t test = inl(RTC_ADDR);
+  uptime->us = (((uint64_t)inl(RTC_ADDR + 4)) << 32) | test;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
