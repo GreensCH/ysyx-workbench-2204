b69ff551b4b2465777afa50efaa23d2855a38428
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-06 02:10:29 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:10:29 up  6:09,  1 user,  load average: 0.29, 0.33, 0.34

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,6 +4,13 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+#define N 32
+  int i;
+  int w = io_read(AM_GPU_CONFIG).width / N;  // TODO: get the correct width
+  int h = io_read(AM_GPU_CONFIG).height / N;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,25 +2,10 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
-#define KEY_QUEUE_LEN 1024
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t key = inl(KBD_ADDR);
-  int keydown = key & KEYDOWN_MASK;
-  int keycode = key % KEY_QUEUE_LEN;
-  if (keycode == AM_KEY_NONE){
-    kbd->keydown = false;
-    kbd->keycode = AM_KEY_NONE;
-  }
-  else{
-    if(keydown != 0)
-      kbd->keydown = true;
-    else
-      kbd->keydown = false;
-    kbd->keycode = keycode;
-  }
+  uint32_t k = inl(KBD_ADDR);
 
-
-  // kbd->keydown = (key & KEYDOWN_MASK ? true : false);
-  // kbd->keycode = key & ~KEYDOWN_MASK;
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
