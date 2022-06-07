7a1da3e9fc91327063fd0b826a3bfaf61899484d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-08 02:48:05 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:48:05 up  7:27,  6 users,  load average: 2.30, 1.79, 1.51

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -41,7 +41,7 @@ class EXU extends Module{
   val shift_src2 = Mux(word, src2(4, 0), src2(5, 0))
   /* Multiplier  */
   val div_result = Wire(UInt(64.W))
-  div_result := Cat(0.U(1.W),src1.asSInt()) / Cat(0.U(1.W),src2.asSInt())
+  div_result := Cat(1.U(1.W),src1.asSInt()) / Cat(0.U(1.W),src2.asSInt())
   printf(p"s1 unsigned 0:${Binary(src1)}\n")
   printf(p"s1 signed 0  :${Binary(src1.asSInt())}\n")
   printf(p"s2 unsigned 0:${Binary(src2)}\n")
