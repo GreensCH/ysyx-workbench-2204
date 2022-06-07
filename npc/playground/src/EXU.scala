8c85eaeffe2d0e07640c331fbd72d2b2a862f35a
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-08 01:42:06 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:42:06 up  6:21,  6 users,  load average: 1.10, 1.32, 1.06

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -40,14 +40,10 @@ class EXU extends Module{
   //val adder_out = adder_in1 + adder_in2
   val shift_src2 = Mux(word, src2(4, 0), src2(5, 0))
   /* Multiplier  */
-  val msrc1 = Wire(UInt(65.W))
-  val msrc2 = Wire(UInt(65.W))
-  msrc1 := Sext(src1, 64)
-  msrc2 := Sext(src2, 64)
-  val div_result = msrc1.asSInt() / msrc2.asSInt()
+  val div_result = src1.asSInt() / src2.asSInt()
   printf(p"div_result 1:${Binary(src1/src2)}\n")
   printf(p"div_result 2:${Binary(alu_src1/alu_src2)}\n")
-  printf(p"div_result 3:${Binary(msrc1/msrc2)}\n")
+  printf(p"div_result 3:${Binary(div_result)}\n")
   /* result generator */
   val result = MuxCase(0.U(64.W),
     Array(
