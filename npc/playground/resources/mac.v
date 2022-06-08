94618dc2401c5b1844df8ae812d9fb00655a5369
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-08 16:21:11 +0800

    > run NPC ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:21:11 up  3:05,  3 users,  load average: 0.42, 0.25, 0.24

--- npc/playground/resources/mac.v
+++ npc/playground/resources/mac.v
@@ -38,16 +38,6 @@ module mac (
   wire [63:0] remu_result = ({64{inf    }} & src1)
                           | ({64{normal | over}} & ($unsigned(src1) % $unsigned(src2)));
 
-  
-  
-  
-  //assign $signed(src1) / $signed(src2);
-  // wire [63:0] divu_result = $unsigned(src1) / $unsigned(src2);
-  // wire [63:0] rem_result  = $signed(src1) % $signed(src2);
-  // wire [63:0] remu_result = $unsigned(src1) % $unsigned(src2);
-
-  // wire [9: 0] op = {2'd3,mul,mulh,mulhu,mulhsu,div,divu,rem,remu};
-
   assign result = ({64{mul    }} & mulh_buf[63:0])
                 | ({64{mulh   }} & mulh_buf[127: 64])
                 | ({64{mulhu  }} & mulhu_buf[127: 64])
@@ -57,12 +47,4 @@ module mac (
                 | ({64{rem    }} & rem_result)
                 | ({64{remu   }} & remu_result);
 
-
-  // always@(*)$display("op%b\n",op);
-  // always@(*)$display("result%b\n",result);
-  // always@(*)$display("test1%b\n",test1);
-  // always@(*)$display("test2%b\n",test1);
-
-  
-  // wire [63:0] test = ($signed(src1) / $signed(src2);
 endmodule
