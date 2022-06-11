83f9f018b679b2f40baf83c2dd180a035a66904f
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-11 20:55:38 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:38 up  6:11,  3 users,  load average: 0.26, 0.13, 0.18

--- npc/playground/src/Staller.scala
+++ npc/playground/src/Staller.scala
@@ -1,106 +1,30 @@
 import chisel3._
 import chisel3.util._
-import chisel3.experimental._
-
-object BypassMuxSel extends ChiselEnum{
-  val normal, ex, mem, wb = Value
-}
 
 class Staller extends Module{
   val io = IO(new Bundle{
-    val id_src1   =   Input (UInt(5.W))
-    val id_src2   =   Input (UInt(5.W))
-    val ex_dst    =   Input (UInt(5.W))
-    val mem_dst   =   Input (UInt(5.W))
-    val wb_dst    =   Input (UInt(5.W))
-    val optype    =   Flipped(new Optype)
-    val operator  =   Flipped(new Operator)
-    val is_load   =   Input (Bool())
-    val stall     =   Output(Bool())
-    val bypassmux_sel1  =   Output(BypassMuxSel())
-    val bypassmux_sel2  =   Output(BypassMuxSel())
+    val addr1    =   Input (UInt(5.W))
+    val addr2    =   Input (UInt(5.W))
+    val ex_dst  =   Input (UInt(5.W))
+    val mem_dst =   Input (UInt(5.W))
+    val wb_dst  =   Input (UInt(5.W))
+    val optype  =   Flipped(new Optype)
+    val operator=   Flipped(new Operator)
+    val stall   =   Output(Bool())
   })
   /* interface */
-  val id_src1     =   io.id_src1
-  val id_src2     =   io.id_src2
-  val ex_dst      =   io.ex_dst
-  val mem_dst     =   io.mem_dst
-  val wb_dst      =   io.wb_dst
-  val optype      =   io.optype
-  val operator    =   io.operator
-  val is_load     =   io.is_load
+  val addr1   =   io.addr1
+  val addr2   =   io.addr2
+  val ex_dst  =   io.ex_dst
+  val mem_dst =   io.mem_dst
+  val wb_dst  =   io.wb_dst
+  val optype  =   io.optype
+  val operator=   io.operator
 
-  // if zero_n is false then stall shouldn't be enable
-  // if zero_n is true  then stall may be enable
-  val zero1_n = ex_dst  =/= 0.U
-  val zero2_n = mem_dst =/= 0.U
-  val zero3_n = wb_dst  =/= 0.U
-  val zero_n   = zero1_n | zero2_n | zero3_n
+  val eq1 = (addr1 =/= 0.U | addr2 =/= 0.U ) & ((addr1 === ex_dst & ex_dst =/= 0.U) | (addr1 === mem_dst & mem_dst =/= 0.U) | (addr1 === wb_dst & wb_dst =/= 0.U)) & (optype.Itype | optype.Rtype | optype.Jtype | optype.Stype)
+  val eq2 = (addr1 =/= 0.U | addr2 =/= 0.U ) & ((addr2 === ex_dst & ex_dst =/= 0.U) | (addr2 === mem_dst & mem_dst =/= 0.U) | (addr2 === wb_dst & wb_dst =/= 0.U)) & (optype.Itype | optype.Rtype | optype.Jtype | optype.Stype)
 
-  val eq1_1 = id_src1 === ex_dst & zero1_n
-  val eq1_2 = id_src1 === mem_dst & zero2_n
-  val eq1_3 = id_src1 === wb_dst & zero3_n
-
-  val eq2_1 = id_src2 === ex_dst & zero1_n
-  val eq2_2 = id_src2 === mem_dst & zero2_n
-  val eq2_3 = id_src2 === wb_dst & zero3_n
-
-
-  val eq1 = eq1_1 | eq1_2 | eq1_3
-  val eq2 = eq2_1 | eq2_2 | eq2_3
-  // after add stall, id-stage data is stopped, such operator.jalr is stopped until all 3 dst addr are 0
-  val stall = zero_n & (operator.jalr | optype.Stype | optype.Jtype| is_load)
-  io.bypassmux_sel1 := MuxCase(BypassMuxSel.normal,
-    Array(
-      (stall)            -> BypassMuxSel.normal,
-      (!is_load & eq1_1) -> BypassMuxSel.ex,
-      (!is_load & eq1_2) -> BypassMuxSel.mem,
-      (!is_load & eq1_3) -> BypassMuxSel.wb,
-    )
-  )
-  io.bypassmux_sel2 := MuxCase(BypassMuxSel.normal,
-    Array(
-      (stall)            -> BypassMuxSel.normal,
-      (!is_load  & eq2_1) -> BypassMuxSel.ex,
-      (!is_load  & eq2_2) -> BypassMuxSel.mem,
-      (!is_load  & eq2_3) -> BypassMuxSel.wb,
-    )
-  )
-  io.stall := stall
-
+  val jalr_cond = (0.U =/= ex_dst | 0.U =/= mem_dst | 0.U =/= wb_dst) & operator.jalr//分支跳转添加空泡
+  /* output */
+  io.stall := eq1 | eq2 | jalr_cond
 }
-
-
-
-//class Staller extends Module{
-//  val io = IO(new Bundle{
-//    val addr1   =   Input (UInt(5.W))
-//    val addr2   =   Input (UInt(5.W))
-//    val ex_dst  =   Input (UInt(5.W))
-//    val mem_dst =   Input (UInt(5.W))
-//    val wb_dst  =   Input (UInt(5.W))
-//    val optype  =   Flipped(new Optype)
-//    val operator=   Flipped(new Operator)
-//    val stall   =   Output(Bool())
-//    val bypass  =   Output(UInt(2.W))
-//  })
-//  /* interface */
-//  val addr1   =   io.addr1
-//  val addr2   =   io.addr2
-//  val ex_dst  =   io.ex_dst
-//  val mem_dst =   io.mem_dst
-//  val wb_dst  =   io.wb_dst
-//  val optype  =   io.optype
-//  val operator=   io.operator
-//
-//  val eq1 = (addr1 =/= 0.U | addr2 =/= 0.U ) &
-//            ((addr1 === ex_dst & ex_dst =/= 0.U) | (addr1 === mem_dst & mem_dst =/= 0.U) | (addr1 === wb_dst & wb_dst =/= 0.U))&
-//            (optype.Itype | optype.Rtype | optype.Btype | optype.Jtype  | optype.Stype)
-//  val eq2 = (addr1 =/= 0.U | addr2 =/= 0.U ) &
-//            ((addr2 === ex_dst & ex_dst =/= 0.U) | (addr2 === mem_dst & mem_dst =/= 0.U) | (addr2 === wb_dst & wb_dst =/= 0.U)) &
-//            (optype.Itype | optype.Rtype | optype.Btype | optype.Jtype  | optype.Stype)
-//
-//  //val jalr_cond = (0.U =/= ex_dst | 0.U =/= mem_dst | 0.U =/= wb_dst) & operator.jalr//分支跳转添加空泡
-//  /* output */
-//  io.stall := eq1 | eq2
-//}
