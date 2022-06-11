e22bfbdd64a4bc256ca0864d7ce380998aae3e03
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-11 23:59:22 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:59:22 up  9:15,  3 users,  load average: 1.37, 0.75, 0.44

--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -41,8 +41,8 @@ class Top extends Module {
   bypassmux.io.sel2 := staller.io.bypassmux_sel2
   bypassmux.io.id_data1 := idu.io.id2ex.src1
   bypassmux.io.id_data2 := idu.io.id2ex.src2
-  bypassmux.io.ex_data := exu.io.ex2mem.we_data
-  bypassmux.io.mem_data := memu.io.ex2mem.rd_addr
+  bypassmux.io.ex_data := exu.io.ex2mem.rd_addr//exu.io.ex2mem.we_data
+  bypassmux.io.mem_data := memu.io.mem2wb.memory_data//memu.io.ex2mem.rd_addr
   bypassmux.io.wb_data := wbu.io.wb2regfile.data
   val new_id2ex = Wire(new ID2EX)
   new_id2ex := idu.io.id2ex
