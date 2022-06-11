3d790ed57f6e2137be17a0efccd8524d5d311b2c
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-11 20:26:31 +0800

    > run NPC ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:26:31 up  5:42,  3 users,  load average: 1.12, 0.73, 0.48

--- npc/playground/src/Staller.scala
+++ npc/playground/src/Staller.scala
@@ -49,7 +49,7 @@ class Staller extends Module{
   val eq1 = eq1_1 | eq1_2 | eq1_3
   val eq2 = eq2_1 | eq2_2 | eq2_3
   // after add stall, id-stage data is stopped, such operator.jalr is stopped until all 3 dst addr are 0
-  val stall = zero_n & (operator.jalr | optype.Stype | optype.Btype | optype.Jtype| is_load)
+  val stall = zero_n & (operator.jalr | optype.Stype | optype.Jtype| is_load)
   io.bypassmux_sel1 := MuxCase(BypassMuxSel.normal,
     Array(
       (stall)            -> BypassMuxSel.normal,
@@ -61,9 +61,9 @@ class Staller extends Module{
   io.bypassmux_sel2 := MuxCase(BypassMuxSel.normal,
     Array(
       (stall)            -> BypassMuxSel.normal,
-      (!is_load & !optype.Btype & eq2_1) -> BypassMuxSel.ex,
-      (!is_load & !optype.Btype & eq2_2) -> BypassMuxSel.mem,
-      (!is_load & !optype.Btype & eq2_3) -> BypassMuxSel.wb,
+      (!is_load  & eq2_1) -> BypassMuxSel.ex,
+      (!is_load  & eq2_2) -> BypassMuxSel.mem,
+      (!is_load  & eq2_3) -> BypassMuxSel.wb,
     )
   )
   io.stall := stall
