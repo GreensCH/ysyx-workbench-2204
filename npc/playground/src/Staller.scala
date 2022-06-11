06c35aafc2c436f7f7eb414f6d2ceabce5fe2381
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-11 20:27:17 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:27:17 up  5:43,  3 users,  load average: 1.58, 0.94, 0.57

--- npc/playground/src/Staller.scala
+++ npc/playground/src/Staller.scala
@@ -53,9 +53,9 @@ class Staller extends Module{
   io.bypassmux_sel1 := MuxCase(BypassMuxSel.normal,
     Array(
       (stall)            -> BypassMuxSel.normal,
-      (!is_load & !optype.Btype & eq1_1) -> BypassMuxSel.ex,
-      (!is_load & !optype.Btype & eq1_2) -> BypassMuxSel.mem,
-      (!is_load & !optype.Btype & eq1_3) -> BypassMuxSel.wb,
+      (!is_load & eq1_1) -> BypassMuxSel.ex,
+      (!is_load & eq1_2) -> BypassMuxSel.mem,
+      (!is_load & eq1_3) -> BypassMuxSel.wb,
     )
   )
   io.bypassmux_sel2 := MuxCase(BypassMuxSel.normal,
