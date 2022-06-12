f2586ac3d730a5fbae2bbc65b7792f2aa0e1b437
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-12 14:26:57 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:26:57 up  4:31,  3 users,  load average: 0.94, 0.48, 0.34

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -31,15 +31,16 @@ class EXReg extends Module{
   val stall = io.stall
   io.valid_out := RegNext(next = io.valid_in)
   // data transfer
-  val id2ex = Mux(stall, 0.U.asTypeOf(new ID2EX), io.in.id2ex)//io.in.id2ex
+  val id2ex = io.in.id2ex//Mux(stall, 0.U.asTypeOf(new ID2EX), io.in.id2ex)//io.in.id2ex
   val id2mem = io.in.id2mem//Mux(stall, 0.U.asTypeOf(new ID2MEM), io.in.id2mem)
 //  val id2wb = Mux(stall, 0.U.asTypeOf(new ID2WB), io.in.id2wb)
-  val id2wb = io.in.id2wb//Wire(new ID2WB)
-//  id2wb.test_pc := Mux(stall, 0.U, io.in.id2wb.wb_sel)
-//  id2wb.test_inst := Mux(stall, 0.U, io.in.id2wb.wb_sel)
-//  id2wb.wb_sel := Mux(stall, 0.U, io.in.id2wb.wb_sel)//io.in.id2wb.wb_sel
-//  id2wb.regfile_we_addr := Mux(stall, 0.U, io.in.id2wb.regfile_we_addr) //io.in.id2wb.regfile_we_addr
-//  id2wb.regfile_we_en := Mux(stall, 0.U, io.in.id2wb.regfile_we_en)//io.in.id2wb.regfile_we_en//
+//  val id2wb = io.in.id2wb//Wire(new ID2WB)
+  val id2wb = Wire(new ID2WB)
+  id2wb.test_pc := io.in.id2wb.test_pc//Mux(stall, 0.U, io.in.id2wb.wb_sel)
+  id2wb.test_inst := io.in.id2wb.test_inst//Mux(stall, 0.U, io.in.id2wb.wb_sel)
+  id2wb.wb_sel := Mux(stall, 0.U, io.in.id2wb.wb_sel)//io.in.id2wb.wb_sel
+  id2wb.regfile_we_addr := Mux(stall, 0.U, io.in.id2wb.regfile_we_addr) //io.in.id2wb.regfile_we_addr
+  id2wb.regfile_we_en := Mux(stall, 0.U, io.in.id2wb.regfile_we_en)//io.in.id2wb.regfile_we_en//
 
   val reg_2ex   =   RegNext(next = id2ex)
   val reg_2mem  =   RegNext(next = id2mem)
