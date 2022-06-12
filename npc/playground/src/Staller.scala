e9cc3657d68acdbf87c7223c0980bbbd5d50bba1
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-12 12:21:22 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:21:22 up  2:26,  3 users,  load average: 0.37, 0.47, 0.35

--- npc/playground/src/Staller.scala
+++ npc/playground/src/Staller.scala
@@ -56,7 +56,7 @@ class Staller extends Module{
 
 
 
-  val stall = zero_n & (operator.jalr | optype.Stype | optype.Jtype  | optype.Btype | is_load) & (valid3 | (valid1 & !valid3))
+  val stall = (operator.jalr | optype.Stype | optype.Jtype  | optype.Btype | is_load) & (valid3 | (valid1 & !valid3))
   val reg_stall = RegNext(stall)
 //  val sIdle :: s1 :: s2 :: s3 :: sEnd :: Nil = Enum(5)
 //  val state = RegInit(sIdle)
