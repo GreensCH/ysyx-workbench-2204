3c9bb34a046523c8c7cabec8915ce9f3ecd9533e
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-22 18:54:05 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:54:05 up  7:29,  2 users,  load average: 1.64, 0.96, 0.58

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -42,21 +42,31 @@ class EXReg extends Module{
   io.out.id2wb  :=  reg_2wb
 }
 //////////////////////////////////////
+class EXUIn extends Bundle{
+  val id2ex = Flipped(new ID2EX)
+}
+class EXUOut extends Bundle{
+  val ex2mem = new EX2MEM
+  val ex2wb = new EX2WB
+}
 class EXU extends Module{
   val io = IO(new Bundle{
-    val id2ex = Flipped(new ID2EX)
-    val ex2mem = new EX2MEM
-    val ex2wb = new EX2WB
+    val in = new EXUIn
+    val out = new EXUOut
   })
+  /** Instance */
+  val id2ex = io.in.id2ex
+  val ex2mem = io.out.ex2mem
+  val ex2wb  = io.out.ex2wb
 
-  val src1 = io.id2ex.src1
-  val src2 = io.id2ex.src2
-  val src3 = io.id2ex.src3
-  val operator = io.id2ex.operator
-  val byte = io.id2ex.srcsize.byte
-  val hword = io.id2ex.srcsize.hword
-  val word = io.id2ex.srcsize.word
-  val dword = io.id2ex.srcsize.dword
+  val src1 = id2ex.src1
+  val src2 = id2ex.src2
+  val src3 = id2ex.src3
+  val operator = id2ex.operator
+  val byte = id2ex.srcsize.byte
+  val hword = id2ex.srcsize.hword
+  val word = id2ex.srcsize.word
+  val dword = id2ex.srcsize.dword
 
   val alu_src1  = Mux(word, src1(31, 0), src1)
   val alu_src2  = Mux(word, src2(31, 0), src2)
@@ -111,10 +121,10 @@ class EXU extends Module{
     )
   )
   /* ex2mem interface */
-  io.ex2mem.rd_addr := src1 + src2
-  io.ex2mem.we_data := result_out
-  io.ex2mem.we_addr := src1 + src3
-  io.ex2mem.we_mask := MuxCase("b0000_00000".U,
+  ex2mem.rd_addr := src1 + src2
+  ex2mem.we_data := result_out
+  ex2mem.we_addr := src1 + src3
+  ex2mem.we_mask := MuxCase("b0000_00000".U,
     Array(
       byte  -> "b0000_0001".U,
       hword -> "b0000_0011".U,
@@ -123,7 +133,7 @@ class EXU extends Module{
     )
   )
   /* ex2wb interface */
-  io.ex2wb.result_data := result_out
+  ex2wb.result_data := result_out
   /* ebreak */
   val ebreak = Module(new Ebreak)
   ebreak.io.valid := operator.ebreak
