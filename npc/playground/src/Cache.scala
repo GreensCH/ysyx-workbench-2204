50246619b70f9df79de450022e5652280b355e25
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-02 00:43:16 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:43:16 up  9:59,  4 users,  load average: 0.27, 0.09, 0.03

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -12,24 +12,44 @@ class ICache extends Module{
       val master = new AXI4
       val out    = new IFUOut
   })
-  val pcOut = io.in
+  val prev = io.in
   val memory = io.master
-  val ifOut = io.out
+  val next = io.out
 
   private val sIdle :: sLookup :: sMissIssue :: sMissCatch :: sMissEnd :: Nil = Enum(4)
   val curState = RegInit(init = sMissIssue)
   val nState = Wire(UInt(sIdle.getWidth.W))
 
-  val outList = MuxCase(
-    default = List(0.U(64.W), 0.U(32.W), true.B, true.B),
-    mapping = List(
-      (curState === sMissIssue) -> List(0.U(64.W), 0.U(32.W), true.B, true.B),
-      (curState === sMissCatch) -> List(0.U(64.W), 0.U(32.W), true.B, true.B),
-      (curState === sMissEnd)   -> List(0.U(64.W), 0.U(32.W), true.B, true.B)
-    )
-  )
 
-  ifOut.bits.if2id.pc := outList(0)
-  ifOut.bits.if2id.inst := outList(1)
+  switch(curState){
+    is (sIdle){
+      next.valid := false.B
+      prev.ready := true.B
+    }
+    is (sMissIssue) {
+      next.valid := false.B
+      prev.ready := false.B
+    }
+    is (sMissCatch){
+      next.valid := false.B
+      prev.ready := false.B
+    }
+    is (sMissEnd){
+      next.valid := prev.valid
+      prev.ready := true.B
+    }
+  }
+
+
 
 }
+//val outList = MuxCase(
+//  default = List(0.U(64.W), 0.U(32.W), true.B, true.B),
+//  mapping = List(
+//    (curState === sMissIssue) -> List(0.U(64.W), 0.U(32.W), true.B, true.B),
+//    (curState === sMissCatch) -> List(0.U(64.W), 0.U(32.W), true.B, true.B),
+//    (curState === sMissEnd)   -> List(0.U(64.W), 0.U(32.W), true.B, true.B)
+//  )
+//)
+//ifOut.bits.if2id.pc := outList(0)
+//ifOut.bits.if2id.inst := outList(1)
