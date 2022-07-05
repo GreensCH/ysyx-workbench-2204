0248a2d5df4dba6d29b0e847cd61bde14d9683c5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 20:44:10 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:44:10 up  3:02,  3 users,  load average: 1.31, 0.85, 0.75

--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -10,8 +10,8 @@ import chisel3.util._
 class Top extends Module {
   val io = IO(new Bundle {
     val inst = Input(UInt(32.W))
-    val mem_axi4_0  = new AXI4
-    val mmio_axi4_0 = new AXI4
+    val mem_axi4  = new AXI4
+    val mmio_axi4 = new AXI4
   })
 
   val BRIFInf = Wire(new BR2IF)
