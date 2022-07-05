51d724440c1de752163c2108311117d72fa5c6ba
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 16:17:23 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:17:23 up  2:00,  2 users,  load average: 1.17, 1.19, 0.83

--- npc/emulator/src/soc/sim_soc.cpp
+++ npc/emulator/src/soc/sim_soc.cpp
@@ -12,76 +12,76 @@
 #include <thread>
 
 
-void connect_wire(axi4_ptr <31,64,4> &mmio_ptr, axi4_ptr <32,64,4> &mem_ptr, VExampleRocketSystem *top) {
+void connect_wire(axi4_ptr <31,64,4> &mmio_ptr, axi4_ptr <32,64,4> &mem_ptr, VTop *top) {
     // connect
     // mmio
     // aw   
-    mmio_ptr.awaddr     = &(top->mmio_axi4_0_aw_bits_addr);
-    mmio_ptr.awburst    = &(top->mmio_axi4_0_aw_bits_burst);
-    mmio_ptr.awid       = &(top->mmio_axi4_0_aw_bits_id);
-    mmio_ptr.awlen      = &(top->mmio_axi4_0_aw_bits_len);
-    mmio_ptr.awready    = &(top->mmio_axi4_0_aw_ready);
-    mmio_ptr.awsize     = &(top->mmio_axi4_0_aw_bits_size);
-    mmio_ptr.awvalid    = &(top->mmio_axi4_0_aw_valid);
+    mmio_ptr.awaddr     = &(top->io_mmio_axi4_0_aw_bits_addr);
+    mmio_ptr.awburst    = &(top->io_mmio_axi4_0_aw_bits_burst);
+    mmio_ptr.awid       = &(top->io_mmio_axi4_0_aw_bits_id);
+    mmio_ptr.awlen      = &(top->io_mmio_axi4_0_aw_bits_len);
+    mmio_ptr.awready    = &(top->io_mmio_axi4_0_aw_ready);
+    mmio_ptr.awsize     = &(top->io_mmio_axi4_0_aw_bits_size);
+    mmio_ptr.awvalid    = &(top->io_mmio_axi4_0_aw_valid);
     // w
-    mmio_ptr.wdata      = &(top->mmio_axi4_0_w_bits_data);
-    mmio_ptr.wlast      = &(top->mmio_axi4_0_w_bits_last);
-    mmio_ptr.wready     = &(top->mmio_axi4_0_w_ready);
-    mmio_ptr.wstrb      = &(top->mmio_axi4_0_w_bits_strb);
-    mmio_ptr.wvalid     = &(top->mmio_axi4_0_w_valid);
+    mmio_ptr.wdata      = &(top->io_mmio_axi4_0_w_bits_data);
+    mmio_ptr.wlast      = &(top->io_mmio_axi4_0_w_bits_last);
+    mmio_ptr.wready     = &(top->io_mmio_axi4_0_w_ready);
+    mmio_ptr.wstrb      = &(top->io_mmio_axi4_0_w_bits_strb);
+    mmio_ptr.wvalid     = &(top->io_mmio_axi4_0_w_valid);
     // b
-    mmio_ptr.bid        = &(top->mmio_axi4_0_b_bits_id);
-    mmio_ptr.bready     = &(top->mmio_axi4_0_b_ready);
-    mmio_ptr.bresp      = &(top->mmio_axi4_0_b_bits_resp);
-    mmio_ptr.bvalid     = &(top->mmio_axi4_0_b_valid);
+    mmio_ptr.bid        = &(top->io_mmio_axi4_0_b_bits_id);
+    mmio_ptr.bready     = &(top->io_mmio_axi4_0_b_ready);
+    mmio_ptr.bresp      = &(top->io_mmio_axi4_0_b_bits_resp);
+    mmio_ptr.bvalid     = &(top->io_mmio_axi4_0_b_valid);
     // ar
-    mmio_ptr.araddr     = &(top->mmio_axi4_0_ar_bits_addr);
-    mmio_ptr.arburst    = &(top->mmio_axi4_0_ar_bits_burst);
-    mmio_ptr.arid       = &(top->mmio_axi4_0_ar_bits_id);
-    mmio_ptr.arlen      = &(top->mmio_axi4_0_ar_bits_len);
-    mmio_ptr.arready    = &(top->mmio_axi4_0_ar_ready);
-    mmio_ptr.arsize     = &(top->mmio_axi4_0_ar_bits_size);
-    mmio_ptr.arvalid    = &(top->mmio_axi4_0_ar_valid);
+    mmio_ptr.araddr     = &(top->io_mmio_axi4_0_ar_bits_addr);
+    mmio_ptr.arburst    = &(top->io_mmio_axi4_0_ar_bits_burst);
+    mmio_ptr.arid       = &(top->io_mmio_axi4_0_ar_bits_id);
+    mmio_ptr.arlen      = &(top->io_mmio_axi4_0_ar_bits_len);
+    mmio_ptr.arready    = &(top->io_mmio_axi4_0_ar_ready);
+    mmio_ptr.arsize     = &(top->io_mmio_axi4_0_ar_bits_size);
+    mmio_ptr.arvalid    = &(top->io_mmio_axi4_0_ar_valid);
     // r
-    mmio_ptr.rdata      = &(top->mmio_axi4_0_r_bits_data);
-    mmio_ptr.rid        = &(top->mmio_axi4_0_r_bits_id);
-    mmio_ptr.rlast      = &(top->mmio_axi4_0_r_bits_last);
-    mmio_ptr.rready     = &(top->mmio_axi4_0_r_ready);
-    mmio_ptr.rresp      = &(top->mmio_axi4_0_r_bits_resp);
-    mmio_ptr.rvalid     = &(top->mmio_axi4_0_r_valid);
+    mmio_ptr.rdata      = &(top->io_mmio_axi4_0_r_bits_data);
+    mmio_ptr.rid        = &(top->io_mmio_axi4_0_r_bits_id);
+    mmio_ptr.rlast      = &(top->io_mmio_axi4_0_r_bits_last);
+    mmio_ptr.rready     = &(top->io_mmio_axi4_0_r_ready);
+    mmio_ptr.rresp      = &(top->io_mmio_axi4_0_r_bits_resp);
+    mmio_ptr.rvalid     = &(top->io_mmio_axi4_0_r_valid);
     // mem
     // aw
-    mem_ptr.awaddr  = &(top->mem_axi4_0_aw_bits_addr);
-    mem_ptr.awburst = &(top->mem_axi4_0_aw_bits_burst);
-    mem_ptr.awid    = &(top->mem_axi4_0_aw_bits_id);
-    mem_ptr.awlen   = &(top->mem_axi4_0_aw_bits_len);
-    mem_ptr.awready = &(top->mem_axi4_0_aw_ready);
-    mem_ptr.awsize  = &(top->mem_axi4_0_aw_bits_size);
-    mem_ptr.awvalid = &(top->mem_axi4_0_aw_valid);
+    mem_ptr.awaddr  = &(top->io_mem_axi4_0_aw_bits_addr);
+    mem_ptr.awburst = &(top->io_mem_axi4_0_aw_bits_burst);
+    mem_ptr.awid    = &(top->io_mem_axi4_0_aw_bits_id);
+    mem_ptr.awlen   = &(top->io_mem_axi4_0_aw_bits_len);
+    mem_ptr.awready = &(top->io_mem_axi4_0_aw_ready);
+    mem_ptr.awsize  = &(top->io_mem_axi4_0_aw_bits_size);
+    mem_ptr.awvalid = &(top->io_mem_axi4_0_aw_valid);
     // w
-    mem_ptr.wdata   = &(top->mem_axi4_0_w_bits_data);
-    mem_ptr.wlast   = &(top->mem_axi4_0_w_bits_last);
-    mem_ptr.wready  = &(top->mem_axi4_0_w_ready);
-    mem_ptr.wstrb   = &(top->mem_axi4_0_w_bits_strb);
-    mem_ptr.wvalid  = &(top->mem_axi4_0_w_valid);
+    mem_ptr.wdata   = &(top->io_mem_axi4_0_w_bits_data);
+    mem_ptr.wlast   = &(top->io_mem_axi4_0_w_bits_last);
+    mem_ptr.wready  = &(top->io_mem_axi4_0_w_ready);
+    mem_ptr.wstrb   = &(top->io_mem_axi4_0_w_bits_strb);
+    mem_ptr.wvalid  = &(top->io_mem_axi4_0_w_valid);
     // b
-    mem_ptr.bid     = &(top->mem_axi4_0_b_bits_id);
-    mem_ptr.bready  = &(top->mem_axi4_0_b_ready);
-    mem_ptr.bresp   = &(top->mem_axi4_0_b_bits_resp);
-    mem_ptr.bvalid  = &(top->mem_axi4_0_b_valid);
+    mem_ptr.bid     = &(top->io_mem_axi4_0_b_bits_id);
+    mem_ptr.bready  = &(top->io_mem_axi4_0_b_ready);
+    mem_ptr.bresp   = &(top->io_mem_axi4_0_b_bits_resp);
+    mem_ptr.bvalid  = &(top->io_mem_axi4_0_b_valid);
     // ar
-    mem_ptr.araddr  = &(top->mem_axi4_0_ar_bits_addr);
-    mem_ptr.arburst = &(top->mem_axi4_0_ar_bits_burst);
-    mem_ptr.arid    = &(top->mem_axi4_0_ar_bits_id);
-    mem_ptr.arlen   = &(top->mem_axi4_0_ar_bits_len);
-    mem_ptr.arready = &(top->mem_axi4_0_ar_ready);
-    mem_ptr.arsize  = &(top->mem_axi4_0_ar_bits_size);
-    mem_ptr.arvalid = &(top->mem_axi4_0_ar_valid);
+    mem_ptr.araddr  = &(top->io_mem_axi4_0_ar_bits_addr);
+    mem_ptr.arburst = &(top->io_mem_axi4_0_ar_bits_burst);
+    mem_ptr.arid    = &(top->io_mem_axi4_0_ar_bits_id);
+    mem_ptr.arlen   = &(top->io_mem_axi4_0_ar_bits_len);
+    mem_ptr.arready = &(top->io_mem_axi4_0_ar_ready);
+    mem_ptr.arsize  = &(top->io_mem_axi4_0_ar_bits_size);
+    mem_ptr.arvalid = &(top->io_mem_axi4_0_ar_valid);
     // r
-    mem_ptr.rdata   = &(top->mem_axi4_0_r_bits_data);
-    mem_ptr.rid     = &(top->mem_axi4_0_r_bits_id);
-    mem_ptr.rlast   = &(top->mem_axi4_0_r_bits_last);
-    mem_ptr.rready  = &(top->mem_axi4_0_r_ready);
-    mem_ptr.rresp   = &(top->mem_axi4_0_r_bits_resp);
-    mem_ptr.rvalid  = &(top->mem_axi4_0_r_valid);
+    mem_ptr.rdata   = &(top->io_mem_axi4_0_r_bits_data);
+    mem_ptr.rid     = &(top->io_mem_axi4_0_r_bits_id);
+    mem_ptr.rlast   = &(top->io_mem_axi4_0_r_bits_last);
+    mem_ptr.rready  = &(top->io_mem_axi4_0_r_ready);
+    mem_ptr.rresp   = &(top->io_mem_axi4_0_r_bits_resp);
+    mem_ptr.rvalid  = &(top->io_mem_axi4_0_r_valid);
 }
