5a3d2e725f462e6cefaf4125d370767407b0de8b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 20:11:52 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:11:52 up  2:30,  2 users,  load average: 1.47, 0.84, 0.47

--- npc/emulator/src/soc/sim_soc.cpp
+++ npc/emulator/src/soc/sim_soc.cpp
@@ -111,26 +111,25 @@ axi4_mem <32,64,4> mem(4096l*1024*1024);
 uartlite           uart;
 std::thread        uart_input_thread(uart_input,std::ref(uart));
 
-void sim_soc_init(VTop *top) {
-    connect_wire(mmio_ptr,mem_ptr,top);
-    assert(mmio_ptr.check());
-    assert(mem_ptr.check());
-    assert(mmio.add_dev(0x60100000,1024*1024,&uart));
-    mem.load_binary(img_file,0x80000000);
-
-}
-
 // void sim_soc_init(VTop *top) {
 //     connect_wire(mmio_ptr,mem_ptr,top);
 //     assert(mmio_ptr.check());
 //     assert(mem_ptr.check());
-//     mmio_ref = axi4_ref <31,64,4> (mmio_ptr);
-//     mmio_sigs_ref = axi4_ref <31,64,4> (mmio_sigs);
-//     uart_input_thread = std::thread (uart_input,std::ref(uart));
 //     assert(mmio.add_dev(0x60100000,1024*1024,&uart));
 //     mem.load_binary(img_file,0x80000000);
 
 // }
+
+void sim_soc_init(VTop *top) {
+    connect_wire(mmio_ptr,mem_ptr,top);
+    assert(mmio_ptr.check());
+    assert(mem_ptr.check());
+    mmio_ref = axi4_ref <31,64,4> (mmio_ptr);
+    mmio_sigs_ref = axi4_ref <31,64,4> (mmio_sigs);
+    uart_input_thread = std::thread (uart_input,std::ref(uart));
+    assert(mmio.add_dev(0x60100000,1024*1024,&uart));
+    mem.load_binary(img_file,0x80000000);
+}
 
 unsigned long ticks = 0;
 long max_trace_ticks = 1000;
