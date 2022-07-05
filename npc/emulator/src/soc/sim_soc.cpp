95fee90189cf23faee23f5954092f72e8cd7f313
GreensCH <changqiaojia@163.com>
2022-07-05 18:15:29 +0800

    unfinish 20220705

---
+++ npc/emulator/src/soc/sim_soc.cpp
@@ -0,0 +1,12 @@
+#include "include.h"
+
+#include "axi4.hpp"
+#include "axi4_mem.hpp"
+#include "axi4_xbar.hpp"
+#include "mmio_mem.hpp"
+#include "uartlite.hpp"
+
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+#include <thread>
