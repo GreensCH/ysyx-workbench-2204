2fe890179771354396d03b10801f496b62f7c241
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 22:14:44 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:14:44 up  7:43,  3 users,  load average: 0.81, 0.55, 0.28

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -112,7 +112,7 @@ class ICache extends Module{
   }
 // Data
   val pc_index = pc(3, 2)
-  val miss_reg = RegInit(0.U.asTypeOf((new IFUOut)))
+  val miss_reg = RegInit(0.U.asTypeOf (new IFUOut))
   val cache_line_in = WireDefault(0.U(128.W)) // soc datasheet [PARA]
   val shift_reg_in = Wire(UInt(64.W)) // soc datasheet [PARA]
   val shift_reg_en = Wire(Bool())
@@ -132,7 +132,7 @@ class ICache extends Module{
     miss_reg.bits.if2id.pc := pc
     miss_reg.bits.if2id.inst := inst_out
   }
-//  next.valid := next.valid & miss_reg.valid
+  next.valid := next.valid & miss_reg.valid
 // Data Output
   next.bits.if2id := miss_reg.bits.if2id
 
