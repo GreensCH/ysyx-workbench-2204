fe3e33796155bb0ce53bc12cee938dfb794d841a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-11 22:55:54 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:55:54 up 12:16,  2 users,  load average: 1.52, 0.88, 0.74

--- npc/playground/src/IFU.scala
+++ npc/playground/src/IFU.scala
@@ -22,7 +22,6 @@ class PC extends Module {
     // if jump is true and ready is false then lock the pc
     // when jump_latch === pc_reg, then clear the latch
     val clear_latch = Wire(Bool())
-  dontTouch(clear_latch)
     val jump_latch = RegEnable(next = jump_pc, init = 0.U(64.W), enable = jump & !io.next.ready | clear_latch)
     val jump_status_latch = RegEnable(next = jump, init = false.B, enable = jump & !io.next.ready | clear_latch)
     val jump_pc_out = Mux(jump, jump_pc, jump_latch)
@@ -34,6 +33,9 @@ class PC extends Module {
     /* connection */
     dataNext.pc := pc_reg
     io.next.valid := true.B
+  /* stay */
+  dontTouch(clear_latch)
+  dontTouch(pc_reg_in)
 }
 
 
