2d73b37d1863b51fadaf0e68ee7e8739da6214db
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 17:47:32 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:47:32 up  7:05,  3 users,  load average: 0.43, 0.82, 0.82

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -177,7 +177,10 @@ class ICache extends Module{
     ctrl_valid := false.B
     ctrl_ready := true.B
   }.elsewhen(curr_state === sLOOKUP){
-    when(miss){
+    when(prev.valid === false.B){
+        ctrl_valid := false.B
+        ctrl_ready := true.B
+    }.elsewhen(miss){
         ctrl_valid := false.B
         ctrl_ready := false.B
     }.otherwise{
