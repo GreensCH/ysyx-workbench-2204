e8bd25681727a548dbd939e8125b9fb48eb19392
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-13 17:10:16 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:10:16 up  6:28,  3 users,  load average: 2.00, 0.70, 0.43

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -237,7 +237,7 @@ val rw_data = MuxLookup(key = prev.bits.pc2if.pc(3, 2), default = 0.U(32.W), map
   }.elsewhen(curr_state === sRCATCH/* next_state === sRWRITE */){
     temp_valid_reg := prev.valid
 //    temp_data_reg.if2id.pc := prev.bits.pc2if.pc
-//    temp_data_reg.if2id.inst := rw_data
+    temp_data_reg.if2id.inst := rw_data
   }
   .elsewhen(curr_state === sRWRITE){
     temp_valid_reg := prev.valid
