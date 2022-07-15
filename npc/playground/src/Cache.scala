35d7ad0192ebdbac0b23cd350c8146044a0d4ed0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 11:58:23 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:58:23 up  2:13,  2 users,  load average: 2.10, 1.73, 0.94

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -263,8 +263,8 @@ class ICache(id: UInt) extends CacheBase[ICacheIn, ICacheOut](id = id, _in = new
     Main Internal Data Signal
    */
   lkup_stage_en := prev.ready
-  lkup_stage_in.data.addr := prev.bits.data.pc2if.pc
-  lkup_stage_in.data.data := DontCare
+  lkup_stage_in.bits.addr := prev.bits.data.pc2if.pc
+  lkup_stage_in.bits.data := DontCare
   lkup_stage_in.valid := prev.valid
   /*
    SRAM LRU
