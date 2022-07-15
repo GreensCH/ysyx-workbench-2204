a8f220acbdea26d54d8f2b336b499fa269e464e9
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 00:23:40 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:23:40 up 14:38,  2 users,  load average: 1.76, 0.97, 0.58

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -320,10 +320,11 @@ class ICache(id: UInt) extends CacheBase[ICacheIn, ICacheOut](id = id, _in = new
     "b10".U(2.W) -> cache_line_data_out(95,64),
     "b11".U(2.W) -> cache_line_data_out(127,96)
   ))
-  next.bits.data := MuxCase(0.U.asTypeOf((new ICacheOut).bits), Array(
+  next.bits.data := MuxCase(0.U.asTypeOf((new ICacheOut).bits.data), Array(
     (curr_state === sLBACK) -> bus_out.data,
     (curr_state === sLOOKUP) -> cache_out.data,
   ))
+  next.bits.data := Mux(is_bus_out, bus_out.data, cache_out.data)
 }
 
 //class DCache (id: UInt)  extends CacheBase(id){
