6b8b9449eab4352597553a0442c261a511c1f819
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 10:31:19 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:31:19 up 46 min,  2 users,  load average: 1.71, 1.98, 1.05

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -103,7 +103,7 @@ object SRAM{
 
 class CacheBaseIn extends MyDecoupledIO{
   override val bits = new Bundle{
-    val data = Bool()
+    val data = new Bundle{}
     val addr = Input(UInt(CacheCfg.paddr_bits.W))
   }
 }
