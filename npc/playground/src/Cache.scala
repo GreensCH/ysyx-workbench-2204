bc759f8e88fcbb070ab69ce5c2e42745ce46619c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 12:46:39 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:46:39 up  3:01,  2 users,  load average: 2.58, 1.42, 1.10

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -192,7 +192,8 @@ class CacheBase[IN <: CacheBaseIn, OUT <: CacheBaseOut] (val id: UInt, _in: IN ,
   protected val r_data = memory.r.bits.data
   protected val tag0_hit = tag_rdata_out_0 === stage_tag
   protected val tag1_hit = tag_rdata_out_1 === stage_tag
-  protected val miss = !(tag0_hit | tag1_hit)
+  protected val tag_invalid = tag_rdata_out_0 =/= 0.U | tag_rdata_out_1 =/= 0.U
+  protected val miss = !(tag0_hit | tag1_hit) | tag_invalid
   /*
    Main Internal Data Signal
    */
