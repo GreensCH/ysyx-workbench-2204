389d1b54c9a6b5f72fecb587ac2ac4e54fc259f7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 13:18:25 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:18:25 up  3:33,  2 users,  load average: 1.28, 0.80, 0.94

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -173,7 +173,6 @@ class CacheBase[IN <: CacheBaseIn, OUT <: CacheBaseOut] (val id: UInt, _in: IN ,
   protected val lkup_stage_out = RegEnable(next = lkup_stage_in, enable = lkup_stage_en)
   /* AXI Read Channel Stage */
   protected val r_stage_in = Wire(UInt(AXI4Parameters.dataBits.W))
-  r_stage_in := memory.r.bits.data
   protected val r_stage_out = RegNext(init = 0.U(AXI4Parameters.dataBits.W), next = r_stage_in)
   /*
    Main Data Reference
@@ -196,6 +195,8 @@ class CacheBase[IN <: CacheBaseIn, OUT <: CacheBaseOut] (val id: UInt, _in: IN ,
   /*
    Main Internal Data Signal
    */
+  //lkup_stage_in should defined in submodule
+  r_stage_in := Mux(curr_state === sREAD & !r_last, memory.r.bits.data)
   protected val bus_rdata_out = Cat(memory.r.bits.data, r_stage_out)//cat(64, 64) -> total out 128 bits
   protected val cache_line_data_out = MuxCase(0.U(CacheCfg.cache_line_bits.W), Array(
     tag0_hit -> data_rdata_out_0,
