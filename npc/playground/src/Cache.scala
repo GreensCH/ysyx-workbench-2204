e762ab3a94ec3a94d419758663f041da2eab4b22
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-15 10:28:09 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:28:09 up 42 min,  2 users,  load average: 2.60, 1.97, 0.82

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -168,13 +168,13 @@ class CacheBase[IN <: CacheBaseIn, OUT <: CacheBaseOut] (val id: UInt, _in: IN ,
   Stage
  */
   /* Lookup Stage */
-  class lkup_stage_type extends Bundle {
+  val lkup_stage_type = new Bundle {
     val valid = Input(Bool())
     val data = Input(_in.bits)
   }
   protected val lkup_stage_en = Wire(Bool())
-  protected val lkup_stage_in = Wire(new lkup_stage_type)
-  protected val lkup_stage_out = RegEnable(init = 0.U.asTypeOf(new lkup_stage_type), next = lkup_stage_in, enable = lkup_stage_en)
+  protected val lkup_stage_in = WireDefault(0.U.asTypeOf(lkup_stage_type))
+  protected val lkup_stage_out = RegEnable(init = 0.U.asTypeOf(lkup_stage_type), next = lkup_stage_in, enable = lkup_stage_en)
   /* AXI Read Channel Stage */
   protected class r_stage_type extends Bundle { val data = Input((new AXI4BundleR).bits.data) }
   protected val r_stage_in = Wire(new r_stage_type)
