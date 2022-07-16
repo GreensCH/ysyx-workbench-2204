147b95c7693542841e88ee9ba02c0c359b439570
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 11:26:19 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:26:19 up 54 min,  3 users,  load average: 1.03, 1.19, 1.15

--- npc/playground/src/MEMU.scala
+++ npc/playground/src/MEMU.scala
@@ -30,7 +30,6 @@ class MEMU extends Module {
 //    val maxi  = new AXI4
 //    val mmio  = new AXI4
   })
-
   if(SparkConfig.DCache){
     /* inst cache instance */
 //    val dcache = Module(new DCache)
@@ -45,54 +44,7 @@ class MEMU extends Module {
 //    io.prev.ready := io.next.ready & dcache.io.prev.ready
 //    io.next.valid := io.prev.valid & dcache.io.next.valid
   }else{
-    val idb = io.prev.bits.id2mem
-    val exb = io.prev.bits.ex2mem
-    val wbb = io.next.bits.mem2wb
-    io.next.bits.ex2wb := io.prev.bits.ex2wb
-    io.next.bits.id2wb := io.prev.bits.id2wb
-    io.prev.ready := io.next.ready
-    io.next.valid := io.prev.valid
-    /* MEMU interface */
-    val byte  = idb.size.byte
-    val hword = idb.size.hword
-    val word  = idb.size.word
-    val dword = idb.size.dword
-    val sext_flag = idb.sext_flag
-    /* memory bus instance */
-    val memory_inf = Module(new MemoryInf).io
-    /* memory interface */
-    val rd_en   = idb.memory_rd_en
-    val rd_addr = exb.rd_addr
-    val rd_data = memory_inf.rd_data
-    val we_en   = idb.memory_we_en
-    val we_addr = exb.we_addr
-    val we_data = exb.we_data
-    val we_mask = exb.we_mask
-    memory_inf.rd_en   := rd_en
-    memory_inf.rd_addr := rd_addr
-    memory_inf.we_en   := we_en
-    memory_inf.we_addr := we_addr
-    memory_inf.we_data := we_data
-    memory_inf.we_mask := we_mask
-
-    val raw_memory_data = MuxCase(memory_inf.rd_data,
-      Array(
-        byte   -> memory_inf.rd_data(7,  0),
-        hword  -> memory_inf.rd_data(15, 0),
-        word   -> memory_inf.rd_data(31, 0),
-        dword  -> memory_inf.rd_data,
-      )
-    )
-    val sext_memory_data = MuxCase(memory_inf.rd_data,
-      Array(
-        byte   -> Sext(data = memory_inf.rd_data(7,  0), pos = 8),
-        hword  -> Sext(data = memory_inf.rd_data(15, 0), pos = 16),
-        word   -> Sext(data = memory_inf.rd_data(31, 0), pos = 32),
-        dword  -> memory_inf.rd_data
-      )
-    )
-    /* mem2wb interface */
-    wbb.memory_data := Mux(sext_flag, sext_memory_data, raw_memory_data)
+    MEMU.dpic_load_save(io.prev, io.next)
   }
 }
 
@@ -117,5 +69,55 @@ object MEMU {
     fwu.dst_addr := EX2MEMReg.io.next.bits.id2wb.regfile_we_addr
     fwu.dst_data := Mux(EX2MEMReg.io.next.bits.id2mem.memory_rd_en, memu.io.next.bits.mem2wb.memory_data, EX2MEMReg.io.next.bits.ex2wb.result_data)
     memu
+  }
+  def dpic_load_save( prev: EXUOut, next: MEMUOut): Unit = {
+    prev.ready := next.ready
+    next.valid := prev.valid
+    next.bits.ex2wb := prev.bits.ex2wb
+    next.bits.id2wb := prev.bits.id2wb
+    val idu = prev.bits.id2mem
+    val exu = prev.bits.ex2mem
+    val wbu = next.bits.mem2wb
+    /* MEMU interface */
+    val byte  = idu.size.byte
+    val hword = idu.size.hword
+    val word  = idu.size.word
+    val dword = idu.size.dword
+    val sext_flag = idu.sext_flag
+    /* memory bus instance */
+    val memory_inf = Module(new MemoryInf).io
+    /* memory interface */
+    val rd_en   = idu.memory_rd_en
+    val rd_addr = exu.rd_addr
+    val rd_data = memory_inf.rd_data
+    val we_en   = idu.memory_we_en
+    val we_addr = exu.we_addr
+    val we_data = exu.we_data
+    val we_mask = exu.we_mask
+    memory_inf.rd_en   := rd_en
+    memory_inf.rd_addr := rd_addr
+    memory_inf.we_en   := we_en
+    memory_inf.we_addr := we_addr
+    memory_inf.we_data := we_data
+    memory_inf.we_mask := we_mask
+
+    val raw_memory_data = MuxCase(memory_inf.rd_data,
+      Array(
+        byte   -> memory_inf.rd_data(7,  0),
+        hword  -> memory_inf.rd_data(15, 0),
+        word   -> memory_inf.rd_data(31, 0),
+        dword  -> memory_inf.rd_data,
+      )
+    )
+    val sext_memory_data = MuxCase(memory_inf.rd_data,
+      Array(
+        byte   -> Sext(data = memory_inf.rd_data(7,  0), pos = 8),
+        hword  -> Sext(data = memory_inf.rd_data(15, 0), pos = 16),
+        word   -> Sext(data = memory_inf.rd_data(31, 0), pos = 32),
+        dword  -> memory_inf.rd_data
+      )
+    )
+    /* mem2wb interface */
+    wbu.memory_data := Mux(sext_flag, sext_memory_data, raw_memory_data)
   }
 }
