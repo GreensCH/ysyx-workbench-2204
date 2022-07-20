265912d2ad42d884e7915630661bfcf8b262ef25
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 13:53:53 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:53:53 up  3:13,  4 users,  load average: 3.44, 1.40, 0.92

--- npc/playground/src/MEMU.scala
+++ npc/playground/src/MEMU.scala
@@ -200,7 +200,7 @@ object MEMU {
       }
       is(sWRITE_1){
         when(overborder)        { next_state := sWRITE_2  }
-        .elsewhen(next.ready)   { next_state := sIDLE }
+        .elsewhen(next.ready & maxi.b.valid)   { next_state := sIDLE }
         .otherwise              { next_state := sWRITE_1 }
       }
       is(sREAD_2){
@@ -208,7 +208,7 @@ object MEMU {
         .otherwise       { next_state := sREAD_2 }
       }
       is(sWRITE_2){
-        when(next.ready) { next_state := sIDLE }
+        when(next.ready & maxi.b.valid) { next_state := sIDLE }
         .otherwise       { next_state := sWRITE_2 }
       }
       is(sEND){
