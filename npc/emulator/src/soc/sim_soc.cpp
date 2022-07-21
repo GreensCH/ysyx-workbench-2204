bb578afcb30cf8985fe29be793e572d6f0c3a4b7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 10:37:54 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:37:54 up 17:48,  4 users,  load average: 0.93, 1.22, 0.81

--- npc/emulator/src/soc/sim_soc.cpp
+++ npc/emulator/src/soc/sim_soc.cpp
@@ -120,7 +120,7 @@ void sim_soc_mem_read(word_t addr){
     char temp[16];
     memset(temp, 0, 16);
     mem.read((off_t)addr, (size_t)16, (uint8_t *)temp);
-    for(int i = 0; i < 8; i++){
+    for(int i = 0; i < 16; i++){
         int a = (int)(temp[15-i]);
         a= a<0 ? -a :a;
         if( a < 16)
