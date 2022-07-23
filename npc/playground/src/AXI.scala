f3ea1a1a7498168c46f5d1b798cd0a92ba181e29
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 11:43:39 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:43:39 up 16:56,  4 users,  load average: 0.29, 0.52, 0.70

--- npc/playground/src/AXI.scala
+++ npc/playground/src/AXI.scala
@@ -20,7 +20,7 @@ object AXI4Parameters extends CoreParameter {
   val respBits  = 2
 
   // These are not fixed:
-  val idBits    = 1
+  val idBits    = 4
   val addrBits  = PAddrBits
   val dataBits  = DataBits
   val userBits  = 1
