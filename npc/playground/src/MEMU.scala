a071809acf08e3f6f1124df63e1addc43261fe4c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-24 23:48:47 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:48:47 up 13:50,  3 users,  load average: 2.59, 1.44, 1.13

--- npc/playground/src/MEMU.scala
+++ npc/playground/src/MEMU.scala
@@ -132,13 +132,13 @@ object MEMU {
       next.bits.ex2wb := stage.ex2wb
       next.bits.mem2wb.memory_data := read_data
       next.valid := true.B
-      next.bits.mem2wb.is_device := true.B
+      next.bits.mem2wb.test_is_device := true.B
     }.elsewhen(busy | valid){
       next.bits.id2wb  := 0.U.asTypeOf(new ID2WB )
       next.bits.ex2wb  := 0.U.asTypeOf(new EX2WB )
       next.bits.mem2wb := 0.U.asTypeOf(new MEM2WB)
       next.valid := false.B
-      next.bits.mem2wb.is_device := true.B
+      next.bits.mem2wb.test_is_device := true.B
     }
     prev.ready := !(busy | valid) | axi4_manager.io.out.finish
 
