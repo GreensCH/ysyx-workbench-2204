98507c5f644c1e9835ea57f96f52bce71fbcedc6
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 01:35:49 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:35:49 up 14:23,  3 users,  load average: 3.15, 2.31, 1.94

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -457,14 +457,14 @@ class DCacheBase[IN <: DCacheBaseIn, OUT <: DCacheBaseOut] (_in: IN, _out: OUT) 
    */
   axi_rd_en := false.B
   axi_we_en := false.B
-  when      (next_state === sREAD       ) { axi_rd_en := true.B }
-  .elsewhen (next_state === sWRITEBACK  ) { axi_we_en := true.B }
-  .elsewhen (next_state === sFLUSH      ){// flush situation
+  when      (true.B/*next_state === sREAD       */) { axi_rd_en := true.B }
+  .elsewhen (true.B/*next_state === sWRITEBACK  */) { axi_we_en := true.B }
+  .elsewhen (true.B/*next_state === sFLUSH      */){// flush situation
     when(curr_state === sLOOKUP)        { axi_we_en := true.B }
     .elsewhen(true.B/*axi_finish*/)               { axi_we_en := true.B }
   }
-  .elsewhen (next_state === sRWAIT)     { axi_rd_en := true.B }
-  .elsewhen (next_state === sWWAIT)     { axi_we_en := true.B }
+  .elsewhen (true.B/*next_state === sRWAIT*/)     { axi_rd_en := true.B }
+  .elsewhen (true.B/*next_state === sWWAIT*/)     { axi_we_en := true.B }
 
   axi_addr := MuxCase(stage2_out.bits.addr, Array(
     (curr_state === sLOOKUP) -> stage1_out.bits.addr,
