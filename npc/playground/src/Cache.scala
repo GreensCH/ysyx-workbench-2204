207861dce24ebcb83f41b980cac4cb85c9733478
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 15:22:29 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:22:29 up  6:14,  4 users,  load average: 2.77, 1.30, 1.53

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -573,9 +573,7 @@ class DCacheUnit extends DCacheBase[DCacheIn, DCacheOut](_in = new DCacheIn, _ou
     is(sFLUSH){ when(flush_cnt_end){ next_state := sLOOKUP } }
   }
   /* data read */
-  private val _is_lookup = Wire(Bool())
-  _is_lookup := curr_state === sLOOKUP
-  dontTouch(_is_lookup)
+  private val _is_lookup = curr_state === sLOOKUP
   private val _is_save = curr_state === sSAVE
   private val read_data_128    = Mux(_is_lookup, cache_line_data_out, axi_rd_data)
   private val read_data_size   = Mux(_is_lookup, prev.bits.size, stage1_out.bits.size)
@@ -643,9 +641,10 @@ class DCacheUnit extends DCacheBase[DCacheIn, DCacheOut](_in = new DCacheIn, _ou
    Output
   */
   prev.ready := _is_lookup & next.ready
-  next.bits.data.id2wb := Mux(curr_state === sLOOKUP, stage1_out.bits.data.id2wb, 0.U.asTypeOf(chiselTypeOf(stage1_out.bits.data.id2wb)))//stage1_out.bits.data.id2wb
-  next.bits.data.ex2wb := Mux(curr_state === sLOOKUP, stage1_out.bits.data.ex2wb, 0.U.asTypeOf(chiselTypeOf(stage1_out.bits.data.ex2wb)))//stage1_out.bits.data.ex2wb
-  next.valid := Mux(curr_state === sLOOKUP, stage1_out.valid, false.B)
+
+  next.bits.data.id2wb := Mux(curr_state === sLOOKUP | (curr_state === sREAD & axi_finish), stage1_out.bits.data.id2wb, 0.U.asTypeOf(chiselTypeOf(stage1_out.bits.data.id2wb)))//stage1_out.bits.data.id2wb
+  next.bits.data.ex2wb := Mux(curr_state === sLOOKUP | (curr_state === sREAD & axi_finish), stage1_out.bits.data.ex2wb, 0.U.asTypeOf(chiselTypeOf(stage1_out.bits.data.ex2wb)))//stage1_out.bits.data.ex2wb
+  next.valid           := Mux(curr_state === sLOOKUP | (curr_state === sREAD & axi_finish), stage1_out.valid, false.B)
   next.bits.data.mem2wb.memory_data := read_data
   next.bits.data.mem2wb.test_is_device := DontCare
 }
