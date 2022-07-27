95d5f1b38e771b8578fd6c14be673556926e87f3
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-27 11:55:19 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:55:19 up 19:14,  4 users,  load average: 0.67, 0.67, 0.89

--- npc/playground/src/FWU.scala
+++ npc/playground/src/FWU.scala
@@ -184,10 +184,10 @@ class FWU extends Module{
   idb.fw_src2_data := MuxCase(id_data2,
     Array(
       (optype.Itype) -> id_data2,
-      (eq1_ex)       -> ex_data,
-      (eq1_mem1)     -> mem_data_1,
-      (eq1_mem2)     -> mem_data_2,
-      (eq1_wb)       -> wb_data
+      (eq2_ex)       -> ex_data,
+      (eq2_mem1)     -> mem_data_1,
+      (eq2_mem2)     -> mem_data_2,
+      (eq2_wb)       -> wb_data
     )
   )
 
