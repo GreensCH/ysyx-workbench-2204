b65498f5b1f1c9f2e65afeb096c9a5c81134e4e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 19:25:57 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:57 up  3:08,  3 users,  load average: 1.91, 1.48, 0.98

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -625,7 +625,7 @@ class DCacheUnit extends DCacheBase[DCacheIn, DCacheOut](_in = new DCacheIn, _ou
   private val save_data_size       = stage1_out.bits.size
   private val save_data_size_2     = Cat(save_data_size.dword, save_data_size.word, save_data_size.hword, save_data_size.byte)
   private val save_start_byte_rshift = stage1_out.bits.addr(3, 0)
-  private val save_start_bit_rshift  = (save_start_byte_rshift << 3).asUInt() + (save_data_size_2 << 3).asUInt()
+  private val save_start_bit_rshift  = (save_start_byte_rshift << 3).asUInt() //+ (save_data_size_2 << 3).asUInt()
   private val save_start_bit_lshift = 128.U - (save_start_byte_rshift << 3).asUInt()
 
   dontTouch( _is_save)
