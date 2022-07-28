0af2fa45a5692fc8fa5188c4a1d930f94a80eb6b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 18:34:46 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:34:46 up  2:17,  3 users,  load average: 0.69, 1.30, 1.29

--- npc/playground/src/Util.scala
+++ npc/playground/src/Util.scala
@@ -12,10 +12,10 @@ object  Sext{
 object  Replace{//size is byte scalar. start, len is bit scalar
   def apply (src: UInt, token: UInt, rshift: UInt, lshift: UInt, len: Int = 128) : UInt = {
 
-    val _L = ((src >> rshift) << rshift).asTypeOf(UInt(len.W))
-    val _R = ((src << lshift) >> lshift).asTypeOf(UInt(len.W))
-    val _M = (token << rshift).asTypeOf(UInt(len.W))
+    val L = ((src >> rshift) << rshift).asTypeOf(UInt(len.W))
+    val R = ((src << lshift) >> lshift).asTypeOf(UInt(len.W))
+    val M = (token << rshift).asTypeOf(UInt(len.W))
 
-    _L | _M  | _R
+    L | M  | R
   }
 }
