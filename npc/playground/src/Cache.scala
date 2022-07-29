e378138585f7e689bbc8ff8134854537bba7141f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 17:08:10 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:08:10 up  7:08,  2 users,  load average: 2.17, 2.16, 1.81

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -765,15 +765,15 @@ class DCacheUnit extends DCacheBase[DCacheIn, DCacheOut](_in = new DCacheIn, _ou
     }
     when(next.bits.data.id2wb.ebreak){
       printf("--------------------Cache Hit Table-------------------------\n")
-      printf(p"Total cache hit rate: ${(100.U * (way0_load_hit_cnt + way0_save_hit_cnt + way1_load_hit_cnt + way1_save_hit_cnt))/(way0_cnt + way1_cnt)}\n")
+      printf(p"Total cache hit rate: ${(100.U * (way0_load_hit_cnt + way0_save_hit_cnt + way1_load_hit_cnt + way1_save_hit_cnt))/(load_cnt + save_cnt)}\n")
       printf("------------------------------------------------------------\n")
       printf(p" way0 cache hit rate: ${(100.U * (way0_load_hit_cnt + way0_save_hit_cnt))/(load_cnt + save_cnt)}\n")
-      printf(p" way0  load hit rate: ${(100.U * (way0_load_hit_cnt))/(way0_load_hit_cnt + way1_load_hit_cnt)}\n")
-      printf(p" way0  save hit rate: ${(100.U * (way0_save_hit_cnt))/(way0_save_hit_cnt + way1_save_hit_cnt)}\n")
+      printf(p" way0 load proportion: ${(100.U * (way0_load_hit_cnt))/(way0_load_hit_cnt + way1_load_hit_cnt)}\n")
+      printf(p" way0 save proportion: ${(100.U * (way0_save_hit_cnt))/(way0_save_hit_cnt + way1_save_hit_cnt)}\n")
       printf("------------------------------------------------------------\n")
-      printf(p" way1  load hit rate: ${(100.U * (way1_load_hit_cnt))/(way1_load_hit_cnt + way1_load_hit_cnt)}\n")
-      printf(p" way1  save hit rate: ${(100.U * (way1_save_hit_cnt))/(way0_save_hit_cnt + way1_save_hit_cnt)}\n")
       printf(p" way1 cache hit rate: ${(100.U * (way1_load_hit_cnt + way1_save_hit_cnt))/(load_cnt + save_cnt)}\n")
+      printf(p" way1 load proportion: ${(100.U * (way1_load_hit_cnt))/(way1_load_hit_cnt + way1_load_hit_cnt)}\n")
+      printf(p" way1 save proportion: ${(100.U * (way1_save_hit_cnt))/(way0_save_hit_cnt + way1_save_hit_cnt)}\n")
       printf("------------------------------------------------------------\n")
     }
 
