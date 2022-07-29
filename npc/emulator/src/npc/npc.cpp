beb063330052ad4764c82573b5adf4684dd1c6f5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 01:37:11 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:37:11 up  7:06,  4 users,  load average: 1.01, 0.93, 1.08

--- npc/emulator/src/npc/npc.cpp
+++ npc/emulator/src/npc/npc.cpp
@@ -39,7 +39,6 @@ void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     printf(ASNI_FG_RED "VLT Register List" ASNI_NONE "\n");
     common_reg_display(&ref);
     printf("-------------\n");
-    return;
   }
   //ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
   IFDEF(CONFIG_ITRACE, add_itrace(_this->logbuf);)
