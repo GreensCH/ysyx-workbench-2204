7365957572c44a665add885023332f57654eeb56
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 22:01:01 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:01:01 up  3:30,  3 users,  load average: 4.13, 1.41, 0.49

--- npc/playground/src/MEMU.scala
+++ npc/playground/src/MEMU.scala
@@ -68,11 +68,12 @@ class MEMU extends Module {
     mmio_unit.io.prev.bits <> 0.U.asTypeOf(chiselTypeOf(prev.bits))
     mmio_unit.io.next.bits <> 0.U.asTypeOf(chiselTypeOf(next.bits))
     mmio_unit.io.next.ready := next.ready
+    mmio_unit.io.prev.valid := prev.valid & load_save & addr_underflow
 //    when(load_save & addr_underflow & dcache_ready){
 //      mmio_unit.io.prev <> prev
 //      mmio_unit.io.next <> next
 //    }
-//    mmio_unit.io.prev.valid := prev.valid & load_save & addr_underflow
+//
   }
 //else if(SparkConfig.MEMU == 2){
 //    val dcache = Module(new DCacheUnit)
