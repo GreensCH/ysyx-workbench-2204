d7f6ead8f9a834a76f4053d5c2bed810b481dccd
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 01:48:27 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:48:27 up  7:17,  4 users,  load average: 0.94, 1.11, 1.08

--- npc/emulator/src/npc/npc.cpp
+++ npc/emulator/src/npc/npc.cpp
@@ -28,13 +28,12 @@ void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     return;//流水线空泡
   else 
     cmp = cpu.pc;//正常情况
-  // if(cpu_device){
-  //   difftest_skip_ref();
-  // }
-  // CPU_state ref;
-  // ref_difftest_regcpy(&ref, DIFFTEST_TO_DUT);
-  // if(ref.pc == 0){ difftest_skip_ref(); }
-  
+  if(cpu_device){
+    difftest_skip_ref();
+  }
+  CPU_state ref;
+  ref_difftest_regcpy(&ref, DIFFTEST_TO_DUT);
+  if(ref.pc == 0){ difftest_skip_ref(); }
   //ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
   IFDEF(CONFIG_ITRACE, add_itrace(_this->logbuf);)
   IFDEF(CONFIG_FTRACE, ftrace_log(_this, dnpc);)
