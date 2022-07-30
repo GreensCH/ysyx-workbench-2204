1eb3d956b8943b2964ff19a2c86db1c3d03274e5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 11:17:00 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:17:00 up  8:07,  4 users,  load average: 0.32, 0.14, 0.27

--- npc/playground/src/Core.scala
+++ npc/playground/src/Core.scala
@@ -4,7 +4,7 @@ import chisel3.util._
 
 
 object SparkConfig{
-  val ICache: Boolean = false
+  val ICache: Boolean = true
   val MEMU: Int = 2//0 -dpic 1-axi 2-dcache
   val ChiselRam: Boolean = true
   val CacheHitCount: Boolean = true
