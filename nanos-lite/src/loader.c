a06528d34c8988ccbca48a5741deb63f85c1ee81
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 23:08:01 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:08:01 up  8:04,  1 user,  load average: 0.50, 0.27, 0.19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,8 +15,12 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //因为ramdisk中目前只有一个文件, filename参数也可以忽略.
   size_t ramdisk_read(void *buf, size_t offset, size_t len);
   size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-  void init_ramdisk(); 
+  void init_ramdisk();
+  Elf_Ehdr ehdr;
+  // Elf_Ehdr phdr;
   init_ramdisk();
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  printf("size:%lx, num:%lx\n", ehdr.e_phentsize, ehdr.e_phnum);
   return 0;
 }
 
