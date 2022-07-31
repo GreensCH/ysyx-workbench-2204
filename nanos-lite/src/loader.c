2d458cf1b8fa0a2f4edadb26ec30e53a30a69105
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 01:17:35 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:17:35 up 10:14,  1 user,  load average: 0.02, 0.17, 0.24

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,31 +20,30 @@
 #elif
 # error unsupported ISA __ISA__
 #endif
+
+static uintptr_t loader(PCB *pcb, const char *filename) {
+  //TODO();
+  //pcb参数目前暂不使用, 可以忽略
+  //因为ramdisk中目前只有一个文件, filename参数也可以忽略.
   void init_ramdisk();
   size_t ramdisk_read(void *buf, size_t offset, size_t len);
   size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+  
+  Elf_Ehdr elf;
+  ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)elf.e_ident == 0x464C457F);//F L E 0x7f
+  assert(EXPECT_TYPE == elf.e_machine);
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  // Log("[Loader] ELF file is reading from '%s'.", filename);
-  // ELF Header
-  Elf_Ehdr *elf = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
-  ramdisk_read(elf, 0, sizeof(Elf_Ehdr));
-  // Check ELF magic number
-  Log("elf->e_ident = 0x%p", *(uint32_t *)elf->e_ident);
-  assert(*(uint32_t *)elf->e_ident == 0x464C457F);
-  // Checl ELF machine
-  Log("elf->e_machine = 0x%p", elf->e_machine);
-  assert(EXPECT_TYPE == elf->e_machine);
-  // Program Header
-  Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf->e_phnum);
-  ramdisk_read(phdr, elf->e_phoff, sizeof(Elf_Phdr) * elf->e_phnum);
-  // Analysis of program header table
-  for (int i = 0; i < elf->e_phnum; ++i) {
-    if (phdr[i].p_type != PT_LOAD) continue;
-    ramdisk_read((char*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
-    memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
+  Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf.e_phnum);
+  ramdisk_read(phdr, elf.e_phoff, sizeof(Elf_Phdr) * elf.e_phnum);
+
+  for (int i = 0; i < elf.e_phnum; i++) {
+    if(phdr[i].p_type != PT_LOAD) continue;
+    ramdisk_read((char*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);//read program
+    memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);//set data 0
   }
-  return elf->e_entry;
+
+  return 0;//elf.e_entry;
 }
 // static uintptr_t loader(PCB *pcb, const char *filename) {
 //   // Log("[Loader] ELF file is reading from '%s'.", filename);
