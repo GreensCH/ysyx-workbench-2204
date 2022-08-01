61960d21a0f050f25d7d660c804e9dbc4d012d76
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 17:27:13 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:27:13 up  3:39,  1 user,  load average: 0.00, 0.18, 0.29

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,8 +9,12 @@ void do_syscall(Context *c) {
       halt(0);
     break;
     case SYS_yield:
+      //dummy程序, 它触发了一个SYS_yield系统调用. 我们约定, 这个系统调用直接调用CTE的yield()即可, 然后返回0
       yield();
       c->GPRx = 0;
+      //处理系统调用的最后一件事就是设置系统调用的返回值. 
+      //对于不同的ISA, 系统调用的返回值存放在不同的寄存器中, 
+      //宏GPRx用于实现这一抽象, 所以我们通过GPRx来进行设置系统调用返回值即可.
     break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
