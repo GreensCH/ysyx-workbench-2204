0f0920ebd7168ae8ac9ba81d0589f322035a538d
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 18:31:39 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:31:39 up  4:43,  1 user,  load average: 0.58, 0.29, 0.13

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -31,6 +31,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   printf("hander%d\n",c->mcause);
+  printf("GPR1%d\n",c->GPR1);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
@@ -39,7 +40,7 @@ Context* __am_irq_handle(Context *c) {
       case  1: ev.event = EVENT_SYSCALL; c->mepc += 4;break; // yield
       case  4: ev.event = EVENT_SYSCALL; c->mepc += 4;break; // write
       case 11:
-        printf("GPR1%d\n",c->GPR1);
+        
         if(c->GPR1 == -1){
           ev.event = EVENT_YIELD;
           c->mepc += 4;
