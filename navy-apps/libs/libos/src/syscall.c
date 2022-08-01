90ac422eb72967a6dc7d407a5021a20e4ba10141
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 23:14:29 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:14:29 up  9:26,  1 user,  load average: 0.59, 0.31, 0.22

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -64,19 +64,28 @@ int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
+extern char _end;
 void *_sbrk(intptr_t increment) {
-  char test[64];//test
-  static int _end = 0;
+  static intptr_t brk = (intptr_t)&_end;//记录开始位置
   if(_syscall_(SYS_brk, increment, 0, 0) == 0){
-    int old = _end;
-    _end += increment;
-    sprintf(test, "old%d new%d\n", old, _end);
-    _write(1, test, 64);
+    intptr_t old = brk;
+    brk += increment;
     return (void *)old;
   }
   return (void *)-1;
 }
-
+// extern char _end;//声明外部变量
+// static intptr_t brk=(intptr_t)&_end;//记录开始位置
+// void *_sbrk(intptr_t increment){  
+//   intptr_t pre = brk;  
+//   intptr_t now=pre+increment;//记录增加后的位置  
+//   intptr_t res = _syscall_(SYS_brk,now,0,0);//系统调用  
+//   if (res==0){//若成功，则返回原位置    
+//   brk=now;    
+//   return (void*)pre;  
+//   }//否则返回-1
+//   return (void *)-1;
+// }
 int _read(int fd, void *buf, size_t count) {
   _exit(SYS_read);
   return 0;
