43668786f893054bf2ed5da02d966c383ebf48f1
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 18:33:19 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:19 up  4:45,  1 user,  load average: 0.29, 0.25, 0.12

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -31,25 +31,13 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   printf("hander%d\n",c->mcause);
-  printf("GPR1%d\n",c->GPR1);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case -1: ev.event = EVENT_SYSCALL; c->mepc += 4;break;   // yield
-      case  0: ev.event = EVENT_SYSCALL; c->mepc += 4;break; // exit
-      case  1: ev.event = EVENT_SYSCALL; c->mepc += 4;break; // yield
-      case  4: ev.event = EVENT_SYSCALL; c->mepc += 4;break; // write
-      case 11:
-        
-        if(c->GPR1 == -1){
-          ev.event = EVENT_YIELD;
-          c->mepc += 4;
-        }
-        else{
-          ev.event = EVENT_YIELD;
-          c->mepc += 4;
-        }
-        break;
+      case -1: ev.event = EVENT_SYSCALL; break;   // yield
+      case  0: ev.event = EVENT_SYSCALL; break; // exit
+      case  1: ev.event = EVENT_SYSCALL; break; // yield
+      case  4: ev.event = EVENT_SYSCALL; break; // write
       default: ev.event = EVENT_ERROR; break;
     }
 
