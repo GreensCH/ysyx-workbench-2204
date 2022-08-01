8684d3d3de8c2ee2828016b4eb7301798781477c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 22:52:37 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:52:37 up  9:04,  1 user,  load average: 0.33, 0.35, 0.29

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -65,12 +65,12 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  static int _end = 0;
-  if(_syscall_(SYS_brk, increment, 0, 0) == 0){
-    int old = _end;
-    _end += increment;
-    return (void *)old;
-  }
+  // static int _end = 0;
+  // if(_syscall_(SYS_brk, increment, 0, 0) == 0){
+  //   int old = _end;
+  //   _end += increment;
+  //   return (void *)old;
+  // }
   return (void *)-1;
 }
 
