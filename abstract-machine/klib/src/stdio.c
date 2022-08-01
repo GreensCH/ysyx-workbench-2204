dd87101c1f40f78c2c67fc215104716387cfe583
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 14:44:58 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:44:58 up 56 min,  1 user,  load average: 0.12, 0.22, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,92 +4,108 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
 int vprintf(const char *fmt, va_list ap)
 {
-  char out[4096];
-  int cnt = vsprintf(out, fmt, ap);
-  putstr(out);
-  return cnt;
+  char *p, *sval;
+  int ival;
+  // double dval;//用来存放double类型数据,这里没有实现因此不写了
+  for (p = (char *)fmt; *p; p++) {
+    if(*p != '%') {
+      putch(*p);
+      continue;
+    }
+    switch(*++p) {
+      case 'd':
+        ival = va_arg(ap, int);
+        char szd[32];
+        itoa(ival, szd, 10);
+        putstr(szd);
+        break;
+      case 's':
+        for (sval = va_arg(ap, char *); *sval; sval++)
+          putch(*sval);
+        break;
+      default:
+        putch(*p);
+        break;
+    }
+  }
+  return p - fmt;
 }
+
 int printf(const char *fmt, ...)
 {
-  va_list args;
-  va_start(args, fmt);
-  int siz = vprintf(fmt, args);
-  va_end(args);
-  return siz;
+  va_list ap;
+  va_start(ap, fmt);
+  int len = vprintf(fmt, ap);
+  va_end(ap);
+  return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap)
 {
-  int cnt = 0;
-  for (int i = 0; fmt[i]; i++)
+  int ival;
+  char *s;
+  const char *sfmt = fmt;
+  char *sout = out;
+  char szd[32];
+  while (*sfmt)
   {
-    if (fmt[i] != '%')
+    if(*sfmt != '%')
     {
-      out[cnt++] = fmt[i];
+      *sout = *sfmt;
+      sfmt++;
+      sout++;
       continue;
     }
-    int num = 0, num_b_cnt = 0;
-    int num_b[20] = {0};
-    char *str = NULL;
-    char chr;
-    switch (fmt[i + 1])
+    switch (*(sfmt+1)) //sfmt = %; sfmt+1 = d; sfmt+2 =?
     {
-    case 'd':
-      num_b_cnt = 0;
-      num = va_arg(ap, int);
-      if (num == 0)
-        num_b[++num_b_cnt] = 0;
-      else if (num < 0)
-        out[cnt++] = '-', num = -num;
-      while (num != 0)
-      {
-        num_b[++num_b_cnt] = num % 10;
-        num /= 10;
-      }
-      for (int i = num_b_cnt; i >= 1; i--)
-        out[cnt++] = (char)(num_b[i] + '0');
-      break;
-    case 's':
-      str = va_arg(ap, char *);
-      for (int i = 0; str[i]; i++)
-        out[cnt++] = str[i];
-      break;
-      break;
-    case 'p':
-      str = va_arg(ap, char *);
-      for (int i = 0; str[i]; i++)
-        out[cnt++] = str[i];
-      break;
-    case 'c':
-      chr = va_arg(ap, int);
-      out[cnt++] = chr;
-      break;
-    default:
-      break;
+      case 's':/* string */
+        s = va_arg(ap, char *);
+        strcpy(sout, s);
+		    sout += strlen(s);
+        break;
+      case 'd':/* int */
+        ival = va_arg(ap, int);
+        itoa(ival, szd, 10);
+        strcpy(sout, szd);
+		    sout += strlen(szd);
+        break;
+      case 'p':/* int */
+        ival = va_arg(ap, int);
+        int ival2 = (int)ival;
+        itoa(ival2, szd, 10);
+        strcpy(sout, szd);
+		    sout += strlen(szd);
+        break;
+      case 'c':/* char */
+        *sout = (char) va_arg(ap, int);
+		    sout ++;
+        break;
     }
-    i++;
+    /* 转移sfmt指针 */
+    sfmt += 2;
   }
-  out[cnt++] = '\0';
-  return cnt;
-}
+  *sout = '\0';
+  return sout - out;
+}//2,773 us
 
 int sprintf(char *out, const char *fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
-  int siz = vsprintf(out, fmt, args);
+  int len = vsprintf(out, fmt, args);
   va_end(args);
-  return siz;
+  return len;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...)
+int snprintf(char *out, size_t n, const char *fmt, ...) 
 {
   panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) 
 {
   panic("Not implemented");
 }
