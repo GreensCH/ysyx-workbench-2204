333e7bf734cc656809e2ed680fe60d9a0e910973
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 21:20:10 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:20:10 up  6:03,  1 user,  load average: 0.82, 0.46, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -95,9 +95,6 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   switch (fd) {
     case FD_STDOUT:
     case FD_STDERR:
-      for (int i = 0; i < len; i ++) {
-        putch( ((char *)buf)[i] );
-      }
       f->write(buf, 0, len);
     case FD_EVENTS:
       return len;
