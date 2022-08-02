b5088b4dcf5e632cdd9644ea7241f96a86800ca9
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 00:33:13 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:33:13 up  9:16,  1 user,  load average: 0.53, 0.55, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -63,7 +63,7 @@ static inline int sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
   // if(usec == -1)  return -1;
 
   tv->tv_sec  = io_read(AM_TIMER_UPTIME).us;
-  tv->tv_usec = 0;
+  tv->tv_usec = io_read(AM_TIMER_UPTIME).us;
   // // printf("sec%d,usec%d\n",sec,usec);
   // tz->tz_minuteswest = 0;
   // tz->tz_dsttime = 0;
