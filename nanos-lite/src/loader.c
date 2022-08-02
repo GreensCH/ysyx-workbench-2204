6e2cab88c9d6ffb5d6f8905d342c6505463dd835
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 17:01:22 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:01:22 up  1:44,  1 user,  load average: 0.16, 0.16, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -33,7 +33,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf;
   fs_read(fd , &elf, sizeof(Elf_Ehdr));
   assert(*(uint32_t *)elf.e_ident == 0x464C457F);//F L E 0x7f
-  
+  printf("elf addr:%d",&elf);
   Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf.e_phnum);
   ramdisk_read(phdr, elf.e_phoff, sizeof(Elf_Phdr) * elf.e_phnum);
   
