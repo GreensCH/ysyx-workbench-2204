443e77e509f70b47a765e167bfc5827ee97b002e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 21:17:54 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:17:54 up  6:01,  1 user,  load average: 0.60, 0.37, 0.29

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -38,12 +38,12 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin",  0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
-  [FD_FB]     = {"/dev/fb", 0, 0},
-  [FD_EVENTS] = {"/dev/events", 0, 0},
-  [FD_DISPINFO] = {"/proc/dispinfo", 128, 0},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
+  [FD_FB]     = {"/dev/fb", 0, 0, 0, invalid_read, invalid_write},
+  [FD_EVENTS] = {"/dev/events", 0, 0, 0, invalid_read, invalid_write},
+  [FD_DISPINFO] = {"/proc/dispinfo", 128, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };//当上述的函数指针为NULL时, 表示相应文件是一个普通文件
 
