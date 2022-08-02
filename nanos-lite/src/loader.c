d5af626f4f34f70e6acbba871c43f6c10ce05735
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 17:27:48 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:27:48 up  2:11,  1 user,  load average: 0.68, 0.32, 0.27

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -26,34 +26,43 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-
-  size_t fd = fs_open(filename, 0, 0);
-  size_t fsize = fs_filesz(fd);
-
-  Elf_Ehdr elf;
-  fs_read(fd , &elf, sizeof(Elf_Ehdr));
-  printf("fd%d,fsize%d,num%d,size%d,e_phoff%d\n",fd,fsize,elf.e_phnum,elf.e_phentsize,elf.e_phoff);
-  assert(*(uint32_t *)elf.e_ident == 0x464C457F);//F L E 0x7f
-  return elf.e_entry;
-}
-
 // static uintptr_t loader(PCB *pcb, const char *filename) {
+
+//   size_t fd = fs_open(filename, 0, 0);
+//   size_t fsize = fs_filesz(fd);
+
 //   Elf_Ehdr elf;
-//   ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
+//   fs_read(fd , &elf, fsize);
+//   printf("fd%d,fsize%d,num%d,size%d,e_phoff%d\n",fd,fsize,elf.e_phnum,elf.e_phentsize,elf.e_phoff);
 //   assert(*(uint32_t *)elf.e_ident == 0x464C457F);//F L E 0x7f
-  
-//   Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf.e_phnum);
-//   ramdisk_read(phdr, elf.e_phoff, sizeof(Elf_Phdr) * elf.e_phnum);
+
+//   Elf_Phdr *phdr = (uintptr_t)(& elf) + elf.e_phoff;
   
 //   for (int i = 0; i < elf.e_phnum; i++) {
 //     if(phdr[i].p_type != PT_LOAD) continue;
 //     ramdisk_read((char*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);//read program
 //     memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);//set data 0
 //   }
-  
+
 //   return elf.e_entry;
 // }
+
+static uintptr_t loader(PCB *pcb, const char *filename) {
+  Elf_Ehdr elf;
+  ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)elf.e_ident == 0x464C457F);//F L E 0x7f
+  
+  Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf.e_phnum);
+  ramdisk_read(phdr, elf.e_phoff, sizeof(Elf_Phdr) * elf.e_phnum);
+  
+  for (int i = 0; i < elf.e_phnum; i++) {
+    if(phdr[i].p_type != PT_LOAD) continue;
+    ramdisk_write((char*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);//write program
+    memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);//set data 0
+  }
+  
+  return elf.e_entry;
+}
 
 // static uintptr_t loader(PCB *pcb, const char *filename) {
 //   // Log("[Loader] ELF file is reading from '%s'.", filename);
