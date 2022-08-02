af97debff43d4c91dac37d8c5ca8a98d10dd6769
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 17:19:49 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:19:49 up  2:03,  1 user,  load average: 0.27, 0.41, 0.32

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -38,11 +38,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf.e_phnum);
   fs_read(fd, elf.e_phoff - sizeof(Elf_Ehdr), sizeof(Elf_Phdr) * elf.e_phnum);
 
-  for (int i = 0; i < elf.e_phnum; i++) {
-    if(phdr[i].p_type != PT_LOAD) continue;
-    ramdisk_read((char*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);//read program
-    memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);//set data 0
-  }
+  // for (int i = 0; i < elf.e_phnum; i++) {
+  //   if(phdr[i].p_type != PT_LOAD) continue;
+  //   ramdisk_read((char*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);//read program
+  //   memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);//set data 0
+  // }
 
 
   return elf.e_entry;
