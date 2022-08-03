c5c9a7255af1e728a0fb771a1793969e9d5eaae0
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 12:39:33 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:39:33 up 19:53,  1 user,  load average: 0.51, 0.33, 0.31

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,20 +4,24 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <fcntl.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
 static struct timeval  tv;
-static struct timezone tz;
 uint32_t NDL_GetTicks() {
-  gettimeofday(&tv, &tz);
-  return tv.tv_usec;
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+
+  return tv.tv_usec / 1000 + tv.tv_sec * 1000;
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  return 0;
+  int fp = open("/dev/events", O_RDONLY);//read only
+
+  return read(fp, buf, sizeof(char) * len);
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
