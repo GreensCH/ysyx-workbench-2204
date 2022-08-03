67d51cb685fcbad38b40fa76189e944c2109d94e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 18:51:45 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:51:45 up 1 day,  2:05,  1 user,  load average: 0.56, 0.51, 0.41

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,9 +4,11 @@
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
+# define ELF_Off  Elf64_Off
 #else
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
+# define ELF_Off  Elf32_Off
 #endif
 
 #if defined(__ISA_AM_NATIVE__)
@@ -26,26 +28,92 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
+static void read(int fd, void *buf, size_t offset, size_t len){
+  fs_lseek(fd, offset, SEEK_SET);
+  fs_read(fd, buf, len);
+}
 
-  size_t fd = fs_open(filename, 0, 0);
-  size_t fsize = fs_filesz(fd);
 
-  Elf_Ehdr elf;
-  fs_read(fd , &elf, fs_filesz);
-  assert(*(uint32_t *)elf.e_ident == 0x464C457F);//F L E 0x7f
+#define NR_PAGE 8
+#define PAGESIZE 4096
 
-  Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf.e_phnum);
-  fs_lseek(fd, elf.e_phoff, SEEK_SET);
-  fs_read(fd, phdr, sizeof(Elf_Phdr) * elf.e_phnum);
+ __attribute__ ((__used__)) static void * alloc_section_space(AddrSpace *as, uintptr_t vaddr, size_t p_memsz){
+  //size_t page_n = p_memsz % PAGESIZE == 0 ? p_memsz / 4096 : (p_memsz / 4096 + 1);
+  size_t page_n = ((vaddr + p_memsz - 1) >> 12) - (vaddr >> 12) + 1;
+  void *page_start = new_page(page_n);
+
+  printf("Loaded Segment from [%x to %x)\n", vaddr, vaddr + p_memsz);
   
-  for (int i = 0; i < elf.e_phnum; i++) {
-    if(phdr[i].p_type != PT_LOAD) continue;
-    fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
-    fs_read(fd, (char*)phdr[i].p_vaddr, phdr[i].p_filesz);
+  for (int i = 0; i < page_n; ++i){
+    // TODO: 这里prot参数不规范
+    map(as, (void *)((vaddr & ~0xfff) + i * PAGESIZE), (void *)(page_start + i * PAGESIZE), 1);
   }
 
-  return elf.e_entry;
+  return page_start;
+}
+
+#define MAX(a, b)((a) > (b) ? (a) : (b))
+
+static uintptr_t loader(PCB *pcb, const char *filename) {
+  int fd = fs_open(filename, 0, 0);
+  if (fd == -1){ 
+    assert(0); //filename指向文件不存在
+  }
+  
+  AddrSpace *as = &pcb->as;
+  
+  Elf_Ehdr elf_header;
+  read(fd, &elf_header, 0, sizeof(elf_header));
+  //根据小端法 0x7F E L F
+  assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
+  
+  ELF_Off program_header_offset = elf_header.e_phoff;
+  size_t headers_entry_size = elf_header.e_phentsize;
+  int headers_entry_num = elf_header.e_phnum;
+
+  for (int i = 0; i < headers_entry_num; ++i){
+    Elf_Phdr section_entry;
+    read(fd, &section_entry, 
+      i * headers_entry_size + program_header_offset, sizeof(elf_header));
+    void *phys_addr;
+    uintptr_t virt_addr;
+    switch (section_entry.p_type) {
+    case PT_LOAD:
+      //virt_addr = (void *)section_entry.p_vaddr; 
+      // phys_addr = (void *)alloced_page_start + (section_entry.p_vaddr - 0x40000000); // 这里是把0x40000000加载到他对应的实际地址
+      virt_addr = section_entry.p_vaddr;
+      phys_addr = alloc_section_space(as, virt_addr, section_entry.p_memsz);
+
+      // printf("Load to %x with offset %x\n", phys_addr, section_entry.p_offset);
+      //做一个偏移
+      read(fd, phys_addr + (virt_addr & 0xfff), section_entry.p_offset, section_entry.p_filesz);
+      //同样做一个偏移
+      memset(phys_addr + (virt_addr & 0xfff) + section_entry.p_filesz, 0, 
+        section_entry.p_memsz - section_entry.p_filesz);
+      
+      if (section_entry.p_filesz < section_entry.p_memsz){// 应该是.bss节
+        //做一个向上的4kb取整数
+        // if (pcb->max_brk == 0){
+        printf("Setting .bss end %x\n", section_entry.p_vaddr + section_entry.p_memsz);
+        // 我们虽然用max_brk记录了最高达到的位置，但是在新的PCB中，我们并未在页表目录中更新这些信息，oH，所以就会失效啦。
+        // 于是我们就做了一些权衡。
+        //pcb->max_brk = MAX(pcb->max_brk, ROUNDUP(section_entry.p_vaddr + section_entry.p_memsz, 0xfff));
+        //TODO: Trade-off
+        pcb->max_brk = ROUNDUP(section_entry.p_vaddr + section_entry.p_memsz, 0xfff);
+        
+        // }
+      }
+      
+      break;
+    
+    default:
+      break;
+    }
+
+  }
+  
+  printf("Entry: %p\n", elf_header.e_entry);
+  return elf_header.e_entry;
 }
 
 // static uintptr_t loader(PCB *pcb, const char *filename) {
