99df4f8fc9155e93f31722e303589c5e50956d56
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 00:20:38 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:20:38 up  4:40,  1 user,  load average: 0.98, 0.78, 0.67

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -220,7 +220,8 @@ void SDL_SoftStretch(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   int y = (srcrect == NULL ? 0 : srcrect->y);
   int w = (srcrect == NULL ? src->w : srcrect->w);
   int h = (srcrect == NULL ? src->h : srcrect->h);
-
+  printf("srcx:%d,srcy%d,srcw:%d srch %d\n",x,y,w,h);
+  printf("dstx:%d,dsty:%d,dstw:%d dsth %d\n",dstrect->x,dstrect->y,dstrect->w,dstrect->h);
   assert(dstrect);
   if(w == dstrect->w && h == dstrect->h) {
     /* The source rectangle and the destination rectangle
