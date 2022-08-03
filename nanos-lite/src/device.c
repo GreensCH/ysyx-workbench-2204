aa4022db2e786212f2378a62494db1460de5a365
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 17:21:28 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:28 up 1 day, 35 min,  1 user,  load average: 0.89, 0.66, 0.49

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -62,7 +62,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   uint32_t *ptr;
   ptr = (uint32_t *)(&buf);
   printf("draw x %d, y %d, pix %d, offset %d, len %d\n",x,y,ptr,offset,len);
-  io_write(AM_GPU_FBDRAW, x, y, (void *)*ptr, len, 1, false);
+  io_write(AM_GPU_FBDRAW, x, y, (void *)*ptr, len, 1, true);
 
   io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
   
