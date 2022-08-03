23d3621b4c06270f54e625b8e7a32213b55e8291
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 00:59:18 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:59:18 up  5:19,  1 user,  load average: 0.92, 0.53, 0.46

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -23,6 +23,26 @@ static void sh_prompt() {
 }
 
 static void sh_handle_cmd(const char *cmd) {
+  char command[128];
+  strcpy(command, cmd);
+  command[strlen(command) - 1] = '\0';//把'\n'搞掉
+
+  const char split[2] = " ";
+  char *token;
+  char *argv[16];
+  int argc = 0;
+
+  /* 获取第一个子字符串 */
+  token = strtok(command, split);
+  
+  /* 继续获取其他的子字符串 */
+  while( token != NULL ) {
+    argv[argc++] = token;
+    token = strtok(NULL, split);
+  }
+  argv[argc] = NULL;
+
+  execvp(argv[0], argv);
 }
 
 void builtin_sh_run() {
