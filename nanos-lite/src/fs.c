5e7d55e2dd97c8f25350303d16ad7b10b3a0ad90
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 12:40:49 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:40:49 up 19:54,  1 user,  load average: 0.57, 0.43, 0.35

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -41,8 +41,8 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
-  [FD_FB]     = {"/dev/fb", 0, 0, 0, events_read, invalid_write},
-  [FD_EVENTS] = {"/dev/events", 0, 0, 0, invalid_read, invalid_write},
+  [FD_FB]     = {"/dev/fb", 0, 0, 0, invalid_read, invalid_write},
+  [FD_EVENTS] = {"/dev/events", 0, 0, 0, events_read, invalid_write},
   [FD_DISPINFO] = {"/proc/dispinfo", 128, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };//当上述的函数指针为NULL时, 表示相应文件是一个普通文件
