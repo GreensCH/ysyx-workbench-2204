b1cd39afd7528f08f8a1a9c1eea7e869f5e7ea96
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 02:47:51 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:47:51 up 11:14,  2 users,  load average: 0.62, 1.16, 1.02

--- npc/playground/src/CSRU.scala
+++ npc/playground/src/CSRU.scala
@@ -110,11 +110,11 @@ class CSRU extends Module with CoreParameter with CSRs{
    mstatus
    */
   private val mstatus = RegInit("ha00001800".U(64.W))//difftest
-  private val vec_mstatus = VecInit(mstatus)
+  private val vec_mstatus = VecInit(mstatus.asBools())
   private val a_is_mstatus = csr_addr === mstatus
   when(a_is_mstatus)          { csr_rdata := mstatus }
   when(a_is_mstatus & is_csr) { mstatus := csr_wdata }
-  .elsewhen(idb_out.intr | idb_out.exec ) { mstatus(7) := mstatus(3) }// mie -> mpie
+  .elsewhen(idb_out.intr | idb_out.exec ) { vec_mstatus(7):= mstatus(3) }// mie -> mpie
   .elsewhen(idb_out.mret){ vec_mstatus(3) := mstatus(7) }// mpie -> mie
   mstatus(12, 11) := "b11".U//mpp
   idb_in.mie := mstatus(3)
