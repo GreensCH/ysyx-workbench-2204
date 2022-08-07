669db62f69e3d668ee8c5300652d9dd2c3e96f7f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 16:36:48 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:36:48 up  6:06,  4 users,  load average: 1.06, 0.74, 0.64

--- abstract-machine/am/include/arch/riscv64-npc.h
+++ abstract-machine/am/include/arch/riscv64-npc.h
@@ -3,12 +3,19 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[32], mstatus;
+  uintptr_t gpr[32], mcause, mstatus, mepc;
+  void *pdir;//可能是地址空间信息？
+  // uintptr_t mepc, mstatus, mcause, gpr[32];
+  // void *pdir;//可能是地址空间信息？
+  // uintptr_t mepc, mcause, gpr[32], mstatus;
 };
 
 #define GPR1 gpr[17] // a7
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+#define GPR2 gpr[10] // a0
+#define GPR3 gpr[11] // a1
+#define GPR4 gpr[12] // a2
+// #define GPR5 gpr[13] // a3
+// #define GPR6 gpr[14] // a4
+// #define GPR7 gpr[15] // a5
+#define GPRx gpr[10] // a0
 #endif
