08b85939e23d1563fa86b90cb0814004e840a0c7
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 00:18:46 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:18:46 up 13:48,  4 users,  load average: 2.82, 1.73, 1.50

--- npc/playground/src/CSRU.scala
+++ npc/playground/src/CSRU.scala
@@ -179,7 +179,7 @@ class CSRU extends Module with CoreParameter with CSRs{
   */
 //  when(rs1_idx === 0.U) { is_csr := false.B }
 //  when(rd_idx === 0.U) { csr_rdata := 0.U }
-//  idb_in.mie := true.B
-//  idb_in.mtie := true.B
+  idb_in.mie := (mtime > 104149.U)
+  idb_in.mtie := (mtime > 104149.U)
 
 }
