edc9f5fcb4e003935cf717f96ab004293dd18f01
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 01:16:28 +0800

    > run NPC ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:16:28 up 38 min,  2 users,  load average: 4.37, 2.01, 1.25

--- npc/playground/src/IFU.scala
+++ npc/playground/src/IFU.scala
@@ -47,10 +47,8 @@ class PC extends Module {
           inc_pc_out)))
     clear_latch := pc_reg === jump_latch
     /* connection */
-    val pc_start = RegInit(false.B)
-    pc_start := true.B
     dataNext.pc := pc_reg
-    io.next.valid := (((!jump) & (!jump_status_latch)) | clear_latch) & pc_start
+    io.next.valid := ((!jump) & (!jump_status_latch)) | clear_latch
     /* stay */
     dontTouch(clear_latch)
     dontTouch(pc_reg_in)
