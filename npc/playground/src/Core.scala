938513b6a7f3ab0e4929ed395a07629a5b23c45a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 17:49:29 +0800

    > run NPC ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:49:29 up  3:01,  3 users,  load average: 2.14, 1.19, 1.13

--- npc/playground/src/Core.scala
+++ npc/playground/src/Core.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 
 object SparkConfig{
   val ICache: Boolean = true
-  val MEMU: Int = 2//0 -dpic 1-axi 2-dcache
+  val MEMU: Int = 1//0 -dpic 1-axi 2-dcache
   val ChiselRam: Boolean = true
   val CacheHitCount: Boolean = true
   val Debug: Boolean = true
