e304bccbf201a8220815afb3f2d2473a22fe5e46
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 15:56:48 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:56:48 up  1:09,  3 users,  load average: 2.45, 2.51, 1.69

--- npc/playground/src/CLINT.scala
+++ npc/playground/src/CLINT.scala
@@ -92,7 +92,10 @@ class CLINT extends  Module with ClintConfig {
   mmio.b.valid := is_write_2
   mmio.b.bits.id := axi_id_2
   mmio.b.bits.resp := AXI4Parameters.RESP_OKAY
-  printf(p"| c(${mtime}) |")
+  when(mmio.ar.valid | mmio.aw.valid){
+    printf(p"| c(${mtime}) |")
+  }
+
 }
 
 object CLINT extends  ClintConfig {
