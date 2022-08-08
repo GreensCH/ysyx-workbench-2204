a711dabb2d4bd01b2de98552bdc56c64cd16b1da
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 15:25:29 +0800

    > run NPC ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:25:29 up 37 min,  2 users,  load average: 1.28, 1.26, 0.91

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -9,6 +9,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 0x8000000000000007:
+        printf("%d",*((int *)0x02004000));
         ev.event = EVENT_IRQ_TIMER;
         break;
       case 0x8000000000000003:
