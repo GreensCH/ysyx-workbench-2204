e315a3b47b0ae1f90a8f97da9e480fac62fd7342
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 21:31:24 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:31:24 up  2:02,  2 users,  load average: 1.51, 2.12, 1.76

--- npc/playground/src/MDU.scala
+++ npc/playground/src/MDU.scala
@@ -4,6 +4,7 @@ import chisel3.util._
  * ebreak
  */
 class MDUIO extends Bundle{
+  val flush   =   Input(Bool())
   val mul     =   Input(Bool())
   val mulh    =   Input(Bool())
   val mulhu   =   Input(Bool())
@@ -19,9 +20,22 @@ class MDUIO extends Bundle{
 }
 
 class mdu extends BlackBox with HasBlackBoxResource {
-  val io = IO(new MDUIO{
+  val io = IO(new Bundle{
     val clock   =   Input(Clock())
     val reset   =   Input(Reset())
+    val flush   =   Input(Bool())
+    val mul     =   Input(Bool())
+    val mulh    =   Input(Bool())
+    val mulhu   =   Input(Bool())
+    val mulhsu  =   Input(Bool())
+    val div     =   Input(Bool())
+    val divu    =   Input(Bool())
+    val rem     =   Input(Bool())
+    val remu    =   Input(Bool())
+    val src1    =   Input(UInt(64.W))
+    val src2    =   Input(UInt(64.W))
+    val result  =   Output(UInt(64.W))
+    val ready   =   Output(Bool())
   })
   addResource("/mdu.v")
 }
@@ -31,8 +45,19 @@ class MDU extends Module{
   val io = IO(new MDUIO)
 
   private val mdu = Module(new mdu)
-
-  mdu.io <> io
   mdu.io.clock <> clock
   mdu.io.reset <> reset
+  mdu.io.flush   := io.flush
+  mdu.io.mul     := io.mul
+  mdu.io.mulh    := io.mulh
+  mdu.io.mulhu   := io.mulhu
+  mdu.io.mulhsu  := io.mulhsu
+  mdu.io.div     := io.div
+  mdu.io.divu    := io.divu
+  mdu.io.rem     := io.rem
+  mdu.io.remu    := io.remu
+  mdu.io.src1    := io.src1
+  mdu.io.src2    := io.src2
+  io.result := mdu.io.result
+  io.ready  := mdu.io.ready
 }
