8d1d357dd6c45be1f62dc2e70ab7456533b63024
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-16 13:17:56 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:17:56 up  2:26,  2 users,  load average: 1.45, 1.03, 0.75

--- npc/emulator/include/device/map.h
+++ npc/emulator/include/device/map.h
@@ -2,7 +2,8 @@
 #define __DEVICE_MAP_H__
 
 #include <common.h>
-#include <difftest.h>
+#include "npc/difftest.h"
+#include "difftest-def.h"
 // #include <cpu/difftest.h>
 typedef void(*io_callback_t)(uint32_t, int, bool);
 uint8_t* new_space(int size);
