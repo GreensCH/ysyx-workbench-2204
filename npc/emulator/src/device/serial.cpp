e8715d059080e8ca5989c6b008ae23efc59d3bac
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-16 16:23:16 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:16 up  5:31,  2 users,  load average: 1.14, 1.09, 0.90

--- npc/emulator/src/device/serial.cpp
+++ npc/emulator/src/device/serial.cpp
@@ -15,7 +15,7 @@ static void serial_putc(char ch) {
 }
 
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
-  // assert(len == 1);
+  assert(len == 1);
   switch (offset) {
     /* We bind the serial port with the host stderr in NEMU. */
     case CH_OFFSET:
@@ -28,6 +28,10 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
 
 void init_serial() {
   serial_base = new_space(8);
+#ifdef CONFIG_HAS_PORT_IO
+  add_pio_map ("serial", CONFIG_SERIAL_PORT, serial_base, 8, serial_io_handler);
+#else
   add_mmio_map("serial", CONFIG_SERIAL_MMIO, serial_base, 8, serial_io_handler);
+#endif
 
 }
