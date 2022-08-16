c443d180f4d366b5bef8af7b61bc0fc16427cf96
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-16 16:13:16 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:13:16 up  5:21,  2 users,  load average: 0.55, 0.56, 0.64

--- npc/emulator/src/device/filelist.mk
+++ npc/emulator/src/device/filelist.mk
@@ -1,12 +1,12 @@
 DIRS-y += src/device/io
-SRCS-$(CONFIG_DEVICE) += src/device/device.c src/device/alarm.c src/device/intr.c
-SRCS-$(CONFIG_HAS_SERIAL) += src/device/serial.c
-SRCS-$(CONFIG_HAS_TIMER) += src/device/timer.c
-SRCS-$(CONFIG_HAS_KEYBOARD) += src/device/keyboard.c
-SRCS-$(CONFIG_HAS_VGA) += src/device/vga.c
-SRCS-$(CONFIG_HAS_AUDIO) += src/device/audio.c
-SRCS-$(CONFIG_HAS_DISK) += src/device/disk.c
-SRCS-$(CONFIG_HAS_SDCARD) += src/device/sdcard.c
+SRCS-$(CONFIG_DEVICE) += src/device/device.c src/device/alarm.c src/device/intr.cpp
+SRCS-$(CONFIG_HAS_SERIAL) += src/device/serial.cpp
+SRCS-$(CONFIG_HAS_TIMER) += src/device/timer.cpp
+SRCS-$(CONFIG_HAS_KEYBOARD) += src/device/keyboard.cpp
+SRCS-$(CONFIG_HAS_VGA) += src/device/vga.cpp
+SRCS-$(CONFIG_HAS_AUDIO) += src/device/audio.cpp
+SRCS-$(CONFIG_HAS_DISK) += src/device/disk.cpp
+SRCS-$(CONFIG_HAS_SDCARD) += src/device/sdcard.cpp
 
 SRCS-BLACKLIST-$(CONFIG_TARGET_AM) += src/device/alarm.c
 
