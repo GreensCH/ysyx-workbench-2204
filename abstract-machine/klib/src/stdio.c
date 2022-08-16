82778ce9d0e7bc7560a75d2bc8c25a7f61242366
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-16 18:23:50 +0800

    > run NEMU ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:23:50 up  7:32,  2 users,  load average: 0.50, 0.75, 0.96

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,86 +4,112 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+//第一个参数是要转换的数字，第二个参数是要写入转换结果的目标字符串，第三个参数是转移数字时所用的基数，10：十进制；2：二进制...
+char* itoa(int num,char* str,int radix)
+{/*索引表*/
+    char index[]="0123456789ABCDEF";
+    unsigned unum;/*中间变量*/
+    int i=0,j,k;
+    /*确定unum的值*/
+    if(radix==10&&num<0)/*十进制负数*/
+    {
+        unum=(unsigned)-num;
+        str[i++]='-';
+    }
+    else unum=(unsigned)num;/*其他情况*/
+    /*转换*/
+    do{
+        str[i++]=index[unum%(unsigned)radix];
+        unum/=radix;
+       }while(unum);
+    str[i]='\0';
+    /*逆序*/
+    if(str[0]=='-')
+        k=1;/*十进制负数*/
+    else
+        k=0;
+     
+    for(j=k;j<=(i-1)/2;j++)
+    {       char temp;
+        temp=str[j];
+        str[j]=str[i-1+k-j];
+        str[i-1+k-j]=temp;
+    }
+    return str;
+}
+
+
 int vprintf(const char *fmt, va_list ap)
 {
-  char out[4096];
-  int cnt = vsprintf(out, fmt, ap);
-  putstr(out);
-  return cnt;
+  panic("Not implemented");
 }
+
 int printf(const char *fmt, ...)
 {
-  va_list args;
-  va_start(args, fmt);
-  int siz = vprintf(fmt, args);
-  va_end(args);
-  return siz;
+  panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap)
 {
-  int cnt = 0;
-  for (int i = 0; fmt[i]; i++)
+  int d;
+  char c;
+  char *s;
+  const char *sfmt = fmt;
+  char *sout = out;
+
+  while (*sfmt)
   {
-    if (fmt[i] != '%')
+    if(*sfmt != '%')
     {
-      out[cnt++] = fmt[i];
+      *sout = *sfmt;
+      sfmt++;
+      sout++;
       continue;
     }
-    int num = 0, num_b_cnt = 0;
-    int num_b[20] = {0};
-    char *str = NULL;
-    char chr;
-    switch (fmt[i + 1])
+    switch (*(sfmt+1)) //sfmt = %; sfmt+1 = d; sfmt+2 =?
     {
-    case 'd':
-      num_b_cnt = 0;
-      num = va_arg(ap, int);
-      if (num == 0)
-        num_b[++num_b_cnt] = 0;
-      else if (num < 0)
-        out[cnt++] = '-', num = -num;
-      while (num != 0)
-      {
-        num_b[++num_b_cnt] = num % 10;
-        num /= 10;
-      }
-      for (int i = num_b_cnt; i >= 1; i--)
-        out[cnt++] = (char)(num_b[i] + '0');
-      break;
-    case 's':
-      str = va_arg(ap, char *);
-      for (int i = 0; str[i]; i++)
-        out[cnt++] = str[i];
-      break;
-    case 'c':
-      chr = va_arg(ap, int);
-      out[cnt++] = chr;
-      break;
-    default:
-      break;
+      case 's':/* string */
+        s = va_arg(ap, char *);
+        strcpy(sout, s);
+		    sout += strlen(s);
+        break;
+      case 'd':/* int */
+        d = va_arg(ap, int);
+        char szd[32];
+        itoa(d, szd, 10);
+        strcpy(sout, szd);
+		    sout += strlen(szd);
+        break;
+      case 'c':/* char */
+        /* need a cast here since va_arg only takes fully promoted types */
+        c = (char) va_arg(ap, int);
+        *sout = c;
+		    sout ++;
+        break;
     }
-    i++;
+    /* 转移sfmt指针 */
+    sfmt += 2;
   }
-  out[cnt++] = '\0';
-  return cnt;
-}
+  *sout = '\0';
+  return sout - out;
+}//2,773 us
 
 int sprintf(char *out, const char *fmt, ...)
 {
   va_list args;
   va_start(args, fmt);
-  int siz = vsprintf(out, fmt, args);
+  int len = vsprintf(out, fmt, args);
   va_end(args);
-  return siz;
+  return len;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...)
+int snprintf(char *out, size_t n, const char *fmt, ...) 
 {
   panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) 
 {
   panic("Not implemented");
 }
