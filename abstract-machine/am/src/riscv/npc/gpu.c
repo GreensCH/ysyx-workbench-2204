fd9d5fc2187f540ec8b96748cfa5c62ddd56eaa9
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 14:42:16 +0800

    > generate verilog ysyx_22040978 常桂林 Linux chang-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:42:16 up 17:44,  4 users,  load average: 2.49, 2.72, 1.90

--- abstract-machine/am/src/riscv/npc/gpu.c
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -11,10 +11,10 @@ void __am_gpu_init() {
   uint32_t vgactl = inl(VGACTL_ADDR);
   vgaw = vgactl >> 16;
   vgah = vgactl & 0xFFFF;
-  // int i;
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);
+  int i;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < vgaw * vgah; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -35,18 +35,18 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {//屏幕大小寄存器
   int i;
   int cnt = 0;
   int t = ctl->y * vgaw + ctl->x;
-  for(j = 0; j < ctl->h; j++){
-    for (i = 0; i < ctl->w; i++){
-      fb[t] = ((uint32_t*)ctl->pixels)[++cnt];
-      t += 1;
-    }
-    t += (vgaw - ctl->w);
-  }
-  //ctl am gpu  --> small
-  //fb  vga mem --> big 
-  if (ctl->sync) {//同步寄存器
-    outl(SYNC_ADDR, 1);
-  }
+  // for(j = 0; j < ctl->h; j++){
+  //   for (i = 0; i < ctl->w; i++){
+  //     fb[t] = ((uint32_t*)ctl->pixels)[++cnt];
+  //     t += 1;
+  //   }
+  //   t += (vgaw - ctl->w);
+  // }
+  // //ctl am gpu  --> small
+  // //fb  vga mem --> big 
+  // if (ctl->sync) {//同步寄存器
+  //   outl(SYNC_ADDR, 1);
+  // }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
